c105b492c6cff853cf9b96639a2fad35cd80ad85
void <theprismaticvoid@gmail.com>
2019-06-15 17:42:51 -0500

    Missing docs

--- src/Windowing/Silk.NET.Windowing.Common/Interfaces/IWindowVirtualFunctions.cs
+++ src/Windowing/Silk.NET.Windowing.Common/Interfaces/IWindowVirtualFunctions.cs
@@ -49,6 +49,10 @@ namespace Silk.NET.Windowing.Common
         /// <param name="isVisible">If the window is visible or not.</param>
         void OnVisibilityChanged(bool isVisible);
 
+        /// <summary>
+        /// Called when the user drops files onto the window.
+        /// </summary>
+        /// <param name="files">An array of paths to the files dropped.</param>
         void OnFileDrop(string[] files);
     }
 }
