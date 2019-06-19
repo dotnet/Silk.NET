00d9fa8e5bd822ace19482f9e61e901898f88d65
void <theprismaticvoid@gmail.com>
2019-06-19 13:10:08 -0500

    Window constructor

--- src/Windowing/Silk.NET.Windowing.Desktop/GlfwWindow.cs
+++ src/Windowing/Silk.NET.Windowing.Desktop/GlfwWindow.cs
@@ -178,8 +178,69 @@ namespace Silk.NET.Windowing.Desktop
 
         public GlfwWindow(WindowOptions options)
         {
-            unsafe
-            {
+            unsafe {
+                // Title and Size must be set before the window is created.
+                _title = options.Title;
+                _size = options.Size;
+                
+                // Set window border.
+                switch (options.WindowBorder) {
+                    case WindowBorder.Hidden:
+                        glfw.WindowHint(WindowHintBool.Decorated, false);
+                        glfw.WindowHint(WindowHintBool.Resizable, false);
+                        break;
+
+                    case WindowBorder.Resizable:
+                        glfw.WindowHint(WindowHintBool.Decorated, true);
+                        glfw.WindowHint(WindowHintBool.Resizable, true);
+                        break;
+
+                    case WindowBorder.Fixed:
+                        glfw.WindowHint(WindowHintBool.Decorated, true);
+                        glfw.WindowHint(WindowHintBool.Resizable, false);
+                        break;
+                }
+
+                _windowBorder = WindowBorder;
+
+                // Set window API.
+                switch (options.API.API) {
+                    case ContextAPI.None:
+                        glfw.WindowHint(WindowHintClientApi.ClientApi, ClientApi.NoApi);
+                        break;
+                    case ContextAPI.OpenGL:
+                        glfw.WindowHint(WindowHintClientApi.ClientApi, ClientApi.OpenGL);
+                        break;
+                    case ContextAPI.OpenGLES:
+                        glfw.WindowHint(WindowHintClientApi.ClientApi, ClientApi.OpenGLES);
+                        break;
+                }
+                
+                // Set API version.
+                glfw.WindowHint(WindowHintInt.ContextVersionMajor, options.API.Version.MajorVersion);
+                glfw.WindowHint(WindowHintInt.ContextVersionMinor, options.API.Version.MinorVersion);
+                
+                // Set API flags
+                if (options.API.Flags.HasFlag(ContextFlags.ForwardCompatible))
+                {
+                    glfw.WindowHint(WindowHintBool.OpenGLForwardCompat, true);
+                }
+
+                if (options.API.Flags.HasFlag(ContextFlags.Debug))
+                {
+                    glfw.WindowHint(WindowHintBool.OpenGLDebugContext, true);
+                }
+
+                // Set API profile
+                glfw.WindowHint(WindowHintOpenGlProfile.OpenGlProfile,
+                    options.API.Profile == ContextProfile.Core ? OpenGlProfile.Core : OpenGlProfile.Compat);
+
+                // Create window
+                WindowPtr = glfw.CreateWindow(_size.Width, _size.Height, _title, null, null);
+
+                WindowState = options.WindowState;
+                Position = options.Position;
+                VSync = options.VSync;
             }
         }
 
