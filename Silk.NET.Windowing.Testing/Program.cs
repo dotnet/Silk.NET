d97a1e6051418efa98de25bcb29aaa91e0485de1
void <theprismaticvoid@gmail.com>
2019-06-21 14:24:00 -0500

    Add testing executable, will be removed at release

---
+++ Silk.NET.Windowing.Testing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Silk.NET.Windowing.Common;
+using Silk.NET.Windowing.Desktop;
+
+namespace Silk.NET.Windowing.Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var window = new GlfwWindow(WindowOptions.Default);
+            window.Run();
+        }
+    }
+}
