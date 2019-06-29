9de503e31645f7f252f2de25744d9f7c7d702095
void <theprismaticvoid@gmail.com>
2019-06-29 00:00:45 -0500

    Add tests for all events

--- src/Windowing/Silk.NET.Windowing.Testing/Program.cs
+++ src/Windowing/Silk.NET.Windowing.Testing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using Silk.NET.Windowing.Common;
 using Silk.NET.Windowing.Desktop;
 
@@ -9,9 +10,21 @@ namespace Silk.NET.Windowing.Testing
         private static void Main()
         {
             var options = WindowOptions.Default;
+            
             options.UpdatesPerSecond = 60.0;
+            options.FramesPerSecond = 60.0;
+            
             var window = new GlfwWindow(options);
+
             window.OnFileDrop += OnFileDrop;
+            window.OnMove += OnMove;
+            window.OnResize += OnResize;
+            window.OnStateChanged += OnStateChanged;
+            window.OnLoad += OnLoad;
+            window.OnClosing += OnClosing;
+            window.OnFocusChanged += OnFocusChanged;
+            window.OnVisibilityChanged += OnVisibilityChanged;
+            
             window.Run();
         }
 
@@ -20,6 +33,41 @@ namespace Silk.NET.Windowing.Testing
             foreach (var file in args) {
                 Console.WriteLine(file);
             }
+        }
+
+        public static void OnMove(Point position)
+        {
+            Console.WriteLine(position);
+        }
+
+        public static void OnResize(Size size)
+        {
+            Console.WriteLine(size);
+        }
+
+        public static void OnStateChanged(WindowState state)
+        {
+            Console.WriteLine(state);
+        }
+
+        public static void OnLoad()
+        {
+            Console.WriteLine("Finished loading");
+        }
+
+        public static void OnClosing()
+        {
+            Console.WriteLine("Window is closing now");
+        }
+
+        public static void OnFocusChanged(bool isFocused)
+        {
+            Console.WriteLine($"Focused = {isFocused}");
+        }
+
+        public static void OnVisibilityChanged(bool isVisible)
+        {
+           Console.WriteLine($"Visible = {isVisible}"); 
         }
     }
 }
