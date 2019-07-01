f4cc1e0ff3eaba6c389bdce95dd9c23a197c5fec
void <theprismaticvoid@gmail.com>
2019-07-01 13:18:37 -0500

    Cleanup in Core

--- src/Silk.NET.Core/Native/NativeExtension.cs
+++ src/Silk.NET.Core/Native/NativeExtension.cs
@@ -2,7 +2,7 @@ using AdvancedDLSupport;
 
 namespace Silk.NET.Core.Native
 {
-    public abstract class NativeExtension<T> : NativeLibraryBase where T:NativeAPI
+    public abstract class NativeExtension : NativeLibraryBase
     {
         protected NativeExtension(string path, ImplementationOptions options) : base(path, options)
         {
