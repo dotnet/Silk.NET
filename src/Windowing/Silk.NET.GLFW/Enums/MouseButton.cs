7ef8774d22dd6e0258b1c37fb5f02d8cd9581773
void <theprismaticvoid@gmail.com>
2019-07-10 14:18:08 -0500

    Add MouseButton to the GLFW bindings

---
+++ src/Windowing/Silk.NET.GLFW/Enums/MouseButton.cs
@@ -0,0 +1,26 @@
+// This file is part of Silk.NET.
+// 
+// You may modify and distribute Silk.NET under the terms
+// of the MIT license. See the LICENSE file for details.
+
+namespace Silk.NET.GLFW
+{
+    /// <summary>
+    /// The mouse buttons.
+    /// </summary>
+    public enum MouseButton
+    {
+        MouseButton1 = 0,
+        MouseButton2,
+        MouseButton3,
+        MouseButton4,
+        MouseButton5,
+        MouseButton6,
+        MouseButton7,
+        MouseButton8,
+        MouseButtonLast = MouseButton8,
+        MouseButtonLeft = MouseButton1,
+        MouseButtonRight = MouseButton2,
+        MouseButtonMiddle = MouseButton3
+    }
+}
