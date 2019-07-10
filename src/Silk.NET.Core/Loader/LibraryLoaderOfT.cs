2f11d06e52ebb162952a9a2167430042c3a367bb
DMP9 <dmp9biz@gmail.com>
2019-07-10 13:39:23 +0100

    Add a new LibraryLoader<T> class

---
+++ src/Silk.NET.Core/Loader/LibraryLoaderOfT.cs
@@ -0,0 +1,24 @@
+// This file is part of Silk.NET.
+// 
+// You may modify and distribute Silk.NET under the terms
+// of the MIT license. See the LICENSE file for details.
+
+using Silk.NET.Core.Native;
+
+namespace Silk.NET.Core.Loader
+{
+    public static class LibraryLoader<T>
+        where T: NativeAPI
+    {
+        public static T Load(SearchPathContainer paths)
+        {
+            return LibraryLoader.Load<T>(paths);
+        }
+
+        public static TExt Load<TExt>(T baseApi)
+            where TExt : NativeExtension<T>
+        {
+            return LibraryLoader.Load<TExt, T>(baseApi);
+        }
+    }
+}
