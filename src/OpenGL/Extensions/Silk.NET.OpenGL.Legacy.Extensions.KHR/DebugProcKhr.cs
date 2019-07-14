98a1dbcf7bfc1b5b066647ff0407309f4a67ef85
DMP9 <dmp9biz@gmail.com>
2019-07-14 19:26:27 +0100

    Add everything to the solution

---
+++ src/OpenGL/Extensions/Silk.NET.OpenGL.Legacy.Extensions.KHR/DebugProcKhr.cs
@@ -0,0 +1,11 @@
+// This file is part of Silk.NET.
+// 
+// You may modify and distribute Silk.NET under the terms
+// of the MIT license. See the LICENSE file for details.
+
+using System;
+
+namespace Silk.NET.OpenGL.Legacy.Extensions.KHR
+{
+    public delegate void DebugProcKhr(KHR source, KHR type, int id, KHR severity, int length, IntPtr message, IntPtr userParam);
+}
