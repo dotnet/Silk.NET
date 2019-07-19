bc3cf2e9d489d9a5db7e9ca50a392ddb4fa8b408
void <theprismaticvoid@gmail.com>
2019-07-18 21:54:38 -0500

    Some more work

--- examples/Triangle/Game.cs
+++ examples/Triangle/Game.cs
@@ -3,6 +3,7 @@
 // You may modify and distribute Silk.NET under the terms
 // of the MIT license. See the LICENSE file for details.
 
+using System.Drawing;
 using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
 using Silk.NET.Windowing.Common;
@@ -12,30 +13,40 @@ namespace Triangle
     public class Game
     {
         private IWindow window;
-        private GL gl = GL.GetApi();
+        private GL gl;
 
         public Game()
         {
             var options = WindowOptions.Default;
+
             window = Window.Create(options);
 
             window.OnLoad += OnLoad;
             window.OnRender += OnRender;
+            window.OnResize += OnResize;
+
+            gl = GL.GetApi();
         }
 
+        public void Run()
+        {
+            window.MakeCurrent();
+            window.Run();
+        }
+        
         public void OnLoad()
         {
             gl.ClearColor(1.0f, 0.4f, 0.3f, 1.0f);
         }
-        
-        public void Run()
-        {
-            window.Run();
-        }
 
         public void OnRender(double delta)
         {
-            gl.Clear(0x4000);
+            gl.Clear((uint)GLEnum.ColorBufferBit);
+        }
+
+        public void OnResize(Size size)
+        {
+            gl.Viewport(0, 0, (uint)size.Width, (uint)size.Height);
         }
     }
 }
