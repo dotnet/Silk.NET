dd9aeaeed9fa0720428e81aa8476cefe2b220fce
DMP9 <dmp9biz@gmail.com>
2019-07-21 13:29:02 +0100

    Iterate over newParameters instead of old parameters + more fixes in SCO

--- src/Core/BuildTools/Bind/Overloading/StaticCountOverloader.cs
+++ src/Core/BuildTools/Bind/Overloading/StaticCountOverloader.cs
@@ -15,43 +15,51 @@ namespace Silk.NET.BuildTools.Bind.Overloading
 {
     public class StaticCountOverloader : IFunctionOverloader
     {
+        private bool IsApplicable(Parameter x)
+        {
+            return (x.Count?.IsStatic ?? false) &&
+                   x.Type.IndirectionLevels == 1 &&
+                   !x.Type.IsVoidPointer() &&
+                   x.Flow == FlowDirection.In;
+        }
         public IEnumerable<Overload> CreateOverloads(Function function)
         {
-            if (!function.Parameters.Any(x => x.Count.IsStatic && x.Type.IndirectionLevels == 1))
+            if (!function.Parameters.Any(IsApplicable))
             {
                 yield break;
             }
 
             var newParameters = new List<Parameter>(function.Parameters);
-            var indent = string.Empty;
             var sb = new StringBuilder();
 
-            for (var i = 0; i < function.Parameters.Count; i++)
+            for (var i = 0; i < newParameters.Count; i++)
             {
-                var param = function.Parameters[i];
-                if (param.Count.IsStatic && param.Type.IndirectionLevels == 1)
+                var param = newParameters[i];
+                if (IsApplicable(param))
                 {
                     var t = new TypeSignatureBuilder(param.Type).WithIndirectionLevel(0).WithArrayDimensions(1).Build();
-                    sb.Append(param.Type + " " + param.Name + " = stackalloc " + param.Type.Name);
+                    sb.Append("var " + param.Name + " = stackalloc " + param.Type.Name);
                     sb.AppendLine("[" + param.Count.StaticCount + "];");
                     for (var j = 0; j < param.Count.StaticCount; j++)
                     {
                         if (j == 0)
                         {
                             newParameters[i + j] = new ParameterSignatureBuilder(param)
-                                .WithName(param.Name + "_" + j)
+                                .WithName(param.Name + j)
                                 .WithType(new TypeSignatureBuilder(param.Type).WithIndirectionLevel(0).Build())
+                                .WithCount(null)
                                 .Build();
                         }
                         else
                         {
                             newParameters.Insert(i + (j - 1), new ParameterSignatureBuilder(param)
-                                .WithName(param.Name + "_" + j)
+                                .WithName(param.Name + j)
+                                .WithCount(null)
                                 .WithType(new TypeSignatureBuilder(param.Type).WithIndirectionLevel(0).Build())
                                 .Build());
                         }
 
-                        sb.AppendLine(param.Name + "[" + j + "] = " + param.Name + "_" + j + ";");
+                        sb.AppendLine(param.Name + "[" + j + "] = " + param.Name + j + ";");
                     }
                 }
             }
@@ -65,6 +73,13 @@ namespace Silk.NET.BuildTools.Bind.Overloading
             sb.Append(string.Join(", ", function.Parameters.Select(x => Format(x.Name))));
             sb.Append(");");
             sb.AppendLine();
+            
+            yield return new Overload
+            (
+                new FunctionSignatureBuilder(function).WithParameters(newParameters).Build(),
+                sb,
+                true
+            );
 
             string Format(string n)
             {
