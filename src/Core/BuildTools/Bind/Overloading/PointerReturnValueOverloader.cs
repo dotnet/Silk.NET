eef028023c7538edb8f16ecef3a4507756ab61a1
Dylan Perks <dylanperks23@gmail.com>
2019-07-31 19:46:58 +0100

    Miscellaneous windowing fixes (#27)
    * Start experimenting with a basic triangle program.
    
    * Some more work
    
    * Include error code in exception message
    
    * Remove MakeCurrent
    
    * Add a WindowBuilder
    
    * Implement the dispatcher ourselves for Update and Render only
    
    * Fix the current context bug
    
    * Allow invocation on the main thread from the main thread
    
    * Invoke within VSync setter
    
    * Fix a NRE, assign everything, and remove initialOptions
    
    * Fix an InvalidOperationException
    
    * Actually fix it :P
    
    * Remove the double assignment of UseSingleThreadedWindow
    
    * Continue with triangle example
    
    * Delete WindowBuilder.cs
    
    * Run a Using cleanup on the entire project

--- src/Core/BuildTools/Bind/Overloading/PointerReturnValueOverloader.cs
+++ src/Core/BuildTools/Bind/Overloading/PointerReturnValueOverloader.cs
@@ -3,7 +3,6 @@
 // You may modify and distribute Silk.NET under the terms
 // of the MIT license. See the LICENSE file for details.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
