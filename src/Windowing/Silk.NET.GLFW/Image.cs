6efa5f461ea3dcdf965ecb2d3780a5522d10a257
DMP9 <dmp9biz@gmail.com>
2019-08-03 23:45:38 +0100

    Fix GLFW.Image

---
+++ src/Windowing/Silk.NET.GLFW/Image.cs
@@ -0,0 +1,28 @@
+// This file is part of Silk.NET.
+// 
+// You may modify and distribute Silk.NET under the terms
+// of the MIT license. See the LICENSE file for details.
+
+namespace Silk.NET.GLFW
+{
+    /// <summary>
+    /// Represents an image.
+    /// </summary>
+    public struct Image
+    {
+        /// <summary>
+        /// Gets or sets the width of this image in pixels.
+        /// </summary>
+        public int Width { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the height of this image in pixels.
+        /// </summary>
+        public int Height { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the image's bytes.
+        /// </summary>
+        public unsafe byte* Pixels { get; set; }
+    }
+}
