a97509a100dcffc3087f22bf9876b617d1338e2a
Kai Jellinghaus <herrucan.schmitt@gmail.com>
2019-08-11 00:13:40 +0200

    Nullable support for ArrayParameterOverloader

--- src/Core/BuildTools/Bind/Overloading/ArrayParameterOverloader.cs
+++ src/Core/BuildTools/Bind/Overloading/ArrayParameterOverloader.cs
@@ -12,8 +12,6 @@ using MoreLinq.Extensions;
 using Silk.NET.BuildTools.Common.Builders;
 using Silk.NET.BuildTools.Common.Functions;
 
-#nullable disable
-
 namespace Silk.NET.BuildTools.Bind.Overloading
 {
     public class ArrayParameterOverloader : IFunctionOverloader
