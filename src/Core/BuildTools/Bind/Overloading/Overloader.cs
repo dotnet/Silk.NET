eed1e289fe337ec36e276c2df07391a921c120dc
Kai Jellinghaus <herrucan.schmitt@gmail.com>
2019-08-11 00:57:47 +0200

    Nullable support for Core/BuildTools/Bind/Overloading/Overloader.cs

--- src/Core/BuildTools/Bind/Overloading/Overloader.cs
+++ src/Core/BuildTools/Bind/Overloading/Overloader.cs
@@ -7,8 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Silk.NET.BuildTools.Common;
 
-#nullable disable
-
 namespace Silk.NET.BuildTools.Bind.Overloading
 {
     public static class Overloader
