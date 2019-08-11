949eabed90cf354cee1c6d872781e212f28c32da
Kai Jellinghaus <herrucan.schmitt@gmail.com>
2019-08-12 01:03:38 +0200

    Nullable support for Core/BuildTools/Common/Struct.cs

--- src/Core/BuildTools/Common/Struct.cs
+++ src/Core/BuildTools/Common/Struct.cs
@@ -4,9 +4,8 @@
 // of the MIT license. See the LICENSE file for details.
 
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using Silk.NET.BuildTools.Common.Structs;
-
-#nullable disable
 
 namespace Silk.NET.BuildTools.Common
 {
@@ -14,7 +13,16 @@ namespace Silk.NET.BuildTools.Common
     {
         public string Name { get; set; }
         public string NativeName { get; set; }
-        public List<Field> Fields { get; set; } = new List<Field>();
-        public List<Attribute> Attributes { get; set; } = new List<Attribute>();
+        public List<Field> Fields { get; set; }
+        public List<Attribute> Attributes { get; set; }
+
+        [JsonConstructor]
+        public Struct(string name, string nativeName, List<Field>? fields = null, List<Attribute>? attributes = null)
+        {
+            Name = name;
+            NativeName = nativeName;
+            Fields = fields ?? new List<Field>();
+            Attributes = attributes ?? new List<Attribute>();
+        }
     }
 }
