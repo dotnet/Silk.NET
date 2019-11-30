07dc327833555982bbb39a1ac47e5106f2aa0826
Void <33298798+devvoid@users.noreply.github.com>
2019-11-30 09:43:58 -0600

    Allow windows to be invisible on opening. (#70)
    * Start fixing visibility bug
    
    * Wrong docs on constructor
    
    * Fix that didn't work, lots of new XML docs
    
    * Windows can now be invisible on opening
    
    * More XML comments
    
    * Most of the review comments
    
    * Fix comment on DefaultVulkan
    
    * Fix comment on Invoke

--- src/Windowing/Silk.NET.Windowing.Common/WindowExtensions.cs
+++ src/Windowing/Silk.NET.Windowing.Common/WindowExtensions.cs
@@ -7,18 +7,39 @@ using System;
 
 namespace Silk.NET.Windowing.Common
 {
+    /// <summary>
+    /// Extensions for IWindow
+    /// </summary>
     public static class WindowExtensions
     {
+        /// <summary>
+        /// Executes a Func&lt;T&gt; delegate on the given window.
+        /// </summary>
+        /// <param name="window">The window to invoke on.</param>
+        /// <param name="t">The function to invoke.</param>
+        /// <typeparam name="T">Type of the function.</typeparam>
+        /// <remarks>This function is not applicable on single-threaded windows</remarks>
+        /// <returns>The return value of the given delegate.</returns>
         public static T Invoke<T>(this IWindow window, Func<T> t)
         {
             return (T) window.Invoke(t);
         }
 
+        /// <summary>
+        /// Executes an <see cref="Action"/> on the given window.
+        /// </summary>
+        /// <param name="window">The window to invoke on.</param>
+        /// <param name="t">The action to invoke.</param>
+        /// <remarks>This function is not applicable on single-threaded windows</remarks>
         public static void Invoke(this IWindow window, Action t)
         {
             window.Invoke(t);
         }
 
+        /// <summary>
+        /// Start the default event loop on this window.
+        /// </summary>
+        /// <param name="window">The window to begin the loop on.</param>
         public static void Run(this IWindow window)
         {
             window.Open();
