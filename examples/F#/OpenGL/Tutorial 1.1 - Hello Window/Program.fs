6c83a6f4e68fe692f0c0c2c623a13676ec0bd2f1
Phil Thompson <phil@electricvisions.com>
2019-12-07 20:57:26 +0100

    F# Tutorial 1.1 - Hello Window

--- examples/F#/OpenGL/Tutorial 1.1 - Hello Window/Program.fs
+++ examples/F#/OpenGL/Tutorial 1.1 - Hello Window/Program.fs
@@ -1,9 +1,33 @@
-﻿open System
+open System
+open System.Drawing
+
+open Silk.NET.Windowing
+open Silk.NET.Windowing.Common
+open Silk.NET.OpenGL
+open Silk.NET.Input
+open Silk.NET.Input.Common
 
 [<EntryPoint>]
-let main argv =
-    printfn "This tutorial has not been written yet."
-    printfn "Have a go at using the C++ version of this tutorial at:"
-    printfn "    learnopengl.com"
-    printfn "Have fun!"
+let main _ =
+    let mutable options = WindowOptions.Default
+    options.Size <- Size(800, 600)
+    options.Title <- "LearnOpenGL with Silk.NET"
+    let mutable window = Window.Create(options)
+    let mutable gl = null
+
+    let onKeyboardKey = Action<IKeyboard, Key, int>(fun _ key _ ->
+        match key with
+        | Key.Escape -> window.Close()
+        | _ -> ()
+    )
+
+    window.add_Load (fun () -> 
+        gl <- GL.GetApi()
+        let input = window.GetInput()
+        input.Keyboards
+        |> Seq.iter (fun keyboard -> keyboard.add_KeyDown onKeyboardKey)
+    )
+    window.add_Resize (fun (size: Size) -> gl.Viewport(size))
+
+    window.Run()
     0
