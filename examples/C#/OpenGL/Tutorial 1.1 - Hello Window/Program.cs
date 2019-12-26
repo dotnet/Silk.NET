f11031a03afc84bc01989274a67ebaa6795a0888
Frederik <fjaplv64@gmail.com>
2019-12-26 20:45:51 +0100

    tutorial 1.1

--- examples/C#/OpenGL/Tutorial 1.1 - Hello Window/Program.cs
+++ examples/C#/OpenGL/Tutorial 1.1 - Hello Window/Program.cs
@@ -1,13 +1,41 @@
 using System;
+using Silk.NET.Input;
+using Silk.NET.Input.Common;
+using Silk.NET.Windowing.Common;
+using Silk.NET.Windowing.Desktop;
 
 namespace Tutorial
 {
     class Program
     {
-        static void Main(string[] args)
+        private static GlfwWindow window;
+
+        private static void Main(string[] args)
         {
-            //Hello world test.
-            Console.WriteLine("Hello World!");
+            GlfwWindow window = new GlfwWindow(new WindowOptions()
+                { Size = new System.Drawing.Size(800, 600),
+                Title = "LearnOpenGL with Silk.NET" });
+
+            window.Load += OnLoad;
+
+            window.Run();
+        }
+
+        private static void OnLoad()
+        {
+            IInputContext input = window.GetInput();
+            for (int i = 0; i < input.Keyboards.Count; i++)
+            {
+                input.Keyboards[0].KeyDown += KeyDown;
+            }
+        }
+
+        private static void KeyDown(IKeyboard arg1, Key arg2, int arg3)
+        {
+            if (arg2 == Key.Escape)
+            {
+                window.Close();
+            }
         }
     }
 }
