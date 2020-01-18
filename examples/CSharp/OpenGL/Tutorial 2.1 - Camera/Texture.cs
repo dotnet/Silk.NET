3bddfca3efa8dcf1dbfafedfb574ff4f9b5b0347
Frederik <fjaplv64@gmail.com>
2020-01-18 19:24:03 +0100

    texture update

--- examples/CSharp/OpenGL/Tutorial 2.1 - Camera/Texture.cs
+++ examples/CSharp/OpenGL/Tutorial 2.1 - Camera/Texture.cs
@@ -2,6 +2,8 @@ using Silk.NET.OpenGL;
 using System;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using System.Runtime.InteropServices;
+using SixLabors.ImageSharp.Advanced;
 
 namespace Tutorial
 {
@@ -10,38 +12,34 @@ namespace Tutorial
         private uint _handle;
         private GL _gl;
 
-        public Texture(GL gl, string path)
+        public unsafe Texture(GL gl, string path)
         {
             Image<Rgba32> img = (Image<Rgba32>)Image.Load(path);
-            byte[] data = new byte[img.Height * img.Width * 4];
-            for (int y = 0; y < img.Height; y++)
+
+            fixed (void* data = &MemoryMarshal.GetReference(img.GetPixelSpan()))
             {
-                for (int x = 0; x < img.Width; x++)
-                {
-                    data[(x + y * img.Width) * 4 + 0] = img[x, y].R;
-                    data[(x + y * img.Width) * 4 + 1] = img[x, y].G;
-                    data[(x + y * img.Width) * 4 + 2] = img[x, y].B;
-                    data[(x + y * img.Width) * 4 + 3] = img[x, y].A;
-                }
+                Load(gl, data, (uint)img.Width, (uint)img.Height);
             }
-            Load(gl, data, (uint)img.Width, (uint)img.Height);
 
             img.Dispose();
         }
 
-        public Texture(GL gl, byte[] data, uint width, uint height) => Load(gl, data, width, height);
+        public unsafe Texture(GL gl, byte[] data, uint width, uint height)
+        {
+            fixed (void* d = &data[0])
+            {
+                Load(gl, d, width, height);
+            }
+        }
 
-        private unsafe void Load(GL gl, byte[] data, uint width, uint height)
+        private unsafe void Load(GL gl, void* data, uint width, uint height)
         {
             _gl = gl;
 
             _handle = _gl.GenTexture();
             Bind();
 
-            fixed (void* d = data)
-            {
-                _gl.TexImage2D(TextureTarget.Texture2D, 0, (int)GLEnum.Rgba, width, height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, d);
-            }
+            _gl.TexImage2D(TextureTarget.Texture2D, 0, (int)GLEnum.Rgba, width, height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, data);
             _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)GLEnum.ClampToEdge);
             _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)GLEnum.ClampToEdge);
             _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)GLEnum.Linear);
