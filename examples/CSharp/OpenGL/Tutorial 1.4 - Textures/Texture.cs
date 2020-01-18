0a64b501a14f0d87eeb0a9db45800b9f45ee6387
Frederik <fjaplv64@gmail.com>
2020-01-18 19:20:55 +0100

    Remove pixel looping

--- examples/CSharp/OpenGL/Tutorial 1.4 - Textures/Texture.cs
+++ examples/CSharp/OpenGL/Tutorial 1.4 - Textures/Texture.cs
@@ -2,6 +2,8 @@ using Silk.NET.OpenGL;
 using System;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using System.Runtime.InteropServices;
+using SixLabors.ImageSharp.Advanced;
 
 namespace Tutorial
 {
@@ -10,33 +12,31 @@ namespace Tutorial
         private uint _handle;
         private GL _gl;
 
-        public Texture(GL gl, string path)
+        public unsafe Texture(GL gl, string path)
         {
             //Loading an image using imagesharp.
             Image<Rgba32> img = (Image<Rgba32>)Image.Load(path);
-            //Converting the image into a byte array of data.
-            byte[] data = new byte[img.Height * img.Width * 4];
-            for (int y = 0; y < img.Height; y++)
+
+            fixed (void* data = &MemoryMarshal.GetReference(img.GetPixelSpan()))
             {
-                for (int x = 0; x < img.Width; x++)
-                {
-                    data[(x + y * img.Width) * 4 + 0] = img[x, y].R;
-                    data[(x + y * img.Width) * 4 + 1] = img[x, y].G;
-                    data[(x + y * img.Width) * 4 + 2] = img[x, y].B;
-                    data[(x + y * img.Width) * 4 + 3] = img[x, y].A;
-                }
+                //Loading the actual image.
+                Load(gl, data, (uint)img.Width, (uint)img.Height);
             }
-            //Loading the actual image.
-            Load(gl, data, (uint)img.Width, (uint)img.Height);
 
             //Deleting the img from imagesharp.
             img.Dispose();
         }
 
-        //We want the ability to create a texture using data generated from code aswell.
-        public Texture(GL gl, byte[] data, uint width, uint height) => Load(gl, data, width, height);
+        public unsafe Texture(GL gl, byte[] data, uint width, uint height)
+        {
+            //We want the ability to create a texture using data generated from code aswell.
+            fixed (void* d = &data[0])
+            {
+                Load(gl, d, width, height);
+            }
+        }
 
-        private unsafe void Load(GL gl, byte[] data, uint width, uint height)
+        private unsafe void Load(GL gl, void* data, uint width, uint height)
         {
             //Saving the gl instance.
             _gl = gl;
@@ -46,10 +46,7 @@ namespace Tutorial
             Bind();
 
             //Setting the data of a texture.
-            fixed (void* d = data)
-            {
-                _gl.TexImage2D(TextureTarget.Texture2D, 0, (int)GLEnum.Rgba, width, height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, d);
-            }
+            _gl.TexImage2D(TextureTarget.Texture2D, 0, (int)GLEnum.Rgba, width, height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, data);
             //Setting some texture perameters so the texture behaves as expected.
             _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)GLEnum.Repeat);
             _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)GLEnum.Repeat);
