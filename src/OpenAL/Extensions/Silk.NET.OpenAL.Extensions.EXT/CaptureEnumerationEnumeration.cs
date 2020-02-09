ec981dab5a36c47c413655085c9ac2a3316da232
Dylan Perks <dylanperks23@gmail.com>
2020-02-09 12:28:08 +0000

    Update CaptureEnumerationEnumeration.cs

--- src/OpenAL/Extensions/Silk.NET.OpenAL.Extensions.EXT/CaptureEnumerationEnumeration.cs
+++ src/OpenAL/Extensions/Silk.NET.OpenAL.Extensions.EXT/CaptureEnumerationEnumeration.cs
@@ -44,7 +44,7 @@ namespace Silk.NET.OpenAL.Extensions.EXT
                 var currentPos = result;
                 while (true) {
                     var currentString = Marshal.PtrToStringAnsi(new IntPtr(currentPos));
-                    if (!string.IsNullOrEmpty(currentString)) {
+                    if (string.IsNullOrEmpty(currentString)) {
                         break;
                     }
 
