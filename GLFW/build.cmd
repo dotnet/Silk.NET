e03766f476e298ab334b2f5dc2ac05f3ac865b08
DMP9 <dmp9biz@gmail.com>
2020-02-18 21:34:49 +0000

    Update to GLFW 3.3.2

--- GLFW/build.cmd
+++ GLFW/build.cmd
@@ -1,5 +1,1 @@
-nuget pack linux-x64/Ultz.Native.GLFW.Linux.x64.nuspec -OutputDirectory out
-nuget pack osx-x64/Ultz.Native.GLFW.OSX.x64.nuspec -OutputDirectory out
-nuget pack win-x64/Ultz.Native.GLFW.Windows.x64.nuspec -OutputDirectory out
-nuget pack win-x86/Ultz.Native.GLFW.Windows.x86.nuspec -OutputDirectory out
-nuget pack HostPackage/Ultz.Native.GLFW.nuspec -OutputDirectory out
+nuget pack Ultz.Native.GLFW.nuspec
