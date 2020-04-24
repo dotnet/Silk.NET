d7d1c42698b6896fb8ae945033152445cfacd442
DMP9 <dmp9biz@gmail.com>
2020-04-25 00:21:52 +0100

    Deprecate adaptive vsync because it never worked anyway

--- src/Windowing/Silk.NET.Windowing.Common/Enums/VSyncMode.cs
+++ src/Windowing/Silk.NET.Windowing.Common/Enums/VSyncMode.cs
@@ -3,6 +3,8 @@
 // You may modify and distribute Silk.NET under the terms
 // of the MIT license. See the LICENSE file for details.
 
+using System;
+
 namespace Silk.NET.Windowing.Common
 {
     /// <summary>
@@ -25,6 +27,12 @@ namespace Silk.NET.Windowing.Common
         /// VSync will be disabled. Otherwise, it will be enabled. If the framerate is uncapped, this is the same as
         /// <see cref="On" />.
         /// </summary>
+        [Obsolete
+        (
+            "Adaptive VSync has been deprecated and usage is not recommended. If you need this sort " +
+            "of behaviour, we recommend implementing your own synchronization systems. " +
+            "This feature will be removed in 2.0 and the VSync property replaced with a boolean."
+        )]
         Adaptive
     }
 }
