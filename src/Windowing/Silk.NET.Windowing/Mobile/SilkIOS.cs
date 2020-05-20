56fa0a82c5642c9113bbb857701235925a5a2a78
Dylan Perks <dmp9biz@gmail.com>
2020-05-20 18:56:26 +0100

    Add preliminary mobile classes

---
+++ src/Windowing/Silk.NET.Windowing/Mobile/SilkIOS.cs
@@ -0,0 +1,15 @@
+// This file is part of Silk.NET.
+// 
+// You may modify and distribute Silk.NET under the terms
+// of the MIT license. See the LICENSE file for details.
+
+namespace Silk.NET.Windowing.Mobile
+{
+    /// <summary>
+    /// Provides functions for using Silk.NET in conjunction with iOS and SDL.
+    /// </summary>
+    public static class SilkIOS
+    {
+        // TODO
+    }
+}
