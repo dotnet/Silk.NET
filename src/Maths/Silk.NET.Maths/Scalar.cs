3991423ba1552171c8bcbd121c05d5b13f5e4d93
Kai Jellinghaus <kai.jellinghaus1@gmail.com>
2020-06-16 06:38:52 +0200

    Add License header to Scalar.cs

--- src/Maths/Silk.NET.Maths/Scalar.cs
+++ src/Maths/Silk.NET.Maths/Scalar.cs
@@ -1,4 +1,9 @@
-﻿using System;
+// This file is part of Silk.NET.
+// 
+// You may modify and distribute Silk.NET under the terms
+// of the MIT license. See the LICENSE file for details.
+
+using System;
 using System.Runtime.CompilerServices;
 
 namespace Silk.NET.Maths
