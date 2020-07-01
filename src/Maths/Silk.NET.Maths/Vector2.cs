bbf331257953a567be29beb2f1303143560e14e8
Kai Jellinghaus <kai.jellinghaus1@gmail.com>
2020-07-01 15:27:54 +0200

    Use [GenerateReferenceFunction] in Vector2

--- src/Maths/Silk.NET.Maths/Vector2.cs
+++ src/Maths/Silk.NET.Maths/Vector2.cs
@@ -83,60 +83,43 @@ namespace Silk.NET.Maths
 
         public Vector2<T> Normalized() => Normalize(this);
 
+        [GenerateReferenceFunction]
         public static Vector2<T> Add(Vector2<T> a, Vector2<T> b)
             => new Vector2<T>(Scalar.Add(a.X, b.X), Scalar.Add(a.Y, b.Y));
 
-        public static void Add(ref Vector2<T> a, ref Vector2<T> b, out Vector2<T> result) => result = Add(a, b);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> Subtract(Vector2<T> a, Vector2<T> b)
             => new Vector2<T>(Scalar.Subtract(a.X, b.X), Scalar.Subtract(a.Y, b.Y));
 
-        public static void Subtract(ref Vector2<T> a, ref Vector2<T> b, out Vector2<T> result)
-            => result = Subtract(a, b);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> Multiply(Vector2<T> vector, T scale)
             => new Vector2<T>(Scalar.Multiply(vector.X, scale), Scalar.Multiply(vector.Y, scale));
 
-        public static void Multiply(ref Vector2<T> vector, T scale, out Vector2<T> result)
-            => result = Multiply(vector, scale);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> Multiply(Vector2<T> vector, Vector2<T> scale)
             => new Vector2<T>(Scalar.Multiply(vector.X, scale.X), Scalar.Multiply(vector.Y, scale.Y));
 
-        public static void Multiply(ref Vector2<T> vector, ref Vector2<T> scale, out Vector2<T> result)
-            => result = Multiply(vector, scale);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> Divide(Vector2<T> vector, T scale)
             => new Vector2<T>(Scalar.Divide(vector.X, scale), Scalar.Divide(vector.Y, scale));
 
-
-        public static void Divide(ref Vector2<T> vector, T scale, out Vector2<T> result)
-            => result = Divide(vector, scale);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> Divide(T value, Vector2<T> scale)
             => new Vector2<T>(Scalar.Divide(value, scale.X), Scalar.Divide(value, scale.Y));
 
-
-        public static void Divide(T value, ref Vector2<T> scale, out Vector2<T> result) => result = Divide(value, scale);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> Divide(Vector2<T> vector, Vector2<T> scale)
             => new Vector2<T>(Scalar.Divide(vector.X, scale.X), Scalar.Divide(vector.Y, scale.Y));
 
-        public static void Divide(ref Vector2<T> vector, ref Vector2<T> scale, out Vector2<T> result)
-            => result = Divide(vector, scale);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> ComponentMin(Vector2<T> a, Vector2<T> b)
             => new Vector2<T>(Min(a.X, b.X), Min(a.Y, b.Y));
 
-        public static void ComponentMin(ref Vector2<T> a, ref Vector2<T> b, out Vector2<T> result)
-            => result = ComponentMin(a, b);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> ComponentMax(Vector2<T> a, Vector2<T> b)
             => new Vector2<T>(Max(a.X, b.X), Max(a.Y, b.Y));
 
-        public static void ComponentMax(ref Vector2<T> a, ref Vector2<T> b, out Vector2<T> result)
-            => result = ComponentMax(a, b);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> MagnitudeMin(Vector2<T> left, Vector2<T> right)
         {
             if (Larger(left.LengthSquared, right.LengthSquared))
@@ -147,9 +130,7 @@ namespace Silk.NET.Maths
             return left;
         }
 
-        public static void MagnitudeMin(ref Vector2<T> left, ref Vector2<T> right, out Vector2<T> result)
-            => result = MagnitudeMin(left, right);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> MagnitudeMax(Vector2<T> left, Vector2<T> right)
         {
             if (Larger(left.LengthSquared, right.LengthSquared))
@@ -160,58 +141,41 @@ namespace Silk.NET.Maths
             return right;
         }
 
-        public static void MagnitudeMax(ref Vector2<T> left, ref Vector2<T> right, out Vector2<T> result)
-            => result = MagnitudeMax(left, right);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> Clamp(Vector2<T> vec, Vector2<T> min, Vector2<T> max)
             => new Vector2<T>(Scalar.Clamp(vec.X, min.X, max.X), Scalar.Clamp(vec.Y, min.Y, max.Y));
 
-        public static void Clamp(ref Vector2<T> vec, ref Vector2<T> min, ref Vector2<T> max, out Vector2<T> result)
-            => result = Clamp(vec, min, max);
-
+        [GenerateReferenceFunction]
         public static T Distance(Vector2<T> vec1, Vector2<T> vec2) => (vec1 - vec2).Length;
 
-        public static void Distance(ref Vector2<T> vec1, ref Vector2<T> vec2, out T result)
-            => result = Distance(vec1, vec2);
-
+        [GenerateReferenceFunction]
         public static T DistanceSquared(Vector2<T> vec1, Vector2<T> vec2) => (vec1 - vec2).LengthSquared;
 
-        public static void DistanceSquared(ref Vector2<T> vec1, ref Vector2<T> vec2, out T result)
-            => result = DistanceSquared(vec1, vec2);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> Normalize(Vector2<T> vec) => vec / vec.Length;
 
-        public static void Normalize(ref Vector2<T> vec, out Vector2<T> result) => result = Normalize(vec);
-
+        [GenerateReferenceFunction]
         public static T Dot(Vector2<T> left, Vector2<T> right)
         {
             var mul = left * right;
             return Scalar.Add(mul.X, mul.Y);
         }
 
-        public static void Dot(ref Vector2<T> left, ref Vector2<T> right, out T result) => result = Dot(left, right);
-
+        [GenerateReferenceFunction]
         public static T PerpDot(Vector2<T> left, Vector2<T> right)
             => Scalar.Subtract(Scalar.Multiply(left.X, right.Y), Scalar.Multiply(left.Y, right.X));
 
-        public static void PerpDot(ref Vector2<T> left, ref Vector2<T> right, out T result)
-            => result = PerpDot(left, right);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> Lerp(Vector2<T> a, Vector2<T> b, T blend)
             => a * Scalar.Subtract(Scalar<T>.One, blend) + b * blend;
 
-        public static void Lerp(ref Vector2<T> a, ref Vector2<T> b, T blend, out Vector2<T> result)
-            => result = Lerp(a, b, blend);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> BaryCentric(Vector2<T> a, Vector2<T> b, Vector2<T> c, T u, T v)
             =>
                 a * Scalar.Subtract(Scalar<T>.One, u) * Scalar.Subtract(Scalar<T>.One, v) +
                 b * u * Scalar.Subtract(Scalar<T>.One, v) + c * Scalar.Subtract(Scalar<T>.One, u) * v;
 
-        public static void BaryCentric
-            (ref Vector2<T> a, ref Vector2<T> b, ref Vector2<T> c, T u, T v, out Vector2<T> result)
-            => result = BaryCentric(a, b, c, u, v);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> Transform(Vector2<T> vec, Matrix2X2<T> mat)
             =>
                 new Vector2<T>
@@ -219,9 +183,7 @@ namespace Silk.NET.Maths
                     Scalar.Add(Scalar.Multiply(vec.X, mat.M11), Scalar.Multiply(vec.Y, mat.M21)),
                     Scalar.Add(Scalar.Multiply(vec.X, mat.M12), Scalar.Multiply(vec.Y, mat.M22)));
 
-        public static void Transform(ref Vector2<T> vec, ref Matrix2X2<T> mat, out Vector2<T> result)
-            => result = Transform(vec, mat);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> Transform(Vector2<T> vec, Quaternion<T> quat)
         {
             var x2 = Scalar.Add(quat.X, quat.X);
@@ -246,17 +208,13 @@ namespace Silk.NET.Maths
                         Scalar.Multiply(vec.Y, Scalar.Subtract(Scalar.Subtract(Scalar<T>.One, xx2), zz2))));
         }
 
-        public static void Transform(ref Vector2<T> vec, ref Quaternion<T> quat, out Vector2<T> result)
-            => result = Transform(vec, quat);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> Cos(Vector2<T> vector) => new Vector2<T>(Scalar.Cos(vector.X), Scalar.Cos(vector.Y));
 
-        public static void Cos(ref Vector2<T> vector, out Vector2<T> result) => result = Cos(vector);
-
+        [GenerateReferenceFunction]
         public static Vector2<T> Sin(Vector2<T> vector) => new Vector2<T>(Scalar.Sin(vector.X), Scalar.Sin(vector.Y));
-
-        public static void Sin(ref Vector2<T> vector, out Vector2<T> result) => result = Sin(vector);
-
+        
+        [GenerateReferenceFunction]
         public static Vector2<T> Negate(Vector2<T> vec) => new Vector2<T>(Scalar.Negate(vec.X), Scalar.Negate(vec.Y));
 
         public static Vector2<T> operator +(Vector2<T> left, Vector2<T> right) => Add(left, right);
