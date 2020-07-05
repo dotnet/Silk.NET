6d963ff73f279b7495fbd61c000870821bc21b29
RhysWhy <31206745+RhysWhy@users.noreply.github.com>
2020-07-05 15:23:45 +0100

    Update SixLabours.ImageSharp to the latest release candidate

--- examples/CSharp/Tutorial 1.6 - Co-ordinate Systems/Texture.cs
+++ examples/CSharp/Tutorial 1.6 - Co-ordinate Systems/Texture.cs
@@ -1,10 +1,9 @@
 using Silk.NET.OpenGL;
-using System;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
-using System.Runtime.InteropServices;
-using SixLabors.ImageSharp.Advanced;
 using SixLabors.ImageSharp.Processing;
+using System;
+using System.Runtime.InteropServices;
 
 namespace Tutorial
 {
@@ -18,7 +17,7 @@ namespace Tutorial
             Image<Rgba32> img = (Image<Rgba32>)Image.Load(path);
             img.Mutate(x => x.Flip(FlipMode.Vertical));
 
-            fixed (void* data = &MemoryMarshal.GetReference(img.GetPixelSpan()))
+            fixed (void* data = &MemoryMarshal.GetReference(img.GetPixelRowSpan(0)))
             {
                 Load(gl, data, (uint)img.Width, (uint)img.Height);
             }
