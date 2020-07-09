f453b9f127785058b3ec1850df061818191c6223
Dylan Perks <dmp9biz@gmail.com>
2020-07-09 23:02:52 +0100

    Update Vulkan bindings to ensure merged 1.0 bindings don't mix with 2.0

--- src/Vulkan/Silk.NET.Vulkan/Structs/DirectFBSurfaceCreateInfoEXT.gen.cs
+++ src/Vulkan/Silk.NET.Vulkan/Structs/DirectFBSurfaceCreateInfoEXT.gen.cs
@@ -8,12 +8,14 @@ using System;
 using System.Runtime.InteropServices;
 using System.Text;
 using Silk.NET.Core.Native;
+using Silk.NET.Core.Attributes;
 using Ultz.SuperInvoke;
 
 #pragma warning disable 1591
 
 namespace Silk.NET.Vulkan
 {
+    [NativeName("Name", "VkDirectFBSurfaceCreateInfoEXT")]
     public unsafe struct DirectFBSurfaceCreateInfoEXT
     {
         public DirectFBSurfaceCreateInfoEXT
@@ -33,14 +35,29 @@ namespace Silk.NET.Vulkan
         }
 
 /// <summary></summary>
+        [NativeName("Type", "VkStructureType")]
+        [NativeName("Type.Name", "VkStructureType")]
+        [NativeName("Name", "sType")]
         public StructureType SType;
 /// <summary></summary>
+        [NativeName("Type", "void*")]
+        [NativeName("Type.Name", "void")]
+        [NativeName("Name", "pNext")]
         public void* PNext;
 /// <summary></summary>
+        [NativeName("Type", "VkDirectFBSurfaceCreateFlagsEXT")]
+        [NativeName("Type.Name", "VkDirectFBSurfaceCreateFlagsEXT")]
+        [NativeName("Name", "flags")]
         public uint Flags;
 /// <summary></summary>
+        [NativeName("Type", "IDirectFB*")]
+        [NativeName("Type.Name", "IDirectFB")]
+        [NativeName("Name", "dfb")]
         public IntPtr Dfb;
 /// <summary></summary>
+        [NativeName("Type", "IDirectFBSurface*")]
+        [NativeName("Type.Name", "IDirectFBSurface")]
+        [NativeName("Name", "surface")]
         public IntPtr Surface;
     }
 }
