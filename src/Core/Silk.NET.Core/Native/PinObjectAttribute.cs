aba237f26731d2a663fbe12b7bde472f5d993d0a
Kai Jellinghaus <kai.jellinghaus1@gmail.com>
2020-08-15 23:20:04 +0200

    Allow PinObjectAttribute on return values

--- src/Core/Silk.NET.Core/Native/PinObjectAttribute.cs
+++ src/Core/Silk.NET.Core/Native/PinObjectAttribute.cs
@@ -7,7 +7,7 @@ using System;
 
 namespace Silk.NET.Core.Native
 {
-    [AttributeUsage(AttributeTargets.Parameter)]
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue)]
     public class PinObjectAttribute : Attribute
     {
         public PinObjectAttribute(PinMode mode = PinMode.Persist)
