49f17985e9009417d06d6f6a67f0261c6a251b68
Kai Jellinghaus <kai.jellinghaus1@gmail.com>
2020-09-06 20:33:29 +0200

    Register Windowing platforms with input platforms

--- src/Input/Silk.NET.Input.Glfw/GlfwInput.cs
+++ src/Input/Silk.NET.Input.Glfw/GlfwInput.cs
@@ -3,12 +3,16 @@
 // You may modify and distribute Silk.NET under the terms
 // of the MIT license. See the LICENSE file for details.
 
+using Silk.NET.Windowing;
+using Silk.NET.Windowing.Glfw;
+
 namespace Silk.NET.Input.Glfw
 {
     public static class GlfwInput
     {
         public static void RegisterPlatform()
         {
+            Window.Add(new GlfwPlatform());
             InputWindowExtensions.Add(new GlfwInputPlatform());
         }
     }
--- src/Input/Silk.NET.Input.Sdl/SdlInput.cs
+++ src/Input/Silk.NET.Input.Sdl/SdlInput.cs
@@ -3,12 +3,16 @@
 // You may modify and distribute Silk.NET under the terms
 // of the MIT license. See the LICENSE file for details.
 
+using Silk.NET.Windowing;
+using Silk.NET.Windowing.Sdl;
+
 namespace Silk.NET.Input.Sdl
 {
     public static class SdlInput
     {
         public static void RegisterWindow()
         {
+            Window.Add(new SdlPlatform());
             InputWindowExtensions.Add(new SdlInputPlatform());
         }
     }
