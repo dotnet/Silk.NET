b3ff73e00b1285dc5d2ac992a75d9e7657d566c1
Kai Jellinghaus <contact@kaij.tech>
2020-10-05 18:43:12 +0200

    Add Test struct to Lab project

--- src/Lab/GenericMaths/Program.cs
+++ src/Lab/GenericMaths/Program.cs
@@ -9,4 +9,18 @@ namespace GenericMaths
         {
         }
     }
+
+    public struct TypeSpecialization
+    {
+        public float Field;
+        public float Property { get; set; }
+
+        public float Method()
+        {
+            var a = Field + Property;
+            var b = 5 + 5;
+            var c = a + b;
+            return c * c;
+        }
+    }
 }
