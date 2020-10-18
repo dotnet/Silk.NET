fc39178c1ada955d6944903957d4866a3a456cf6
Dylan Perks <dmp9biz@gmail.com>
2020-10-18 16:29:38 +0100

    SilkMarshal 2.0 w/ GlobalMemory - a HGlobal/POH abstraction

---
+++ src/Core/Silk.NET.Core/Native/NativeStringEncoding.cs
@@ -0,0 +1,20 @@
+﻿// This file is part of Silk.NET.
+// 
+// You may modify and distribute Silk.NET under the terms
+// of the MIT license. See the LICENSE file for details.
+
+namespace Silk.NET.Core.Native
+{
+    public enum NativeStringEncoding
+    {
+        BStr,
+        LPStr,
+        LPTStr,
+        LPUTF8Str,
+        LPWStr,
+        Ansi = LPStr,
+        Auto = LPTStr,
+        Uni = LPWStr,
+        UTF8 = LPUTF8Str
+    }
+}
