a7010fe59fa049706d4ce9f90fb3dbfa0379f0e0
Kai Jellinghaus <contact@kaij.tech>
2020-11-27 13:55:42 +0100

    Cube & Rect

---
+++ src/Maths/src/Cube.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Silk.NET.Numerics
+{
+    /// <summary>
+    /// A structure representing a Cube with an <see cref="Origin"/> and <see cref="Size"/>
+    /// </summary>
+    public struct Cube<T>
+        : IEquatable<Cube<T>>
+        where T : unmanaged, IFormattable, IEquatable<T>, IComparable<T>
+    {
+        /// <summary>
+        /// The origin.
+        /// </summary>
+        public Vector3<T> Origin;
+        /// <summary>
+        /// The size.
+        /// </summary>
+        public Vector3<T> Size;
+
+        /// <summary>
+        /// Constructs a Cube from an origin and it's size
+        /// </summary>
+        /// <param name="origin">The origin of the cube.</param>
+        /// <param name="size">The size of the cube.</param>
+        public Cube(Vector3<T> origin, Vector3<T> size)
+        {
+            Origin = origin;
+            Size = size;
+        }
+
+        /// <summary>Returns a boolean indicating whether the given Cube is equal to this Cube instance.</summary>
+        /// <param name="other">The Cube to compare this instance to.</param>
+        /// <returns>True if the other Cube is equal to this instance; False otherwise.</returns>
+        public bool Equals(Cube<T> other)
+        {
+            return Origin.Equals(other.Origin) && Size.Equals(other.Size);
+        }
+
+        /// <summary>Returns a boolean indicating whether the given Object is equal to this Cube instance.</summary>
+        /// <param name="obj">The Object to compare against.</param>
+        /// <returns>True if the Object is equal to this Cube; False otherwise.</returns>
+        public override bool Equals(object? obj)
+        {
+            return obj is Cube<T> other && Equals(other);
+        }
+
+        /// <summary>Returns the hash code for this instance.</summary>
+        /// <returns>The hash code.</returns>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Origin, Size);
+        }
+
+        /// <summary>Returns a boolean indicating whether the two given Cubes are equal.</summary>
+        /// <param name="value1">The first Cube to compare.</param>
+        /// <param name="value2">The second Cube to compare.</param>
+        /// <returns>True if the Cubes are equal; False otherwise.</returns>
+        public static bool operator ==(Cube<T> value1, Cube<T> value2)
+        {
+            return value1.Equals(value2);
+        }
+
+        /// <summary>Returns a boolean indicating whether the two given Cubes are not equal.</summary>
+        /// <param name="value1">The first Cube to compare.</param>
+        /// <param name="value2">The second Cube to compare.</param>
+        /// <returns>True if the Cubes are not equal; False if they are equal.</returns>
+        public static bool operator !=(Cube<T> value1, Cube<T> value2)
+        {
+            return !value1.Equals(value2);
+        }
+    }
+}
---
+++ src/Maths/src/Rect.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Silk.NET.Numerics
+{
+    /// <summary>
+    /// A structure representing a Rectangle with an <see cref="Origin"/> and <see cref="Size"/>
+    /// </summary>
+    public struct Rect<T>
+        : IEquatable<Rect<T>>
+        where T : unmanaged, IFormattable, IEquatable<T>, IComparable<T>
+    {
+        /// <summary>
+        /// The origin.
+        /// </summary>
+        public Vector2<T> Origin;
+        /// <summary>
+        /// The size.
+        /// </summary>
+        public Vector2<T> Size;
+        
+        /// <summary>
+        /// Constructs a Rect from an origin and an size
+        /// </summary>
+        /// <param name="origin">The origin of the rect.</param>
+        /// <param name="size">The size of the rect.</param>
+        public Rect(Vector2<T> origin, Vector2<T> size)
+        {
+            Origin = origin;
+            Size = size;
+        }
+
+        /// <summary>Returns a boolean indicating whether the given Rect is equal to this Rect instance.</summary>
+        /// <param name="other">The Rect to compare this instance to.</param>
+        /// <returns>True if the other Rect is equal to this instance; False otherwise.</returns>
+        public bool Equals(Rect<T> other)
+        {
+            return Origin.Equals(other.Origin) && Size.Equals(other.Size);
+        }
+
+        /// <summary>Returns a boolean indicating whether the given Object is equal to this Rect instance.</summary>
+        /// <param name="obj">The Object to compare against.</param>
+        /// <returns>True if the Object is equal to this Rect; False otherwise.</returns>
+        public override bool Equals(object? obj)
+        {
+            return obj is Rect<T> other && Equals(other);
+        }
+
+        /// <summary>Returns the hash code for this instance.</summary>
+        /// <returns>The hash code.</returns>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Origin, Size);
+        }
+
+        /// <summary>Returns a boolean indicating whether the two given Rectangles are equal.</summary>
+        /// <param name="value1">The first Rect to compare.</param>
+        /// <param name="value2">The second Rect to compare.</param>
+        /// <returns>True if the Rectangles are equal; False otherwise.</returns>
+        public static bool operator ==(Rect<T> value1, Rect<T> value2)
+        {
+            return value1.Equals(value2);
+        }
+
+        /// <summary>Returns a boolean indicating whether the two given Rectangles are not equal.</summary>
+        /// <param name="value1">The first Rect to compare.</param>
+        /// <param name="value2">The second Rect to compare.</param>
+        /// <returns>True if the Rectangles are not equal; False if they are equal.</returns>
+        public static bool operator !=(Rect<T> value1, Rect<T> value2)
+        {
+            return !value1.Equals(value2);
+        }
+    }
+}
