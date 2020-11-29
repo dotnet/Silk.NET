5aa2acd828caa2e5aea584ae473ba7d48f858c67
Kai Jellinghaus <contact@kaij.tech>
2020-11-29 16:51:25 +0100

    Rect -> Rectangle

--- src/Maths/src/Rectangle.cs
+++ src/Maths/src/Rectangle.cs
@@ -5,8 +5,8 @@ namespace Silk.NET.Maths
     /// <summary>
     /// A structure representing a Rectangle with an <see cref="Origin"/> and <see cref="Size"/>
     /// </summary>
-    public struct Rect<T>
-        : IEquatable<Rect<T>>
+    public struct Rectangle<T>
+        : IEquatable<Rectangle<T>>
         where T : unmanaged, IFormattable, IEquatable<T>, IComparable<T>
     {
         /// <summary>
@@ -19,30 +19,30 @@ namespace Silk.NET.Maths
         public Vector2<T> Size;
         
         /// <summary>
-        /// Constructs a Rect from an origin and an size
+        /// Constructs a Rectangle from an origin and an size
         /// </summary>
         /// <param name="origin">The origin of the rect.</param>
         /// <param name="size">The size of the rect.</param>
-        public Rect(Vector2<T> origin, Vector2<T> size)
+        public Rectangle(Vector2<T> origin, Vector2<T> size)
         {
             Origin = origin;
             Size = size;
         }
 
-        /// <summary>Returns a boolean indicating whether the given Rect is equal to this Rect instance.</summary>
-        /// <param name="other">The Rect to compare this instance to.</param>
-        /// <returns>True if the other Rect is equal to this instance; False otherwise.</returns>
-        public bool Equals(Rect<T> other)
+        /// <summary>Returns a boolean indicating whether the given Rectangle is equal to this Rectangle instance.</summary>
+        /// <param name="other">The Rectangle to compare this instance to.</param>
+        /// <returns>True if the other Rectangle is equal to this instance; False otherwise.</returns>
+        public bool Equals(Rectangle<T> other)
         {
             return Origin.Equals(other.Origin) && Size.Equals(other.Size);
         }
 
-        /// <summary>Returns a boolean indicating whether the given Object is equal to this Rect instance.</summary>
+        /// <summary>Returns a boolean indicating whether the given Object is equal to this Rectangle instance.</summary>
         /// <param name="obj">The Object to compare against.</param>
-        /// <returns>True if the Object is equal to this Rect; False otherwise.</returns>
+        /// <returns>True if the Object is equal to this Rectangle; False otherwise.</returns>
         public override bool Equals(object? obj)
         {
-            return obj is Rect<T> other && Equals(other);
+            return obj is Rectangle<T> other && Equals(other);
         }
 
         /// <summary>Returns the hash code for this instance.</summary>
@@ -53,19 +53,19 @@ namespace Silk.NET.Maths
         }
 
         /// <summary>Returns a boolean indicating whether the two given Rectangles are equal.</summary>
-        /// <param name="value1">The first Rect to compare.</param>
-        /// <param name="value2">The second Rect to compare.</param>
+        /// <param name="value1">The first Rectangle to compare.</param>
+        /// <param name="value2">The second Rectangle to compare.</param>
         /// <returns>True if the Rectangles are equal; False otherwise.</returns>
-        public static bool operator ==(Rect<T> value1, Rect<T> value2)
+        public static bool operator ==(Rectangle<T> value1, Rectangle<T> value2)
         {
             return value1.Equals(value2);
         }
 
         /// <summary>Returns a boolean indicating whether the two given Rectangles are not equal.</summary>
-        /// <param name="value1">The first Rect to compare.</param>
-        /// <param name="value2">The second Rect to compare.</param>
+        /// <param name="value1">The first Rectangle to compare.</param>
+        /// <param name="value2">The second Rectangle to compare.</param>
         /// <returns>True if the Rectangles are not equal; False if they are equal.</returns>
-        public static bool operator !=(Rect<T> value1, Rect<T> value2)
+        public static bool operator !=(Rectangle<T> value1, Rectangle<T> value2)
         {
             return !value1.Equals(value2);
         }
