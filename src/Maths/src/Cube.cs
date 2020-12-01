d2c8b10491319675a7e22ab104636ebfbaf1bc78
Kai Jellinghaus <contact@kaij.tech>
2020-12-01 18:57:20 +0100

    Cube & Rectangle API

--- src/Maths/src/Cube.cs
+++ src/Maths/src/Cube.cs
@@ -28,6 +28,104 @@ namespace Silk.NET.Maths
             Origin = origin;
             Size = size;
         }
+        
+        /// <summary>
+        /// The center of this cube.
+        /// </summary>
+        public Vector3<T> Center
+        {
+            get => Origin + HalfSize;
+        }
+
+        /// <summary>
+        /// The Maximum point of this cube.
+        /// </summary>
+        public Vector3<T> Max => Origin + Size;
+
+        /// <summary>
+        /// Half the size of this cube.
+        /// </summary>
+        public Vector3<T> HalfSize => Size / Scalar<T>.Two;
+
+        /// <summary>
+        /// Calculates whether this cube contains a point.
+        /// </summary>
+        /// <param name="point">The point.</param>
+        /// <returns>True if this cube contains the point; False otherwise.</returns>
+        /// <remarks>This does consider a point on the edge contained.</remarks>
+        public bool Contains(Vector3<T> point)
+        {
+            var max = Max;
+            return Scalar.GreaterThanOrEqual(point.X, Origin.X) && Scalar.GreaterThanOrEqual
+                (point.Y, Origin.Y) && Scalar.GreaterThanOrEqual(point.Z, Origin.Z) && Scalar.LessThanOrEqual
+                (point.X, max.X) && Scalar.LessThanOrEqual(point.Y, max.Y) && Scalar.LessThanOrEqual(point.Z, max.Z);
+        }
+
+        /// <summary>
+        /// Calculates whether this cube contains another rectangle
+        /// </summary>
+        /// <param name="other">The rectangle.</param>
+        /// <returns>True if this cube contains the given cube; False otherwise.</returns>
+        /// <remarks>This does consider a cube that touches the edge contained.</remarks>
+        public bool Contains(Cube<T> other)
+        {
+            var tMax = this.Max;
+            var oMax = other.Max;
+            return Scalar.GreaterThanOrEqual(other.Origin.X, this.Origin.X) && Scalar.GreaterThanOrEqual
+                (other.Origin.Y, this.Origin.Y) && Scalar.GreaterThanOrEqual
+                (other.Origin.Z, this.Origin.Z) && Scalar.LessThanOrEqual(oMax.X, tMax.X) && Scalar.LessThanOrEqual
+                (oMax.Y, tMax.Y) && Scalar.GreaterThanOrEqual(oMax.Y, tMax.Y);
+        }
+
+        /// <summary>
+        /// Calculates the distance to the nearest edge from the point.
+        /// </summary>
+        /// <param name="point">The point.</param>
+        /// <returns>The distance.</returns>
+        public T GetDistanceToNearestEdge(Vector3<T> point)
+        {
+            var max = Max;
+            var dx = Scalar.Max(Scalar.Max(Scalar.Subtract(Origin.X, point.X), Scalar<T>.Zero), Scalar.Subtract(point.X, max.X));
+            var dy = Scalar.Max(Scalar.Max(Scalar.Subtract(Origin.Y, point.Y), Scalar<T>.Zero), Scalar.Subtract(point.Y, max.Y));
+            var dz = Scalar.Max(Scalar.Max(Scalar.Subtract(Origin.Z, point.Z), Scalar<T>.Zero), Scalar.Subtract(point.Z, max.Z));
+            return Scalar.Sqrt(Scalar.Add(Scalar.Add(Scalar.Multiply(dx, dx), Scalar.Multiply(dy, dy)), Scalar.Multiply(dz, dz)));
+        }
+
+        /// <summary>
+        /// Calculates a new cube translated by a given distance.
+        /// </summary>
+        /// <param name="distance">The distance.</param>
+        /// <returns>The calculated cube.</returns>
+        public Cube<T> GetTranslated(Vector3<T> distance)
+        {
+            return new(Origin + distance, Size);
+        }
+
+        /// <summary>
+        /// Calculates a new cube scaled by the given scale around the given anchor.
+        /// </summary>
+        /// <param name="scale">The scale.</param>
+        /// <param name="anchor">The anchor.</param>
+        /// <returns>The calculated cube.</returns>
+        public Cube<T> GetScaled(Vector3<T> scale, Vector3<T> anchor)
+        {
+            var origMax = Max;
+            var min = (scale * (Origin - anchor)) + Origin;
+            var max = (scale * (origMax - anchor)) + origMax;
+            return new(min, max - min);
+        }
+
+        /// <summary>
+        /// Calculates a cube inflated to contain the given point.
+        /// </summary>
+        /// <param name="point">The point.</param>
+        /// <returns>The cube rectangle.</returns>
+        public Cube<T> GetInflated(Vector3<T> point)
+        {
+            var min = Vector3<T>.Min(Origin, point);
+            var max = Vector3<T>.Max(Max, point);
+            return new(min, max - min);
+        }
 
         /// <summary>Returns a boolean indicating whether the given Cube is equal to this Cube instance.</summary>
         /// <param name="other">The Cube to compare this instance to.</param>
--- src/Maths/src/Rectangle.cs
+++ src/Maths/src/Rectangle.cs
@@ -28,6 +28,101 @@ namespace Silk.NET.Maths
             Origin = origin;
             Size = size;
         }
+        
+        /// <summary>
+        /// The center of this rectangle.
+        /// </summary>
+        public Vector2<T> Center
+        {
+            get => Origin + HalfSize;
+        }
+
+        /// <summary>
+        /// The Maximum point of this Rectangle.
+        /// </summary>
+        public Vector2<T> Max => Origin + Size;
+
+        /// <summary>
+        /// Half the size of this rectangle.
+        /// </summary>
+        public Vector2<T> HalfSize => Size / Scalar<T>.Two;
+
+        /// <summary>
+        /// Calculates whether this rectangle contains a point.
+        /// </summary>
+        /// <param name="point">The point.</param>
+        /// <returns>True if this rectangle contains the point; False otherwise.</returns>
+        /// <remarks>This does consider a point on the edge contained.</remarks>
+        public bool Contains(Vector2<T> point)
+        {
+            var max = Max;
+            return Scalar.GreaterThanOrEqual(point.X, Origin.X) && Scalar.GreaterThanOrEqual
+                (point.Y, Origin.Y) && Scalar.LessThanOrEqual(point.X, max.X) && Scalar.LessThanOrEqual(point.Y, max.Y);
+        }
+
+        /// <summary>
+        /// Calculates whether this rectangle contains another rectangle
+        /// </summary>
+        /// <param name="other">The rectangle.</param>
+        /// <returns>True if this rectangle contains the given rectangle; False otherwise.</returns>
+        /// <remarks>This does consider a rectangle that touches the edge contained.</remarks>
+        public bool Contains(Rectangle<T> other)
+        {
+            var tMax = this.Max;
+            var oMax = other.Max;
+            return Scalar.GreaterThanOrEqual(other.Origin.X, this.Origin.X) && Scalar.GreaterThanOrEqual
+                (other.Origin.Y, this.Origin.Y) && Scalar.LessThanOrEqual(oMax.X, tMax.X) && Scalar.LessThanOrEqual
+                (oMax.Y, tMax.Y);
+        }
+
+        /// <summary>
+        /// Calculates the distance to the nearest edge from the point.
+        /// </summary>
+        /// <param name="point">The point.</param>
+        /// <returns>The distance.</returns>
+        public T GetDistanceToNearestEdge(Vector2<T> point)
+        {
+            var max = Max;
+            var dx = Scalar.Max(Scalar.Max(Scalar.Subtract(Origin.X, point.X), Scalar<T>.Zero), Scalar.Subtract(point.X, max.X));
+            var dy = Scalar.Max(Scalar.Max(Scalar.Subtract(Origin.Y, point.Y), Scalar<T>.Zero), Scalar.Subtract(point.Y, max.Y));
+            return Scalar.Sqrt(Scalar.Add(Scalar.Multiply(dx, dx), Scalar.Multiply(dy, dy)));
+        }
+
+        /// <summary>
+        /// Calculates a new rectangle translated by a given distance.
+        /// </summary>
+        /// <param name="distance">The distance.</param>
+        /// <returns>The calculated rectangle.</returns>
+        public Rectangle<T> GetTranslated(Vector2<T> distance)
+        {
+            return new(Origin + distance, Size);
+        }
+
+        /// <summary>
+        /// Calculates a new rectangle scaled by the given scale around the given anchor.
+        /// </summary>
+        /// <param name="scale">The scale.</param>
+        /// <param name="anchor">The anchor.</param>
+        /// <returns>The calculated rectangle.</returns>
+        public Rectangle<T> GetScaled(Vector2<T> scale, Vector2<T> anchor)
+        {
+            var origMax = Max;
+            var min = (scale * (Origin - anchor)) + Origin;
+            var max = (scale * (origMax - anchor)) + origMax;
+            return new(min, max - min);
+        }
+
+        /// <summary>
+        /// Calculates a rectangle inflated to contain the given point.
+        /// </summary>
+        /// <param name="point">The point.</param>
+        /// <returns>The calculated rectangle.</returns>
+        public Rectangle<T> GetInflated(Vector2<T> point)
+        {
+            var min = Vector2<T>.Min(Origin, point);
+            var max = Vector2<T>.Max(Max, point);
+            return new(min, max - min);
+        }
 
         /// <summary>Returns a boolean indicating whether the given Rectangle is equal to this Rectangle instance.</summary>
         /// <param name="other">The Rectangle to compare this instance to.</param>
