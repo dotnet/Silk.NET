bbedea0deffc77d894ef3c4f9455fb95d822335a
Kai Jellinghaus <contact@kaij.tech>
2021-05-06 12:50:47 +0200

    Rework Benchmarks

---
+++ src/Lab/Benchmarks/InvokeBenchmarks/StringBenchmark.cs
@@ -0,0 +1,106 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Order;
+using Silk.NET.Core.Contexts;
+
+namespace InvokeBenchmarks
+{
+    [CategoriesColumn]
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+    public class StringBenchmark
+    {
+        private TestContext _testContext;
+        private SilkAPI _silkAPI;
+        
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _testContext = new TestContext();
+            _silkAPI = new SilkAPI(_testContext);
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            _testContext.Dispose();
+            _silkAPI.Dispose();
+        }
+
+        [Benchmark]
+        [BenchmarkCategory("BStr")]
+        public int WithReturnAndBStrMarshalSilk()
+        {
+            return _silkAPI.WithReturnAndBStrMarshal("Hello World!");
+        }
+        
+        [Benchmark]
+        [BenchmarkCategory("LPStr")]
+        public int WithReturnAndLPStrMarshalSilk()
+        {
+            return _silkAPI.WithReturnAndLPStrMarshal("Hello World!");
+        }
+        
+        [Benchmark]
+        [BenchmarkCategory("LPTStr")]
+        public int WithReturnAndLPTStrMarshalSilk()
+        {
+            return _silkAPI.WithReturnAndLPTStrMarshal("Hello World!");
+        }
+        
+        [Benchmark]
+        [BenchmarkCategory("LPWStr")]
+        public int WithReturnAndLPWStrMarshalSilk()
+        {
+            return _silkAPI.WithReturnAndLPWStrMarshal("Hello World!");
+        }
+        
+        [Benchmark]
+        [BenchmarkCategory("LPUTF8Str")]
+        public int WithReturnAndLPUTF8StrMarshalSilk()
+        {
+            return _silkAPI.WithReturnAndLPUTF8StrMarshal("Hello World!");
+        }
+        
+        [Benchmark(Baseline = true)]
+        [BenchmarkCategory("BStr")]
+        public int WithReturnAndBStrMarshalPInvoke()
+        {
+            return PInvoke.WithReturnAndBStrMarshal("Hello World!");
+        }
+        
+        [Benchmark(Baseline = true)]
+        [BenchmarkCategory("LPStr")]
+        public int WithReturnAndLPStrMarshalPInvoke()
+        {
+            return PInvoke.WithReturnAndLPStrMarshal("Hello World!");
+        }
+        
+        [Benchmark(Baseline = true)]
+        [BenchmarkCategory("LPTStr")]
+        public int WithReturnAndLPTStrMarshalPInvoke()
+        {
+            return PInvoke.WithReturnAndLPTStrMarshal("Hello World!");
+        }
+        
+        [Benchmark(Baseline = true)]
+        [BenchmarkCategory("LPWStr")]
+        public int WithReturnAndLPWStrMarshalPInvoke()
+        {
+            return PInvoke.WithReturnAndLPWStrMarshal("Hello World!");
+        }
+        
+        [Benchmark(Baseline = true)]
+        [BenchmarkCategory("LPUTF8Str")]
+        public int WithReturnAndLPUTF8StrMarshalPInvoke()
+        {
+            return PInvoke.WithReturnAndLPUTF8StrMarshal("Hello World!");
+        }
+    }
+}
--- src/Lab/Benchmarks/InvokeBenchmarks/TestContext.cs
+++ src/Lab/Benchmarks/InvokeBenchmarks/TestContext.cs
@@ -1,96 +1,39 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Silk.NET.Core.Contexts;
+using Silk.NET.Core.Loader;
 
 namespace InvokeBenchmarks
 {
     public sealed class TestContext : INativeContext
     {
+        private readonly UnmanagedLibrary _lib;
+
+        public TestContext()
+        {
+            _lib = new UnmanagedLibrary("TestLib.dll");
+        }
+
         public void Dispose()
-        { }
-
-        [UnmanagedCallersOnly(CallConvs = new[] {typeof(CallConvCdecl)})]
-        private static void RawInvoke() { }
-
-        [UnmanagedCallersOnly(CallConvs = new[] {typeof(CallConvCdecl)})]
-        private static int WithReturn()
         {
-            return 5;
+            _lib.Dispose();
         }
-
-        [UnmanagedCallersOnly(CallConvs = new[] {typeof(CallConvCdecl)})]
-        private static unsafe int WithReturnAndStringMarshal(byte* bytes)
-        {
-            return *bytes;
-        }
-
-        [UnmanagedCallersOnly(CallConvs = new[] {typeof(CallConvCdecl)})]
-        private static unsafe int WithReturnAndMultiStringMarshal(byte* bytes, byte* bytes2)
-        {
-            return *bytes + *bytes2;
-        }
-
-        [UnmanagedCallersOnly(CallConvs = new[] {typeof(CallConvCdecl)})]
-        private static unsafe int WithReturnAndSpanMarshal(byte* bytes)
-        {
-            return *bytes;
-        }
-
-        [UnmanagedCallersOnly(CallConvs = new[] {typeof(CallConvCdecl)})]
-        private static unsafe int WithReturnAndBoolMarshal(short b)
-        {
-            return b;
-        }
-
+        
         public nint GetProcAddress(string proc, int? slot = default)
         {
             TryGetProcAddress(proc, out var v, slot);
             return v;
         }
 
-        public unsafe bool TryGetProcAddress(string proc, out nint addr, int? slot = default)
+        public bool TryGetProcAddress(string proc, out nint addr, int? slot = default)
         {
-            if (proc == "Test1")
-            {
-                addr = (nint) (delegate* unmanaged[Cdecl]<void>) (&RawInvoke);
-                return true;
-            }
-
-            if (proc == "Test2")
-            {
-                addr = (nint) (delegate* unmanaged[Cdecl]<int>) (&WithReturn);
-                return true;
-            }
-
-            if (proc == "Test3")
-            {
-                addr = (nint) (delegate* unmanaged[Cdecl]<byte*, int>) (&WithReturnAndStringMarshal);
-                return true;
-            }
-
-            if (proc == "Test4")
-            {
-                addr = (nint) (delegate* unmanaged[Cdecl]<byte*, byte*, int>) (&WithReturnAndMultiStringMarshal);
-                return true;
-            }
-
-            if (proc == "Test5")
-            {
-                addr = (nint) (delegate* unmanaged[Cdecl]<byte*, int>) (&WithReturnAndSpanMarshal);
-                return true;
-            }
-
-            if (proc == "Test6")
-            {
-                addr = (nint) (delegate* unmanaged[Cdecl]<short, int>) (&WithReturnAndBoolMarshal);
-                return true;
-            }
-
-            addr = 0;
-            return false;
+            addr = _lib.LoadFunction(proc);
+            return addr != 0;
         }
     }
 }
