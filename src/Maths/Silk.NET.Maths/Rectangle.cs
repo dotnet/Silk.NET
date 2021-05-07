1a1e0b610fd8f80490dfefc10c435e3e0838785e
Kai Jellinghaus <contact@kaij.tech>
2021-05-07 20:40:17 +0200

    Fixes for Box/Rectangle/Cube (#485)
    * Fix GetScaled behavior
    
    * Add GetScaled overloads with TScale

--- src/Maths/Silk.NET.Maths/Rectangle.cs
+++ src/Maths/Silk.NET.Maths/Rectangle.cs
@@ -111,10 +111,25 @@ namespace Silk.NET.Maths
         /// <returns>The calculated rectangle.</returns>
         public Rectangle<T> GetScaled(Vector2D<T> scale, Vector2D<T> anchor)
         {
-            var origMax = Max;
-            var min = (scale * (Origin - anchor)) + Origin;
-            var max = (scale * (origMax - anchor)) + origMax;
+            var min = (scale * (Origin - anchor)) + anchor;
+            var max = (scale * (Max - anchor)) + anchor;
             return new(min, max - min);
+        }
+        
+        /// <summary>
+        /// Calculates a new rectangle scaled by the given scale around the given anchor.
+        /// </summary>
+        /// <typeparam name="TScale">The type of the scale.</typeparam>
+        /// <param name="scale">The scale.</param>
+        /// <param name="anchor">The anchor.</param>
+        /// <returns>The calculated rectangle.</returns>
+        public Rectangle<T> GetScaled<TScale>(Vector2D<TScale> scale, Vector2D<T> anchor)
+            where TScale : unmanaged, IFormattable, IEquatable<TScale>, IComparable<TScale>
+        {
+            var convertedAnchor = anchor.As<TScale>();
+            var min = (scale * (Origin.As<TScale>() - convertedAnchor)) + convertedAnchor;
+            var max = (scale * (Max.As<TScale>() - convertedAnchor)) + convertedAnchor;
+            return new(min.As<T>(), (max - min).As<T>());
         }
 
         /// <summary>
