140a74545dc658243c625770bb137b8b50dd0711
Matthias Koch <ithrowexceptions@gmail.com>
2021-05-18 13:48:44 +0200

    Add documentation action

---
+++ src/ReSharper/src/rider/main/kotlin/com/jetbrains/rider/plugins/silknet/actions/SilkNetOpenDocumentationAction.kt
@@ -0,0 +1,10 @@
+package com.jetbrains.rider.plugins.silknet.actions
+
+import com.jetbrains.rider.actions.base.RiderAnAction
+import icons.ReSharperIcons
+
+class SilkNetOpenDocumentationAction : RiderAnAction(
+        "SilkNetOpenDocumentationAction",
+        "Open Documentation",
+        null,
+    ReSharperIcons.Browsers.BrowserSystemDefault)
