256e48ac65b3497a06c31a577f7e1ae6c1ee3e20
roeyskoe <26493216+roeyskoe@users.noreply.github.com>
2021-08-10 21:17:55 +0300

    Fix calling SetWindowShouldClose if _glfwWindow is null (#568)

--- src/Windowing/Silk.NET.Windowing.Glfw/GlfwWindow.cs
+++ src/Windowing/Silk.NET.Windowing.Glfw/GlfwWindow.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
@@ -246,7 +246,13 @@ namespace Silk.NET.Windowing.Glfw
 
         protected override bool IsClosingSettable
         {
-            set => _glfw.SetWindowShouldClose(_glfwWindow, value);
+            set
+            {
+                if (_glfwWindow != null)
+                {
+                    _glfw.SetWindowShouldClose(_glfwWindow, value);
+                }
+            }
         }
 
         protected override Vector2D<int> SizeSettable
