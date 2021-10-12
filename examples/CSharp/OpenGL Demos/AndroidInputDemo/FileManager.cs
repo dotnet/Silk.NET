9e5bcdb806371da1e8cc5aa6a5a4d121d9acc2ab
Mikko Röyskö <mikko.roysko@gmail.com>
2021-10-12 20:53:59 +0300

    GLES demos

---
+++ examples/CSharp/OpenGL Demos/AndroidInputDemo/FileManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Content.Res;
+
+namespace AndroidInputDemo
+{
+    /// <summary>
+    /// Helper to assist with file loading.
+    /// </summary>
+    public static class FileManager
+    {
+        internal static AssetManager AssetManager;
+
+        public static string LoadString(string path)
+        {
+            using StreamReader s = new StreamReader(AssetManager.Open(path));
+            return s.ReadToEnd();
+        }
+
+        public static Stream OpenStream(string path)
+        {
+            return AssetManager.Open(path);
+        }
+    }
+}
