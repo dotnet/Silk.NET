9fa7696f8688aa7692a4be015c7e063eda82afb1
Dylan Perks <dmp9biz@gmail.com>
2021-11-12 23:32:17 +0000

    Theme switcher

--- website/_theme/lib/silk.js
+++ website/_theme/lib/silk.js
@@ -4,3 +4,28 @@ if (headrooms != null && headrooms.length > 0) {
 }
 
 hljs.highlightAll();
+
+function setTheme(theme) {
+    localStorage.setItem('theme', theme);
+    $("body").attr("class", "silk-" + theme);
+}
+
+$("#lighton").click(function() {
+    setTheme("light");
+});
+
+$("#lightoff").click(function() {
+    setTheme("dark");
+});
+
+(function () {
+    if (localStorage.getItem("theme") === "dark") {
+        setTheme("dark");
+    } else if (localStorage.getItem("theme") === "light") {
+        setTheme("light");
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        setTheme("dark");
+    } else {
+        setTheme("light");
+    }
+ })();
