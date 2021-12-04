cb5b510c8c6aa92840c9798eaf178143d924cf39
roeyskoe <26493216+roeyskoe@users.noreply.github.com>
2021-12-04 19:55:24 +0200

    Add Android-OpenGLES samples (#629)
    * GLES demos
    
    * Fix duplicate project in sln
    
    * Finishing samples
    
    * new silk logo
    
    * Remove img.Mutate like was done with the other examples
    
    * Only reference actually needed projects
    
    * Some comments
    
    * Sln files are fun
    
    * It would be nice to be able to see this error locally
    
    * running with nuke helps :D
    
    * Visual Studio lied to me

---
+++ examples/CSharp/OpenGL Demos/AndroidDemo/VertexArrayObject.cs
@@ -0,0 +1,42 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using Silk.NET.OpenGL;
+
+namespace AndroidDemo
+{
+    public class VertexArrayObject<TVertexType, TIndexType> : IDisposable
+        where TVertexType : unmanaged
+        where TIndexType : unmanaged
+    {
+        private uint _handle;
+        private GL _gl;
+
+        public VertexArrayObject(GL gl, BufferObject<TVertexType> vbo, BufferObject<TIndexType> ebo)
+        {
+            _gl = gl;
+
+            _handle = _gl.GenVertexArray();
+            Bind();
+            vbo.Bind();
+            ebo.Bind();
+        }
+
+        public unsafe void VertexAttributePointer(uint index, int count, VertexAttribPointerType type, uint vertexSize, int offSet)
+        {
+            _gl.VertexAttribPointer(index, count, type, false, vertexSize * (uint) sizeof(TVertexType), (void*) (offSet * sizeof(TVertexType)));
+            _gl.EnableVertexAttribArray(index);
+        }
+
+        public void Bind()
+        {
+            _gl.BindVertexArray(_handle);
+        }
+
+        public void Dispose()
+        {
+            _gl.DeleteVertexArray(_handle);
+        }
+    }
+}
