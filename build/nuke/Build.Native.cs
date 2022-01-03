c1f4d30bfc010a055e62f3401e87e522fdf220bf
Dylan Perks <dmp9biz@gmail.com>
2022-01-03 22:01:30 +0000

    This pipeline _will_ fail, but want to see how far it will get

--- build/nuke/Build.Native.cs
+++ build/nuke/Build.Native.cs
@@ -9,16 +9,32 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using JetBrains.Annotations;
 using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.Git;
+using Octokit;
+using Octokit.Internal;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tooling.ProcessTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.Git.GitTasks;
+using static Nuke.Common.Tools.GitHub.GitHubTasks;
 
+[GitHubActions
+(
+    "SwiftShader", GitHubActionsImage.UbuntuLatest, GitHubActionsImage.WindowsLatest, GitHubActionsImage.MacOsLatest,
+    ImportSecrets = new[] { "PUSHABLE_GITHUB_TOKEN" },
+    OnPushIncludePaths = new[] { "build/submodules/SwiftShader" },
+    InvokedTargets = new[] { nameof(SwiftShader) },
+    AutoGenerate = true,
+    PublishArtifacts = true
+)]
 partial class Build
 {
-    [Parameter("Build native code")] readonly bool Native;
+    [Nuke.Common.Parameter("Build native code")] readonly bool Native;
 
     [CanBeNull] string AndroidHomeValue;
 
@@ -96,6 +112,101 @@ partial class Build
                         FileExistsPolicy.Overwrite
                     );
                     return ret;
+                }
+            )
+    );
+
+    AbsolutePath SwiftShaderBuildPath => RootDirectory / "build" / "submodules" / "SwiftShader" / "build";
+    Target SwiftShader => CommonTarget
+    (
+        x => x.Before(Compile)
+            .After(Clean)
+            .Executes
+            (
+                () =>
+                {
+                    var sysName = OperatingSystem.IsLinux() ? "Linux" :
+                        OperatingSystem.IsWindows() ? "Windows" :
+                        OperatingSystem.IsMacOS() ? "Darwin" : throw new PlatformNotSupportedException();
+                    DeleteDirectory(SwiftShaderBuildPath);
+                    Git("checkout HEAD build/", SwiftShaderBuildPath / "..");
+                    StartProcess("cmake", ".. -DCMAKE_BUILD_TYPE=MinSizeRel", SwiftShaderBuildPath)
+                        .AssertZeroExitCode();
+                    StartProcess("cmake", "--build . --parallel --config Release", SwiftShaderBuildPath)
+                        .AssertWaitForExit(); // might fail... as long as the output exists we're happy
+                    var fname = sysName switch
+                    {
+                        "Linux" => "libvk_swiftshader.so",
+                        "Windows" => "vk_swiftshader.dll",
+                        "Darwin" => "libvk_swiftshader.dylib",
+                        _ => throw new("what")
+                    };
+
+                    var runtimes = RootDirectory / "src" / "Native" / "Silk.NET.Vulkan.SwiftShader.Native" / "runtimes";
+                    var outputPath = SwiftShaderBuildPath / sysName;
+                    const string icd = "vk_swiftshader_icd.json";
+                    if (OperatingSystem.IsWindows())
+                    {
+                        CopyFile(outputPath / fname, runtimes / "win-x64" / "native" / fname); // we'll use WOW64
+                        CopyFile(outputPath / fname, runtimes / "win-x86" / "native" / fname);
+                        CopyFile(outputPath / icd, runtimes / "win-x64" / "native" / icd);
+                        CopyFile(outputPath / icd, runtimes / "win-x86" / "native" / icd);
+                    }
+                    else if (OperatingSystem.IsLinux())
+                    {
+                        CopyFile(outputPath / fname, runtimes / "linux-x64" / "native" / fname);
+                        CopyFile(outputPath / icd, runtimes / "linux-x64" / "native" / icd);
+                    }
+                    else if (OperatingSystem.IsMacOS())
+                    {
+                        CopyFile(outputPath / fname, runtimes / "osx-x64" / "native" / fname);
+                        CopyFile(outputPath / icd, runtimes / "osx-x64" / "native" / icd);
+                    }
+
+                    var pushableToken = EnvironmentInfo.GetVariable<string>("PUSHABLE_GITHUB_TOKEN");
+                    var curBranch = GitCurrentBranch(RootDirectory);
+                    if (GitHubActions.Instance.GitHubRepository == "dotnet/Silk.NET" &&
+                        !string.IsNullOrWhiteSpace(pushableToken) &&
+                        curBranch != "HEAD" &&
+                        !string.IsNullOrWhiteSpace(curBranch))
+                    {
+                        Git
+                        (
+                            $"remote set-url origin https://silkdotnet:{pushableToken}@github.com/dotnet/Silk.NET",
+                            RootDirectory
+                        );
+                        Git("fetch --all", RootDirectory);
+                        Git("add src/Native", RootDirectory);
+                        Logger.Info("Checking for existing branch...");
+                        var newBranch = $"ci/{curBranch}/swiftshader_bins";
+                        var exists = StartProcess("git", $"checkout \"{newBranch}\"", RootDirectory)
+                            .AssertWaitForExit()
+                            .ExitCode == 0;
+                        if (!exists)
+                        {
+                            Git($"checkout -b \"{newBranch}\"");
+                        }
+
+                        var curCommit = GitCurrentCommit(RootDirectory);
+                        Git($"commit -m \"New binaries for SwiftShader on {sysName}\"");
+                        if (GitCurrentCommit(RootDirectory) != curCommit)
+                        {
+                            Git($"push --set-upstream origin \"{newBranch}\"");
+                            if (!exists)
+                            {
+                                var github = new GitHubClient
+                                (
+                                    new ProductHeaderValue("Silk.NET-CI"),
+                                    new InMemoryCredentialStore(new Credentials(pushableToken))
+                                );
+
+                                var pr = github.PullRequest.Create
+                                        ("dotnet", "Silk.NET", new("Update SwiftShader binaries", newBranch, curBranch))
+                                    .GetAwaiter()
+                                    .GetResult();
+                            }
+                        }
+                    }
                 }
             )
     );
