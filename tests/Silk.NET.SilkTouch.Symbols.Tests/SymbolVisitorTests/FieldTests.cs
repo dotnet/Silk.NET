0a79945db7c9684d4ca8817d7d85717f315ed026
Kai Jellinghaus <contact@kaij.tech>
2022-04-10 22:06:26 +0200

    Reorganize Tests

--- tests/Silk.NET.SilkTouch.Symbols.Tests/SymbolVisitorTests/FieldTests.cs
+++ tests/Silk.NET.SilkTouch.Symbols.Tests/SymbolVisitorTests/FieldTests.cs
@@ -1,59 +1,14 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Moq;
 using Moq.Protected;
 using Xunit;
 
-namespace Silk.NET.SilkTouch.Symbols.Tests;
+namespace Silk.NET.SilkTouch.Symbols.Tests.SymbolVisitorTests;
 
-public sealed class SymbolVisitorTests
+public class FieldTests
 {
-    [Fact]
-    public void StructSymbolIsVisitedAsType()
-    {
-        var symbol = new StructSymbol(new IdentifierSymbol(""));
-        var visitor = new Mock<SymbolVisitor>
-        {
-            CallBase = true
-        };
-
-        visitor.Object.Visit(symbol);
-        
-        visitor.Protected()
-            .Verify<TypeSymbol>("VisitType", Times.Once(), ItExpr.IsAny<TypeSymbol>());
-    }
-
-    [Fact]
-    public void StructSymbolIsVisitedAsStruct()
-    {
-        var symbol = new StructSymbol(new IdentifierSymbol(""));
-        var visitor = new Mock<SymbolVisitor>
-        {
-            CallBase = true
-        };
-
-        visitor.Object.Visit(symbol);
-        
-        visitor.Protected()
-            .Verify<StructSymbol>("VisitStruct", Times.Once(), ItExpr.IsAny<StructSymbol>());
-    }
-
-    [Fact]
-    public void StructIdentifierIsVisitedAsIdentifier()
-    {
-        var symbol = new StructSymbol(new IdentifierSymbol(""));
-        var visitor = new Mock<SymbolVisitor>
-        {
-            CallBase = true
-        };
-
-        visitor.Object.Visit(symbol);
-        
-        visitor.Protected()
-            .Verify<IdentifierSymbol>("VisitIdentifier", Times.Once(), ItExpr.IsAny<IdentifierSymbol>());
-    }
-
     [Fact]
     public void FieldIsVisitedAsField()
     {
---
+++ tests/Silk.NET.SilkTouch.Symbols.Tests/SymbolVisitorTests/StructTests.cs
@@ -0,0 +1,56 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Moq;
+using Moq.Protected;
+using Xunit;
+
+namespace Silk.NET.SilkTouch.Symbols.Tests.SymbolVisitorTests;
+
+public class StructTests
+{
+    [Fact]
+    public void StructSymbolIsVisitedAsType()
+    {
+        var symbol = new StructSymbol(new IdentifierSymbol(""));
+        var visitor = new Mock<SymbolVisitor>
+        {
+            CallBase = true
+        };
+
+        visitor.Object.Visit(symbol);
+        
+        visitor.Protected()
+            .Verify<TypeSymbol>("VisitType", Times.Once(), ItExpr.IsAny<TypeSymbol>());
+    }
+
+    [Fact]
+    public void StructSymbolIsVisitedAsStruct()
+    {
+        var symbol = new StructSymbol(new IdentifierSymbol(""));
+        var visitor = new Mock<SymbolVisitor>
+        {
+            CallBase = true
+        };
+
+        visitor.Object.Visit(symbol);
+        
+        visitor.Protected()
+            .Verify<StructSymbol>("VisitStruct", Times.Once(), ItExpr.IsAny<StructSymbol>());
+    }
+
+    [Fact]
+    public void StructIdentifierIsVisitedAsIdentifier()
+    {
+        var symbol = new StructSymbol(new IdentifierSymbol(""));
+        var visitor = new Mock<SymbolVisitor>
+        {
+            CallBase = true
+        };
+
+        visitor.Object.Visit(symbol);
+        
+        visitor.Protected()
+            .Verify<IdentifierSymbol>("VisitIdentifier", Times.Once(), ItExpr.IsAny<IdentifierSymbol>());
+    }
+}
