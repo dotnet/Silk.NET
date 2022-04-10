6f580bd2bad477358293eaf2b8ee3be4a12e5f0c
Kai Jellinghaus <contact@kaij.tech>
2022-04-10 22:08:14 +0200

    Add Identifier Tests

---
+++ tests/Silk.NET.SilkTouch.Symbols.Tests/SymbolVisitorTests/IdentifierTests.cs
@@ -0,0 +1,26 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Moq;
+using Moq.Protected;
+using Xunit;
+
+namespace Silk.NET.SilkTouch.Symbols.Tests.SymbolVisitorTests;
+
+public class IdentifierTests
+{
+    [Fact]
+    public void IdentifierIsVisitedAsIdentifier()
+    {
+        var symbol = new IdentifierSymbol("");
+        var visitor = new Mock<SymbolVisitor>
+        {
+            CallBase = true
+        };
+
+        visitor.Object.Visit(symbol);
+        
+        visitor.Protected()
+            .Verify<IdentifierSymbol>("VisitIdentifier", Times.Once(), ItExpr.IsAny<IdentifierSymbol>());
+    }
+}
