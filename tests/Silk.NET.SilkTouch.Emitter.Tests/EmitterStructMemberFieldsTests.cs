69286b50bc02a79faaeeff4a7ad19297f4fcac22
Kai Jellinghaus <contact@kaij.tech>
2022-04-20 17:13:10 +0200

    Add failing layout tests

--- tests/Silk.NET.SilkTouch.Emitter.Tests/EmitterStructMemberFieldsTests.cs
+++ tests/Silk.NET.SilkTouch.Emitter.Tests/EmitterStructMemberFieldsTests.cs
@@ -29,9 +29,54 @@ public class EmitterStructMemberFieldsTests : EmitterTest
 
         Assert.Equal
         (
-            @"public struct Test
+            @"[StructLayout(LayoutKind.Explicit)]
+public struct Test
 {
+    [FieldOffset(0)]
     public int F1;
+}", node.ToFullString()
+        );
+    }
+    
+    [Fact]
+    public void StructWithMultipleFieldsIntegration()
+    {
+        var node = Transform
+        (
+            new StructSymbol
+            (
+                new IdentifierSymbol("Test"), new StructLayout((new[]
+                {
+                    new LayoutEntry(new FieldSymbol
+                    (
+                        new StructSymbol(new IdentifierSymbol("int"), StructLayout.Empty),
+                        new IdentifierSymbol("F1")
+                    ), 0),
+                    new LayoutEntry(new FieldSymbol
+                    (
+                        new StructSymbol(new IdentifierSymbol("int"), StructLayout.Empty),
+                        new IdentifierSymbol("F2")
+                    ), 20),
+                    new LayoutEntry(new FieldSymbol
+                    (
+                        new StructSymbol(new IdentifierSymbol("int"), StructLayout.Empty),
+                        new IdentifierSymbol("F3")
+                    ), 12)
+                }).ToImmutableArray())
+            )
+        );
+
+        Assert.Equal
+        (
+            @"[StructLayout(LayoutKind.Explicit)]
+public struct Test
+{
+    [FieldOffset(0)]
+    public int F1;
+    [FieldOffset(20)]
+    public int F2;
+    [FieldOffset(12)]
+    public int F3;
 }", node.ToFullString()
         );
     }
