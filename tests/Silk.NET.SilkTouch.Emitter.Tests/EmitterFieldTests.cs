f9c804311c02b7f9437100292a0f73761bb9ecf5
Kai Jellinghaus <contact@kaij.tech>
2022-04-30 16:16:43 +0200

    Field Tests (#896)

--- tests/Silk.NET.SilkTouch.Emitter.Tests/EmitterFieldTests.cs
+++ tests/Silk.NET.SilkTouch.Emitter.Tests/EmitterFieldTests.cs
@@ -2,6 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Silk.NET.SilkTouch.Symbols;
 using Xunit;
 
@@ -10,11 +14,63 @@ namespace Silk.NET.SilkTouch.Emitter.Tests;
 public sealed class EmitterFieldIntegrationTests : EmitterTest
 {
     [Fact]
-    public void StructHasStructKeyword()
+    public void FieldIntegration()
     {
         var syntax = Transform(new FieldSymbol(new StructSymbol(new IdentifierSymbol("int"),StructLayout.Empty), new IdentifierSymbol("Test")));
 
         var result = syntax.ToFullString();
         Assert.Equal("public int Test;", result);
+    }
+
+    [Fact]
+    public void FieldIsPublic()
+    {
+        var syntax = Transform
+        (
+            new FieldSymbol
+            (
+                new StructSymbol(new IdentifierSymbol("int"), ImmutableArray<MemberSymbol>.Empty),
+                new IdentifierSymbol("Test")
+            )
+        ) as FieldDeclarationSyntax;
+
+        Assert.NotNull(syntax);
+        Assert.Single(syntax!.Modifiers, x => x.IsKind(SyntaxKind.PublicKeyword));
+    }
+
+    [Fact]
+    public void FieldHasCorrectTypeIdentifier()
+    {
+        var syntax = Transform
+        (
+            new FieldSymbol
+            (
+                new StructSymbol(new IdentifierSymbol("int"), ImmutableArray<MemberSymbol>.Empty),
+                new IdentifierSymbol("Test")
+            )
+        ) as FieldDeclarationSyntax;
+
+        Assert.NotNull(syntax);
+        var type = syntax!.Declaration.Type as SimpleNameSyntax;
+        Assert.NotNull(type);
+        Assert.Equal("int", type!.Identifier.Text);
+    }
+
+    [Fact]
+    public void FieldHasCorrectIdentifier()
+    {
+        var syntax = Transform
+        (
+            new FieldSymbol
+            (
+                new StructSymbol(new IdentifierSymbol("int"), ImmutableArray<MemberSymbol>.Empty),
+                new IdentifierSymbol("Test")
+            )
+        ) as FieldDeclarationSyntax;
+
+        Assert.NotNull(syntax);
+
+        var variableDeclaratorSyntax = Assert.Single(syntax!.Declaration.Variables);
+        Assert.Equal("Test", variableDeclaratorSyntax.Identifier.Text);
     }
 }
