1159c65180c1e84d362f0e166b36dbf006038b2a
Kai Jellinghaus <contact@kaij.tech>
2022-05-07 23:12:43 +0200

    Create & Test Namespace Symbol

---
+++ tests/Silk.NET.SilkTouch.Symbols.Tests/SymbolVisitorTests/NamespaceTests.cs
@@ -0,0 +1,47 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Immutable;
+using Moq;
+using Moq.Protected;
+using Xunit;
+
+namespace Silk.NET.SilkTouch.Symbols.Tests.SymbolVisitorTests;
+
+public class NamespaceTests
+{
+    [Fact]
+    public void NamespaceIdentifierIsVisited()
+    {
+        var symbol = new NamespaceSymbol(new IdentifierSymbol(""), ImmutableArray<TypeSymbol>.Empty);
+            
+        var visitor = new Mock<SymbolVisitor>
+        {
+            CallBase = true
+        };
+
+        visitor.Object.Visit(symbol);
+        
+        visitor.Protected()
+            .Verify<IdentifierSymbol>("VisitIdentifier", Times.Once(), ItExpr.IsAny<IdentifierSymbol>());
+    }
+    
+    [Fact]
+    public void NamespaceMemberIsVisited()
+    {
+        var symbol = new NamespaceSymbol(new IdentifierSymbol(""), new []
+        {
+            (TypeSymbol)new StructSymbol(new IdentifierSymbol(""), StructLayout.Empty)
+        }.ToImmutableArray());
+            
+        var visitor = new Mock<SymbolVisitor>
+        {
+            CallBase = true
+        };
+
+        visitor.Object.Visit(symbol);
+        
+        visitor.Protected()
+            .Verify<StructSymbol>("VisitStruct", Times.Once(), ItExpr.IsAny<StructSymbol>());
+    }
+}
