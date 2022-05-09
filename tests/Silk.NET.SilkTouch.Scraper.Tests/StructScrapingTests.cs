d83379e15f9f3a581af37b900f9e59bfb0867409
Kai Jellinghaus <contact@kaij.tech>
2022-05-09 18:49:37 +0200

    Struct Scraping Support

--- tests/Silk.NET.SilkTouch.Scraper.Tests/StructScrapingTests.cs
+++ tests/Silk.NET.SilkTouch.Scraper.Tests/StructScrapingTests.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Xml;
+using Silk.NET.SilkTouch.Symbols;
 using Xunit;
 
 namespace Silk.NET.SilkTouch.Scraper.Tests;
@@ -10,6 +12,25 @@ public class StructScrapingTests
     [Fact]
     public void StructXMLGeneratesStructSymbol()
     {
+        var doc = new XmlDocument();
+        doc.LoadXml(@"<struct name=""Test""></struct>");
 
+        var symbols = new ClangScraper().ScrapeXML(doc);
+        
+        var symbol = Assert.Single(symbols);
+        var @struct = Assert.IsType<StructSymbol>(symbol);
+    }
+    
+    [Fact]
+    public void StructXMLGeneratesCorrectIdentifier()
+    {
+        var doc = new XmlDocument();
+        doc.LoadXml(@"<struct name=""Test""></struct>");
+
+        var symbols = new ClangScraper().ScrapeXML(doc);
+        
+        var symbol = Assert.Single(symbols);
+        var @struct = Assert.IsType<StructSymbol>(symbol);
+        Assert.Equal("Test", @struct.Identifier.Value);
     }
 }
