a3c9a5c98fc6e11be6e329b92baaccff8c01c433
tilkinsc <ctilkins@mail.com>
2022-07-10 00:21:53 -0400

    Specified mipmap levels to make a complete texture

--- examples/CSharp/OpenGL Tutorials/Tutorial 1.5 - Transformations/Texture.cs
+++ examples/CSharp/OpenGL Tutorials/Tutorial 1.5 - Transformations/Texture.cs
@@ -56,6 +56,8 @@ namespace Tutorial
             _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int) GLEnum.ClampToEdge);
             _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int) GLEnum.Linear);
             _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int) GLEnum.Linear);
+            _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureBaseLevel, 0);
+            _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMaxLevel, 8);
             _gl.GenerateMipmap(TextureTarget.Texture2D);
         }
 
