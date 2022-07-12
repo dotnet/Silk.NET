94ea0b16c1f3511cbe457e827edb2ffd4ae9293b
Kai Jellinghaus <contact@kaij.tech>
2022-07-12 08:45:08 +0200

    Add extra tests

--- tests/Silk.NET.SilkTouch.IntegrationTests/GLFWIntegrationTests.cs
+++ tests/Silk.NET.SilkTouch.IntegrationTests/GLFWIntegrationTests.cs
@@ -21,8 +21,9 @@ public class GLFWIntegrationTests
      Trait("Category",        "Integration"),
      Trait("Source Language", "C++"),
      Trait("Target Language", "C#"),
-     Trait("Feature",         "Structs"),
-     Trait("Feature",         "Fields")]
+     Trait("Feature",         "Fields"),
+     Trait("Feature", "Static Methods"),
+     Trait("Feature", "Classes")]
     public Task GlfwInit()
     {
         var result = TestHelper.GetCSharpOutputFromCpp
@@ -46,6 +47,92 @@ public class GLFWIntegrationTests
 #endif
 
 GLFWAPI int glfwInit(void);",
+            definedMacros: new string[] { "_WIN32", "GLFW_DLL" }
+        );
+        return Verifier.Verify(result);
+    }
+    
+    [Fact,
+     Trait("Category",        "Integration"),
+     Trait("Source Language", "C++"),
+     Trait("Target Language", "C#"),
+     Trait("Feature",         "Fields"),
+     Trait("Feature",         "Static Methods"),
+     Trait("Feature",         "Classes")]
+    public Task GlfwSetErrorCallback()
+    {
+        var result = TestHelper.GetCSharpOutputFromCpp
+        (
+            @"
+/* GLFWAPI is used to declare public API functions for export
+ * from the DLL / shared library / dynamic library.
+ */
+#if defined(_WIN32) && defined(_GLFW_BUILD_DLL)
+ /* We are building GLFW as a Win32 DLL */
+ #define GLFWAPI __declspec(dllexport)
+#elif defined(_WIN32) && defined(GLFW_DLL)
+ /* We are calling GLFW as a Win32 DLL */
+ #define GLFWAPI __declspec(dllimport)
+#elif defined(__GNUC__) && defined(_GLFW_BUILD_DLL)
+ /* We are building GLFW as a shared / dynamic library */
+ #define GLFWAPI __attribute__((visibility(""default"")))
+#else
+        /* We are building or calling GLFW as a static library */
+#define GLFWAPI
+#endif
+
+typedef void (* GLFWerrorfun)(int error_code, const char* description);
+
+GLFWAPI GLFWerrorfun glfwSetErrorCallback(GLFWerrorfun callback);",
+            definedMacros: new string[] { "_WIN32", "GLFW_DLL" }
+        );
+        return Verifier.Verify(result);
+    }
+    
+    [Fact,
+     Trait("Category",        "Integration"),
+     Trait("Source Language", "C++"),
+     Trait("Target Language", "C#"),
+     Trait("Feature",         "Structs"),
+     Trait("Feature",         "Fields"),
+     Trait("Feature",         "Static Methods"),
+     Trait("Feature",         "Classes")]
+    public Task GlfwGetVideoModes()
+    {
+        var result = TestHelper.GetCSharpOutputFromCpp
+        (
+            @"
+/* GLFWAPI is used to declare public API functions for export
+ * from the DLL / shared library / dynamic library.
+ */
+#if defined(_WIN32) && defined(_GLFW_BUILD_DLL)
+ /* We are building GLFW as a Win32 DLL */
+ #define GLFWAPI __declspec(dllexport)
+#elif defined(_WIN32) && defined(GLFW_DLL)
+ /* We are calling GLFW as a Win32 DLL */
+ #define GLFWAPI __declspec(dllimport)
+#elif defined(__GNUC__) && defined(_GLFW_BUILD_DLL)
+ /* We are building GLFW as a shared / dynamic library */
+ #define GLFWAPI __attribute__((visibility(""default"")))
+#else
+        /* We are building or calling GLFW as a static library */
+#define GLFWAPI
+#endif
+
+
+typedef struct GLFWvidmode
+{
+    int width;
+    int height;
+    int redBits;
+    int greenBits;
+    int blueBits;
+    int refreshRate;
+} GLFWvidmode;
+
+typedef struct GLFWmonitor GLFWmonitor;
+
+GLFWAPI const GLFWvidmode* glfwGetVideoModes(GLFWmonitor* monitor, int* count);",
             definedMacros: new string[] { "_WIN32", "GLFW_DLL" }
         );
         return Verifier.Verify(result);
