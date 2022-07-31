414cbc2220bfd324ed2d5ebf07b12afb1d3b0c2e
Ollie Robinson <ollie@ollierobinson.co.uk>
2022-07-31 19:21:15 +0100

    changed to be a less accurate ratio (but more accurate to the user)

--- src/Windowing/Silk.NET.Windowing.Common/Structs/VideoMode.cs
+++ src/Windowing/Silk.NET.Windowing.Common/Structs/VideoMode.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using Silk.NET.Maths;
 
 namespace Silk.NET.Windowing
@@ -11,13 +12,11 @@ namespace Silk.NET.Windowing
         {
             Resolution = resolution;
             RefreshRate = refreshRate;
-            AspectRatio = resolution != null ? resolution / GreatestCommonFactor(resolution.Value.X, resolution.Value.Y) : null;
+            AspectRatio = resolution != null ? CalculateAspectRatio(resolution.Value) : null;
         }
 
         public VideoMode(int refreshRate)
-            : this(null, refreshRate)
-        {
-        }
+            : this(null, refreshRate) { }
 
         /// <summary>
         /// Resolution of the full screen window.
@@ -28,9 +27,11 @@ namespace Silk.NET.Windowing
         /// Refresh rate of the full screen window in Hz.
         /// </summary>
         public int? RefreshRate { get; }
-        
+
         /// <summary>
-        /// Aspect ratio of the full screen window.
+        /// Aspect ratio of the full screen window. This value will not be 100% accurate to the actual value, however
+        /// is more accurate to what a user may expect the ratio to be (for example a resolution of 1366x768 will appear
+        /// as 16:9, even though it is not quite 16:9 in reality).
         /// </summary>
         public Vector2D<int>? AspectRatio { get; }
 
@@ -39,40 +40,23 @@ namespace Silk.NET.Windowing
         /// </summary>
         public static VideoMode Default => new VideoMode();
 
-        private static int GreatestCommonFactor(int value1, int value2)
+        private static Vector2D<int> CalculateAspectRatio(Vector2D<int> res)
         {
-            while (true)
+            // Calculate the width-height ratio.
+            float ratio = res.X / (float) res.Y;
+
+            // Count up until the lowest value as the aspect ratio cannot be higher than the lowest value.
+            int lowestValue = res.X < res.Y ? res.X : res.Y;
+            for (int i = 1; i < lowestValue; i++)
             {
-                if (value2 > value1)
-                {
-                    int val = value2 - value1;
-                    if (val <= 0)
-                        return value1;
-                    if (val > value1)
-                    {
-                        value2 = val;
-                        continue;
-                    }
-
-                    var value3 = value1;
-                    value1 = val;
-                    value2 = value3;
-                }
-                else
-                {
-                    int val = value1 - value2;
-                    if (val <= 0)
-                        return value2;
-                    if (val > value2)
-                    {
-                        value1 = value2;
-                        value2 = val;
-                        continue;
-                    }
-
-                    value1 = val;
-                }
+                // Multiply both together and calculate a good enough value, a bias of 0.1 seems to work well.
+                float multiplied = ratio * i;
+                if (multiplied - (int) multiplied < 0.1f)
+                    return new Vector2D<int>((int) multiplied, i);
             }
+
+            return res;
         }
+
     }
 }
