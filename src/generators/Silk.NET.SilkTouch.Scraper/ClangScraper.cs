f3b6e5a0dfd473e6c76c7ebce2d2fce0fdd3db97
Kai Jellinghaus <contact@kaij.tech>
2022-08-05 16:32:04 +0200

    Truncate xcrun output

--- src/generators/Silk.NET.SilkTouch.Scraper/ClangScraper.cs
+++ src/generators/Silk.NET.SilkTouch.Scraper/ClangScraper.cs
@@ -77,7 +77,7 @@ public sealed class ClangScraper
         process.Start();
         process.WaitForExit();
         var output = process.StandardOutput.ReadToEnd();
-        logger.LogTrace("Got Response from xcrun: {response}", output);
+        logger.LogTrace("Got Response from xcrun: {response}", output.ReplaceLineEndings("\\n"));
         var lines = output.Split('\n');
         var path = lines.Length > 0 ? lines[0] : null;
         if (string.IsNullOrWhiteSpace(path))
