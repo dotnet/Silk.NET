3d982b16b98a5021f5593108b002619d405a33fb
Kai Jellinghaus <contact@kaij.tech>
2022-08-06 20:35:46 +0200

    Add equality members to TypeId

--- src/generators/Silk.NET.SilkTouch.Symbols/TypeId.cs
+++ src/generators/Silk.NET.SilkTouch.Symbols/TypeId.cs
@@ -6,7 +6,7 @@ namespace Silk.NET.SilkTouch.Symbols;
 /// <summary>
 /// The Id of a <see cref="TypeSymbol"/>
 /// </summary>
-public readonly struct TypeId
+public readonly struct TypeId : IEquatable<TypeId>
 {
     private readonly Guid _guid;
     private TypeId(Guid guid)
@@ -19,4 +19,19 @@ public readonly struct TypeId
     /// </summary>
     /// <returns>The new instance</returns>
     public static TypeId CreateNew() => new TypeId(Guid.NewGuid());
+
+    /// <inheritdoc />
+    public bool Equals(TypeId other) => _guid.Equals(other._guid);
+    
+    /// <inheritdoc />
+    public override bool Equals(object? obj) => obj is TypeId other && Equals(other);
+    
+    /// <inheritdoc />
+    public override int GetHashCode() => _guid.GetHashCode();
+    
+    /// <inheritdoc />
+    public static bool operator ==(TypeId left, TypeId right) => left.Equals(right);
+    
+    /// <inheritdoc />
+    public static bool operator !=(TypeId left, TypeId right) => !left.Equals(right);
 }
