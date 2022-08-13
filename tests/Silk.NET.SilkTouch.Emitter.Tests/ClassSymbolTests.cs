eafa495b7189c6ae44bc623b8dd566b2f4f4af86
Kai Jellinghaus <contact@kaij.tech>
2022-08-13 11:30:47 +0200

    Add Class Support to Emitter

--- tests/Silk.NET.SilkTouch.Emitter.Tests/ClassSymbolTests.cs
+++ tests/Silk.NET.SilkTouch.Emitter.Tests/ClassSymbolTests.cs
@@ -18,4 +18,30 @@ public sealed class ClassSymbolTests : EmitterTest
 
         Assert.Equal("public class C\n" + "{\n" + "}", result.ToFullString());
     }
+    
+    [Fact, Trait("Category", "Symbols"), Trait("Target Language", "C#")]
+    public void StringTestWithMethods()
+    {
+        var method = new StaticExternalMethodSymbol
+        (
+            new ExternalTypeReference(null, new IdentifierSymbol("int")),
+            ImmutableArray<Parameter>.Empty,
+            new IdentifierSymbol("M")
+        );
+        var symbol = new ClassSymbol
+        (
+            TypeId.CreateNew(),
+            new IdentifierSymbol("C"),
+            new MethodSymbol[]
+            {
+                method
+            }.ToImmutableArray()
+        );
+        
+        
+
+        var result = Transform(symbol);
+
+        Assert.Equal("public class C\n" + "{" + "\n    public static extern int M();" + "\n}", result.ToFullString());
+    }
 }
