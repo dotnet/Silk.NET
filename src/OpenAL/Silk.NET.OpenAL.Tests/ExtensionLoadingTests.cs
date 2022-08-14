6577a0499ad5acb10be351705e91ed9eedb661f8
okaniku <okaniku@outlook.com>
2022-08-14 20:50:28 +1000

    Add OpenAL disconnect extension

--- src/OpenAL/Silk.NET.OpenAL.Tests/ExtensionLoadingTests.cs
+++ src/OpenAL/Silk.NET.OpenAL.Tests/ExtensionLoadingTests.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
@@ -78,6 +78,7 @@ public class ExtensionLoadingTests
         Test<EnumerateAll>();
         Test<CaptureEnumerationEnumeration>();
         Test<EffectExtensionContext>();
+        Test<Disconnect>();
 
         SilkMarshal.Free(loaderPtr);
         SilkMarshal.Free(ctxLoaderPtr);
