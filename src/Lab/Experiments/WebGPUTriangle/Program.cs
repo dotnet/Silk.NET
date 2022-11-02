817d7f37618dffffc855f642efd7facfa0cac939
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-11-01 17:01:39 -0700

    Add Silk.NET.Windowing.Extensions.WebGPU to create surface from window

---
+++ src/Lab/Experiments/WebGPUTriangle/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Silk.NET.WebGPU;
+using Silk.NET.Windowing;
+
+namespace WebGPUTriangle;
+
+public static unsafe class Program
+{
+    private static WebGPU   wgpu;
+    private static Surface* surface;
+    
+    public static void Main()
+    {
+        //Create window
+        var options = WindowOptions.Default;
+        options.API = GraphicsAPI.None;
+
+        var window = Window.Create(WindowOptions.Default);
+
+        window.Load   += WindowOnLoad;
+        window.Update += WindowOnUpdate;
+        window.Render += WindowOnRender;
+
+        window.Run();
+
+        void WindowOnLoad()
+        {
+            wgpu = WebGPU.GetApi();
+    
+            
+        }
+
+        void WindowOnUpdate(double delta)
+        {
+    
+        }
+
+        void WindowOnRender(double delta)
+        {
+    
+        }
+
+    }
+}
---
+++ src/Windowing/Extensions/Silk.NET.WIndowing.Extensions.WebGPU/WebGPUWindow.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Silk.NET.Core.Contexts;
+using Silk.NET.WebGPU;
+using Silk.NET.Windowing;
+
+namespace Silk.NET.WIndowing.Extensions.WebGPU;
+
+public static class WebGPUWindow
+{
+    /// <summary>
+    /// Creates a WebGPU surface from a window.
+    /// </summary>
+    /// <param name="wgpu">A WebGPU instance</param>
+    /// <param name="window">The window to get the surface from</param>
+    /// <returns>A newly created Surface*/</returns>
+    /// <exception cref="NotSupportedException">Throws when an unsupported platform is detected.</exception>
+    public static unsafe Surface* CreateSurface(NET.WebGPU.WebGPU wgpu, IWindow window)
+    {
+        var descriptor = new SurfaceDescriptor();
+        
+        if (window.Native.X11 != null)
+        {
+            var xlibDescriptor = new SurfaceDescriptorFromXlibWindow
+            {
+                Chain = new ChainedStruct
+                {
+                    Next = null,
+                    SType = SType.SurfaceDescriptorFromXlibWindow
+                },
+                Display = (void*) window.Native.X11.Value.Display,
+                Window  = (uint) window.Native.X11.Value.Window
+            };
+
+            descriptor.NextInChain = (ChainedStruct*) (&xlibDescriptor);
+        }
+        else if (window.Native.Cocoa != null)
+        {
+            throw new NotSupportedException("WebGPU on MacOS is not supported at this time!)");
+            
+            var cocoa = window.Native.Cocoa.Value;
+
+            var cocoaDescriptor = new SurfaceDescriptorFromMetalLayer
+            {
+                Chain = new ChainedStruct
+                {
+                    Next  = null,
+                    SType = SType.SurfaceDescriptorFromMetalLayer
+                }, 
+                Layer = null //TODO: Get the layer from the window
+            };
+        }
+        else if (window.Native.Wayland != null)
+        {
+            var waylandDescriptor = new SurfaceDescriptorFromWaylandSurface
+            {
+                Chain = new ChainedStruct
+                {
+                    Next  = null,
+                    SType = SType.SurfaceDescriptorFromWaylandSurface
+                },
+                Display = (void*) window.Native.Wayland.Value.Display,
+                Surface = (void*) window.Native.Wayland.Value.Surface
+            };
+
+            descriptor.NextInChain = (ChainedStruct*) (&waylandDescriptor); 
+        }
+        else if (window.Native.Win32 != null)
+        {
+            var win32Descriptor = new SurfaceDescriptorFromWindowsHWND
+            {
+                Chain = new ChainedStruct
+                {
+                    Next  = null,
+                    SType = SType.SurfaceDescriptorFromWindowsHwnd
+                },
+                Hwnd      = (void*) window.Native.Win32.Value.Hwnd,
+                Hinstance = (void*) window.Native.Win32.Value.HInstance
+            };
+
+            descriptor.NextInChain = (ChainedStruct*) (&win32Descriptor);  
+        }
+        else
+        {
+            throw new NotSupportedException($"Your platform is not supported! {window.Native.Kind}");
+        }
+        
+        return wgpu.InstanceCreateSurface(null, ref descriptor);
+    }
+}
