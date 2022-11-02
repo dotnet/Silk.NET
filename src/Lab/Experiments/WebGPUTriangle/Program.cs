2f022350b84c84d6989e7c299b2a43d89fb88070
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-11-01 17:42:20 -0700

    WebGPUTriangle: Shader creation

--- src/Lab/Experiments/WebGPUTriangle/Program.cs
+++ src/Lab/Experiments/WebGPUTriangle/Program.cs
@@ -10,12 +10,25 @@ namespace WebGPUTriangle;
 public static unsafe class Program
 {
     private static WebGPU   wgpu;
-    private static Surface* surface;
     private static IWindow  window;
 
-    private static Adapter* adapter;
-    private static Device*  device;
+    private static Surface*      surface;
+    private static Adapter*      adapter;
+    private static Device*       device;
+    private static ShaderModule* shader;
 
+    private const string SHADER = @"@vertex
+fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
+    let x = f32(i32(in_vertex_index) - 1);
+    let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
+    return vec4<f32>(x, y, 0.0, 1.0);
+}
+
+@fragment
+fn fs_main() -> @location(0) vec4<f32> {
+    return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+}";
+    
     public static void Main()
     {
         //Create window
@@ -56,7 +69,11 @@ public static unsafe class Program
                 new PfnRequestAdapterCallback((_, adapter1, _, _) => adapter = adapter1),
                 null
             );
+            
+            Console.WriteLine($"Got adapter {(nuint)adapter:X}");
         } //Get adapter
+
+        PrintAdapterFeatures();
 
         { //Get device
             var requiredLimits = stackalloc RequiredLimits[1];
@@ -75,18 +92,32 @@ public static unsafe class Program
                 new PfnRequestDeviceCallback((_, device1, _, _) => device = device1),
                 null
             );
+            
+            Console.WriteLine($"Got device {(nuint)device:X}");
         } //Get device
         
         wgpu.DeviceSetUncapturedErrorCallback(device, new PfnErrorCallback(UncapturedError), null);
-        
-        PrintAdapterFeatures();
+        wgpu.DeviceSetDeviceLostCallback(device, new PfnDeviceLostCallback(DeviceLost), null);
+
+        { //Load shader
+            var wgslDescriptor = new ShaderModuleWGSLDescriptor
+            {
+                Code = (byte*) SilkMarshal.StringToPtr(SHADER),
+                Chain = new ChainedStruct
+                {
+                    SType = SType.ShaderModuleWgsldescriptor
+                }
+            };
+            
+            var shaderModuleDescriptor = new ShaderModuleDescriptor
+            {
+                NextInChain = (ChainedStruct*) (&wgslDescriptor),
+            };
+            
+            shader = wgpu.DeviceCreateShaderModule(device, ref shaderModuleDescriptor);
+        } //Load shader
     }
     
-    private static void UncapturedError(ErrorType arg0, byte* arg1, void* arg2)
-    {
-        Console.WriteLine($"{arg0}: {SilkMarshal.PtrToString((nint) arg1)}");
-    }
-
     private static void WindowOnUpdate(double delta) {}
 
     private static void WindowOnRender(double delta) {}
@@ -105,5 +136,15 @@ public static unsafe class Program
         {
             Console.WriteLine($"\t{features[i]}");
         }
+    }
+    
+    private static void DeviceLost(DeviceLostReason arg0, byte* arg1, void* arg2)
+    {
+        Console.WriteLine($"Device lost! Reason: {arg0} Message: {SilkMarshal.PtrToString((nint) arg1)}");
+    }
+
+    private static void UncapturedError(ErrorType arg0, byte* arg1, void* arg2)
+    {
+        Console.WriteLine($"{arg0}: {SilkMarshal.PtrToString((nint) arg1)}");
     }
 }
