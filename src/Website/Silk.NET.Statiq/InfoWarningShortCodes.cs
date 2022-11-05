ee2bdae0746221bff3144ca7762c8e3102d46322
ollie <ollie@ollierobinson.co.uk>
2022-11-05 15:47:01 +0000

    Added shortcodes

---
+++ src/Website/Silk.NET.Statiq/InfoWarningShortCodes.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Statiq.Common;
+
+namespace Silk.NET.Statiq
+{
+    public class InfoShortCode : IShortcode
+    {
+        public Task<IEnumerable<ShortcodeResult>> ExecuteAsync
+            (KeyValuePair<string, string>[] args, string content, IDocument document, IExecutionContext context) =>
+            Task.FromResult(args.Select(x => new ShortcodeResult($"<div class=\"alert alert-info\" role=\"alert\">{x.Value}</div>")));
+    }
+    
+    public class WarningShortCode : IShortcode
+    {
+        public Task<IEnumerable<ShortcodeResult>> ExecuteAsync
+            (KeyValuePair<string, string>[] args, string content, IDocument document, IExecutionContext context) =>
+            Task.FromResult(args.Select(x => new ShortcodeResult($"<div class=\"alert alert-warning\" role=\"alert\">{x.Value}</div>")));
+    }
+}
