c301a15853aeea3e771c3327c3cc5efc0863baf9
Beyley Thomas <ep1cm1n10n123@gmail.com>
2022-11-05 21:17:10 -0700

    Begin working on textured quad example

--- examples/CSharp/WebGPU/WebGPUTexturedQuad/Program.cs
+++ examples/CSharp/WebGPU/WebGPUTexturedQuad/Program.cs
@@ -1,18 +1,29 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using System.Diagnostics;
+using System.Numerics;
 using Silk.NET.Core.Native;
 using Silk.NET.Maths;
 using Silk.NET.WebGPU;
 using Silk.NET.WebGPU.Extensions.Disposal;
 using Silk.NET.WebGPU.Extensions.WGPU;
 using Silk.NET.Windowing;
+using Buffer = Silk.NET.WebGPU.Buffer;
 
-namespace WebGPUTriangle;
+namespace WebGPUTexturedQuad;
 
-public static unsafe class Program
+// ReSharper disable once InconsistentNaming
+public static unsafe class WebGPUTexturedQuad
 {
+    public struct Vertex
+    {
+        public Vertex(Vector2 position, Vector2 texCoord)
+        {
+            Position = position;
+            TexCoord = texCoord;
+        }
+
+        public Vector2 Position;
+        public Vector2 TexCoord;
+    }
+    
     // ReSharper disable once InconsistentNaming
     private static WebGPU         wgpu            = null!;
     private static WebGPUDisposal _WebGpuDisposal = null!;
@@ -26,19 +37,38 @@ public static unsafe class Program
     private static SwapChain*      _SwapChain;
     private static TextureFormat   _SwapChainFormat;
 
-    private const string SHADER = @"@vertex
-fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
-    let x = f32(i32(in_vertex_index) - 1);
-    let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
-    return vec4<f32>(x, y, 0.0, 1.0);
+    private static Buffer* _VertexBuffer;
+    private static ulong   _VertexBufferSize;
+
+    private const string SHADER = @"
+struct VertexOutputs {
+    @builtin(position) position: vec4<f32>,
+    @location(0) tex_coord: vec2<f32>
+}
+
+struct FragmentInputs {
+    @location(0) tex_coord: vec2<f32>
+}
+
+@vertex
+fn vs_main(
+    @location(0) pos: vec2<f32>,
+    @location(1) tex_coord: vec2<f32>
+) -> VertexOutputs {
+    var output: VertexOutputs;
+
+    output.position = vec4<f32>(pos, 0.0, 1.0);
+    output.tex_coord = tex_coord;
+
+    return output;
 }
 
 @fragment
-fn fs_main() -> @location(0) vec4<f32> {
-    return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+fn fs_main(input: FragmentInputs) -> @location(0) vec4<f32> {
+    return vec4<f32>(input.tex_coord.x, input.tex_coord.y, input.tex_coord.x, input.tex_coord.y);
 }";
 
-    public static void Main()
+    public static void Main(string[] args)
     {
         //Create window
         var options = WindowOptions.Default;
@@ -47,7 +77,7 @@ fn fs_main() -> @location(0) vec4<f32> {
         options.FramesPerSecond          = 60;
         options.UpdatesPerSecond         = 60;
         options.Position                 = new Vector2D<int>(0, 0);
-        options.Title                    = "WebGPU Triangle";
+        options.Title                    = "WebGPU Textured Quad";
         options.IsVisible                = true;
         options.ShouldSwapAutomatically  = false;
         options.IsContextControlDisabled = true;
@@ -63,10 +93,7 @@ fn fs_main() -> @location(0) vec4<f32> {
         _Window.Run();
     }
 
-    private static void FramebufferResize(Vector2D<int> obj)
-    {
-        CreateSwapchain();
-    }
+    private static void WindowOnUpdate(double delta) {}
 
     private static void WindowOnLoad()
     {
@@ -91,6 +118,11 @@ fn fs_main() -> @location(0) vec4<f32> {
             );
 
             Console.WriteLine($"Got adapter {(nuint) _Adapter:X}");
+
+            var properties = new AdapterProperties();
+            wgpu.AdapterGetProperties(_Adapter, &properties);
+
+            Console.WriteLine($"Name: {SilkMarshal.PtrToString((nint) properties.Name)}, Vendor name: {SilkMarshal.PtrToString((nint) properties.VendorName)} backend: {properties.BackendType} Arch: {SilkMarshal.PtrToString((nint) properties.Architecture)}");
         } //Get adapter
 
         PrintAdapterFeatures();
@@ -141,8 +173,31 @@ fn fs_main() -> @location(0) vec4<f32> {
 
         _SwapChainFormat = wgpu.SurfaceGetPreferredFormat(_Surface, _Adapter);
 
-        { //Create pipeline
+        var vertexAttributes = stackalloc VertexAttribute[2];
 
+        vertexAttributes[0] = new VertexAttribute
+        {
+            Format         = VertexFormat.Float32x2,
+            Offset         = 0,
+            ShaderLocation = 0
+        };
+        vertexAttributes[1] = new VertexAttribute
+        {
+            Format = VertexFormat.Float32x2,
+            Offset = (ulong) sizeof(Vector2),
+            ShaderLocation = 1
+        };
+
+        var vertexBufferLayout = new VertexBufferLayout
+        {
+            Attributes     = vertexAttributes,
+            AttributeCount = 2,
+            StepMode       = VertexStepMode.Vertex, 
+            ArrayStride = (ulong) sizeof(Vertex)
+        };
+        //Create vertex buffer layout
+
+        { //Create pipeline
             var blendState = new BlendState
             {
                 Color = new BlendComponent
@@ -178,8 +233,10 @@ fn fs_main() -> @location(0) vec4<f32> {
             {
                 Vertex = new VertexState
                 {
-                    Module     = _Shader,
-                    EntryPoint = (byte*) SilkMarshal.StringToPtr("vs_main"),
+                    Module      = _Shader,
+                    EntryPoint  = (byte*) SilkMarshal.StringToPtr("vs_main"),
+                    Buffers     = &vertexBufferLayout,
+                    BufferCount = 1
                 },
                 Primitive = new PrimitiveState
                 {
@@ -195,7 +252,7 @@ fn fs_main() -> @location(0) vec4<f32> {
                     AlphaToCoverageEnabled = false
                 },
                 Fragment     = &fragmentState,
-                DepthStencil = null
+                DepthStencil = null,
             };
 
             _Pipeline = wgpu.DeviceCreateRenderPipeline(_Device, ref renderPipelineDescriptor);
@@ -203,35 +260,46 @@ fn fs_main() -> @location(0) vec4<f32> {
             Console.WriteLine($"Created pipeline {(nuint) _Pipeline:X}");
         } //Create pipeline
 
+        { //Create vertex buffer
+            var descriptor = new BufferDescriptor
+            {
+                Size  = _VertexBufferSize = (ulong) (sizeof(Vertex) * 6),
+                Usage = BufferUsage.Vertex | BufferUsage.CopyDst
+            };
+
+            _VertexBuffer = wgpu.DeviceCreateBuffer(_Device, ref descriptor);
+
+            //Get a queue
+            var queue = wgpu.DeviceGetQueue(_Device);
+
+            var data = stackalloc Vertex[6];
+
+            //Fill data with a quad with a CCW front face
+            data[0] = new Vertex(new Vector2(-0.5f, -0.5f), new Vector2(-1f, -1f)); //Top left
+            data[1] = new Vertex(new Vector2(0.5f, -0.5f), new Vector2(1f, -1f));   //Top right
+            data[2] = new Vertex(new Vector2(0.5f, 0.5f), new Vector2(1f, 1f));     //Bottom right
+            data[3] = new Vertex(new Vector2(-0.5f, -0.5f), new Vector2(-1f, -1f)); //Top left
+            data[4] = new Vertex(new Vector2(0.5f, 0.5f), new Vector2(1f, 1f));     //Bottom right
+            data[5] = new Vertex(new Vector2(-0.5f, 0.5f), new Vector2(-1f, 1f));   //Bottom left
+
+            //Write the data to the buffer
+            wgpu.QueueWriteBuffer(queue, _VertexBuffer, 0, data, (nuint) _VertexBufferSize);
+
+            var commandEncoderDescriptor = new CommandEncoderDescriptor();
+
+            //Create a new command encoder
+            var commandEncoder = wgpu.DeviceCreateCommandEncoder(_Device, ref commandEncoderDescriptor);
+
+            var commandBufferDescriptor = new CommandBufferDescriptor();
+
+            //Finish the command encoder
+            var commandBuffer = wgpu.CommandEncoderFinish(commandEncoder, ref commandBufferDescriptor);
+
+            wgpu.QueueSubmit(queue, 1, &commandBuffer);
+        } //Create vertex buffer
+
         CreateSwapchain();
     }
-
-    private static void WindowClosing()
-    {
-        _WebGpuDisposal.Dispose(_Shader);
-        _WebGpuDisposal.Dispose(_Pipeline);
-        _WebGpuDisposal.Dispose(_Device);
-        _WebGpuDisposal.Dispose(_Adapter);
-        _WebGpuDisposal.Dispose(_Surface);
-
-        wgpu.Dispose();
-    }
-
-    private static void CreateSwapchain()
-    {
-        var swapChainDescriptor = new SwapChainDescriptor
-        {
-            Usage       = TextureUsage.RenderAttachment,
-            Format      = _SwapChainFormat,
-            Width       = (uint) _Window.FramebufferSize.X,
-            Height      = (uint) _Window.FramebufferSize.Y,
-            PresentMode = PresentMode.Fifo
-        };
-
-        _SwapChain = wgpu.DeviceCreateSwapChain(_Device, _Surface, ref swapChainDescriptor);
-    }
-
-    private static void WindowOnUpdate(double delta) {}
 
     private static void WindowOnRender(double delta)
     {
@@ -286,7 +354,9 @@ fn fs_main() -> @location(0) vec4<f32> {
         var renderPass = wgpu.CommandEncoderBeginRenderPass(encoder, ref renderPassDescriptor);
 
         wgpu.RenderPassEncoderSetPipeline(renderPass, _Pipeline);
-        wgpu.RenderPassEncoderDraw(renderPass, 3, 1, 0, 0);
+        wgpu.RenderPassEncoderSetVertexBuffer(renderPass, 0, _VertexBuffer, 0, _VertexBufferSize);
+        wgpu.RenderPassEncoderDraw(renderPass, 6, 1, 0, 0);
+
         wgpu.RenderPassEncoderEnd(renderPass);
         _WebGpuDisposal.Dispose(nextTexture);
 
@@ -298,7 +368,37 @@ fn fs_main() -> @location(0) vec4<f32> {
 
         wgpu.QueueSubmit(queue, 1, &commandBuffer);
         wgpu.SwapChainPresent(_SwapChain);
-        // _Window.SwapBuffers();
+        _Window.SwapBuffers();
+    }
+
+    private static void WindowClosing()
+    {
+        _WebGpuDisposal.Dispose(_Shader);
+        _WebGpuDisposal.Dispose(_Pipeline);
+        _WebGpuDisposal.Dispose(_Device);
+        _WebGpuDisposal.Dispose(_Adapter);
+        _WebGpuDisposal.Dispose(_Surface);
+
+        wgpu.Dispose();
+    }
+
+    private static void FramebufferResize(Vector2D<int> obj)
+    {
+        CreateSwapchain();
+    }
+
+    private static void CreateSwapchain()
+    {
+        var swapChainDescriptor = new SwapChainDescriptor
+        {
+            Usage       = TextureUsage.RenderAttachment,
+            Format      = _SwapChainFormat,
+            Width       = (uint) _Window.FramebufferSize.X,
+            Height      = (uint) _Window.FramebufferSize.Y,
+            PresentMode = PresentMode.Fifo
+        };
+
+        _SwapChain = wgpu.DeviceCreateSwapChain(_Device, _Surface, ref swapChainDescriptor);
     }
 
     private static void PrintAdapterFeatures()
