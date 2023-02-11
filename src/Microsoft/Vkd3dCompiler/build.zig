2dd80751271b639b74932b6015780daba2e3fa82
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-02-11 00:10:54 -0800

    Link against SPIRV-Tools-shared

--- src/Microsoft/Vkd3dCompiler/build.zig
+++ src/Microsoft/Vkd3dCompiler/build.zig
@@ -12,9 +12,10 @@ pub fn build(b: *std.Build) void {
     };
 
     const vkd3d_shim: *std.build.LibExeObjStep = b.addSharedLibrary(shared_lib_options);
-    // scetool.linkSystemLibrary("c++");
     vkd3d_shim.addLibraryPath(".");
     vkd3d_shim.linkSystemLibraryName("vkd3d-shader");
+    //Since we statically link with vkd3d-shader, we need to dynamically link against SPIRV-Tools ourselves
+    vkd3d_shim.linkSystemLibraryName("SPIRV-Tools-shared");
     vkd3d_shim.linkLibC();
 
     vkd3d_shim.addCSourceFiles(&.{"lib.c"}, &.{"-std=c99", "-fPIC"});
