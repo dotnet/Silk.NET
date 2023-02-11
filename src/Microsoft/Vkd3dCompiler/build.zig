0f67f9bb92a16dfbd4371c6c75bc7ade76458275
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-02-11 00:12:34 -0800

    vkd3d_compiler: vkd3d_shim -> vkd3d_compiler in build scripts

--- src/Microsoft/Vkd3dCompiler/build.zig
+++ src/Microsoft/Vkd3dCompiler/build.zig
@@ -11,14 +11,14 @@ pub fn build(b: *std.Build) void {
         .optimize = mode,
     };
 
-    const vkd3d_shim: *std.build.LibExeObjStep = b.addSharedLibrary(shared_lib_options);
-    vkd3d_shim.addLibraryPath(".");
-    vkd3d_shim.linkSystemLibraryName("vkd3d-shader");
+    const vkd3d_compiler: *std.build.LibExeObjStep = b.addSharedLibrary(shared_lib_options);
+    vkd3d_compiler.addLibraryPath(".");
+    vkd3d_compiler.linkSystemLibraryName("vkd3d-shader");
     //Since we statically link with vkd3d-shader, we need to dynamically link against SPIRV-Tools ourselves
-    vkd3d_shim.linkSystemLibraryName("SPIRV-Tools-shared");
-    vkd3d_shim.linkLibC();
+    vkd3d_compiler.linkSystemLibraryName("SPIRV-Tools-shared");
+    vkd3d_compiler.linkLibC();
 
-    vkd3d_shim.addCSourceFiles(&.{"lib.c"}, &.{"-std=c99", "-fPIC"});
-    vkd3d_shim.addSystemIncludePath("../../../build/submodules/vkd3d/include/");
-    vkd3d_shim.install();
+    vkd3d_compiler.addCSourceFiles(&.{"lib.c"}, &.{ "-std=c99", "-fPIC" });
+    vkd3d_compiler.addSystemIncludePath("../../../build/submodules/vkd3d/include/");
+    vkd3d_compiler.install();
 }
