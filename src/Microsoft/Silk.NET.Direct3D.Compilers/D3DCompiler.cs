b12061231231b58bc2b2fd6893f315ad567ac6e2
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-02-11 00:16:06 -0800

    D3DCompiler: Register SPIRV-Tools as a dependency on non-windows platforms

--- src/Microsoft/Silk.NET.Direct3D.Compilers/D3DCompiler.cs
+++ src/Microsoft/Silk.NET.Direct3D.Compilers/D3DCompiler.cs
@@ -11,6 +11,16 @@ namespace Silk.NET.Direct3D.Compilers
 {
     public partial class D3DCompiler
     {
+        static D3DCompiler()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var loader = LibraryLoader.GetPlatformDefaultLoader();
+                //d3dcompile_vkd3d on linux has a shared dependency on SPIRV-Tools, so register it
+                loader.RegisterDependencies("libSPIRV-Tools-shared.so");
+            }
+        }
+
         public static D3DCompiler GetApi()
         {
             return new D3DCompiler(CreateDefaultContext(new D3DCompilerLibraryNameContainer().GetLibraryNames()));
