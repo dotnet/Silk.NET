46d2d11f896021cd5f056ccb37d96fc02d0b9cda
Dylan Perks <dmp9biz@gmail.com>
2023-02-20 15:26:38 +0000

    Review comment, apply NativeAOT fix only on NS21+

--- src/Windowing/Silk.NET.Windowing.Common/Interfaces/IWindowPlatform.cs
+++ src/Windowing/Silk.NET.Windowing.Common/Interfaces/IWindowPlatform.cs
@@ -10,10 +10,12 @@ namespace Silk.NET.Windowing
     /// </summary>
     public interface IWindowPlatform : IWindowHost
     {
+#if NETSTANDARD2_1
         /// <summary>
         /// Gets the name of this window platform.
         /// </summary>
         string Name => GetType().Name;
+#endif
 
         bool IsViewOnly { get; }
 
