c08c023a7b65ee6bf42e9ee3e0331bffbd5a4860
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-03-25 17:12:45 -0700

    SilkTouch: Use fallible codepath for multiple libname fallback

--- src/Core/Silk.NET.SilkTouch/NativeContextOverrideGeneration.cs
+++ src/Core/Silk.NET.SilkTouch/NativeContextOverrideGeneration.cs
@@ -49,7 +49,34 @@ namespace Silk.NET.SilkTouch
                                     IdentifierName("n")))))
             );
 
-            StatementSyntax arrLast = last;
+            StatementSyntax arrLast = IfStatement
+            (
+                InvocationExpression
+                (
+                    ParseName("Silk.NET.Core.Contexts.DefaultNativeContext.TryCreate"),
+                    ArgumentList
+                    (
+                        SeparatedList
+                        (
+                            new []
+                            {
+                                Argument(IdentifierName("name")),
+                                Argument
+                                (
+                                    null,
+                                    Token(SyntaxKind.OutKeyword),
+                                    DeclarationExpression
+                                    (
+                                        IdentifierName("DefaultNativeContext"),
+                                        SingleVariableDesignation(Identifier("context"))
+                                    )
+                                )
+                            }
+                        )
+                    )
+                ),
+                ReturnStatement(IdentifierName("context"))
+            );
 
             foreach (var (attSymbol, attId, lib, @override) in overrides.OrderBy(x => x.Item2))
             {
@@ -63,39 +90,14 @@ namespace Silk.NET.SilkTouch
                         var matchId = (int) x2.ConstructorArguments[0].Value!;
                         return matchId != attId;
                     })).ToArray(), comp.SyntaxTrees?.FirstOrDefault()?.IsNet5OrGreater() ?? false)));
+
                 arrLast = IfStatement
                 (
-                    InvocationExpression
+                    BinaryExpression
                     (
-                        MemberAccessExpression
-                        (
-                            SyntaxKind.SimpleMemberAccessExpression,
-                            IdentifierName("System.Linq.Enumerable"),
-                            IdentifierName("Contains")
-                        ),
-                        ArgumentList
-                        (
-                            SeparatedList
-                            (
-                                new []
-                                {
-                                    Argument
-                                    (
-                                        IdentifierName("n")
-                                    ),
-                                    Argument
-                                    (
-                                        LiteralExpression
-                                        (
-                                            SyntaxKind.StringLiteralExpression,
-                                            Literal(lib)
-                                        )
-                                    )
-                                }
-                            )
-                        )
-                    ),
-                    ReturnStatement(ObjectCreationExpression(IdentifierName(name), ArgumentList(), null)),
+                        SyntaxKind.EqualsExpression, IdentifierName("name"),
+                        LiteralExpression(SyntaxKind.StringLiteralExpression, Literal(lib))
+                    ), ReturnStatement(ObjectCreationExpression(IdentifierName(name), ArgumentList(), null)),
                     ElseClause(arrLast)
                 );
 
@@ -140,7 +142,7 @@ namespace Silk.NET.SilkTouch
                                             AttributeArgument
                                             (
                                                 LiteralExpression(SyntaxKind.StringLiteralExpression,
-                                                Literal("This function is obsolete!")
+                                                Literal("This function is obsolete! Please use the string[] overload!")
                                             )
                                         )
                                     )
@@ -150,6 +152,10 @@ namespace Silk.NET.SilkTouch
                     )
                 ))
             );
+
+            //wrap arrLast into its foreach loop
+            //eg `foreach(string name in n)`
+            arrLast = ForEachStatement(PredefinedType(Token(SyntaxKind.StringKeyword)), Identifier("name"), IdentifierName("n"), Block(arrLast));
 
             members.Add
             (
@@ -163,7 +169,34 @@ namespace Silk.NET.SilkTouch
                             (Parameter(Identifier("n")).WithType(ArrayType(PredefinedType(Token(SyntaxKind.StringKeyword)), SingletonList(ArrayRankSpecifier()))))
                         )
                     )
-                    .WithBody(Block(arrLast))
+                .WithBody
+                (
+                    Block
+                    (
+                        arrLast,
+                        ThrowStatement
+                        (
+                            ObjectCreationExpression(IdentifierName("System.IO.FileNotFoundException"))
+                                .WithArgumentList
+                                (
+                                    ArgumentList
+                                    (
+                                        SingletonSeparatedList
+                                        (
+                                            Argument
+                                            (
+                                                LiteralExpression
+                                                (
+                                                    SyntaxKind.StringLiteralExpression,
+                                                    Literal("Could not load from any of the possible library names! Please make sure that the library is installed and in the right place!")
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                        )
+                    )
+                )
             );
         }
 
