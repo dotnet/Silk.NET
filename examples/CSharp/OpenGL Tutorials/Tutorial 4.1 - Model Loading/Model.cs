d18b5cd6fdbfbd1e0a3db8481b4f01c043c57fb5
Rumtery <artromskiy@gmail.com>
2023-05-08 02:01:49 +0300

    Fix of texcoord

--- examples/CSharp/OpenGL Tutorials/Tutorial 4.1 - Model Loading/Model.cs
+++ examples/CSharp/OpenGL Tutorials/Tutorial 4.1 - Model Loading/Model.cs
@@ -89,7 +89,7 @@ namespace Tutorial
                     // a vertex can contain up to 8 different texture coordinates. We thus make the assumption that we won't 
                     // use models where a vertex can have multiple texture coordinates so we always take the first set (0).
                     Vector3 texcoord3 = mesh->MTextureCoords[0][i];
-                    vertex.TexCoords = new Vector2(texcoord3.X, texcoord3.y);
+                    vertex.TexCoords = new Vector2(texcoord3.X, texcoord3.Y);
                 }
 
                 vertices.Add(vertex);
