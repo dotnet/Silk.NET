ddd78acdf1f5eed46272cb1f4a5e48eafdd5127a
Skye <ollie@ollierobinson.co.uk>
2023-07-22 15:27:39 +0100

    started GL textured cube demo

---
+++ examples/CSharp/OpenGL Demos/SelfContainedTexturedCube/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+
+namespace SelfContainedTexturedCube;
+
+public class Program
+{
+    private static IWindow _window;
+    private static GL _gl;
+    
+    public static void Main(string[] args)
+    {
+        WindowOptions options = WindowOptions.Default with
+        {
+            Title = "Self Contained Textured Cube Demo",
+            Size = new Vector2D<int>(1280, 720)
+        };
+
+        _window = Window.Create(options);
+        _window.Load += Initialize;
+        _window.Update += Update;
+        _window.Render += Draw;
+        
+        _window.Run();
+        
+        _window.Dispose();
+    }
+
+    private static void Initialize()
+    {
+        _gl = _window.CreateOpenGL();
+
+        _gl.ClearColor(Color.Indigo);
+    }
+
+    private static void Update(double dt)
+    {
+        
+    }
+
+    private static void Draw(double dt)
+    {
+        _gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+    }
+}
