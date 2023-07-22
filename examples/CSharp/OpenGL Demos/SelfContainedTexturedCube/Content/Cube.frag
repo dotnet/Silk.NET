99ec825443388cf2e5cd7680a4ac131df32c8432
Skye <ollie@ollierobinson.co.uk>
2023-07-22 17:05:04 +0100

    GL textured cube done & working

--- examples/CSharp/OpenGL Demos/SelfContainedTexturedCube/Content/Cube.frag
+++ examples/CSharp/OpenGL Demos/SelfContainedTexturedCube/Content/Cube.frag
@@ -4,7 +4,9 @@ in vec2 frag_texCoord;
 
 out vec4 out_color;
 
+layout (binding = 2) uniform sampler2D Texture;
+
 void main()
 {
-    out_color = vec4(1.0, 0.5, 0.25, 1.0);
+    out_color = texture(Texture, frag_texCoord);
 }
--- examples/CSharp/OpenGL Demos/SelfContainedTexturedCube/Program.cs
+++ examples/CSharp/OpenGL Demos/SelfContainedTexturedCube/Program.cs
@@ -8,6 +8,7 @@ using System.Numerics;
 using Silk.NET.Maths;
 using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
+using StbImageSharp;
 
 namespace SelfContainedTexturedCube;
 
@@ -59,24 +60,36 @@ public class Program
 
         Cube cube = new Cube();
 
+        // Create vertex + index buffers.
         _vbo = _gl.GenBuffer();
         _ebo = _gl.GenBuffer();
         
+        // Bind the buffers to their respective targets.
         _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _vbo);
         _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, _ebo);
 
+        // Upload the vertices and indices.
+        // Note that the vertices are a struct comprising of a Vector3 and Vector2.
+        // OpenGL supports this, as byte for byte, this will be exactly the same as just giving it an array of floats,
+        // and doing it this way makes it much easier to understand.
         fixed (VertexPositionTexture* vertices = cube.Vertices)
             _gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (cube.Vertices.Length * sizeof(VertexPositionTexture)), vertices, BufferUsageARB.StaticDraw);
 
         fixed (uint* indices = cube.Indices)
             _gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint) (cube.Indices.Length * sizeof(uint)), indices, BufferUsageARB.StaticDraw);
 
+        // Create our camera struct.
         CameraInfo camera = new CameraInfo()
         {
             ProjectionMatrix = Matrix4x4.CreatePerspectiveFieldOfView(75 * MathF.PI / 180, _window.Size.X / (float) _window.Size.Y, 0.1f, 100f),
-            ViewMatrix = Matrix4x4.CreateLookAt(new Vector3(0, -3, 0), Vector3.Zero, Vector3.UnitX)
+            ViewMatrix = Matrix4x4.CreateLookAt(new Vector3(0, -2, 0), Vector3.Zero, Vector3.UnitX)
         };
 
+        // Uniform buffers! A very powerful tool.
+        // In short - they are a buffer containing data, much like any other buffer, however can be bound to a uniform
+        // in the shader. The advantage of this is that you can update the buffer once, and use it in any shader that
+        // wants to use it, without updating a ton of uniforms every time.
+        // They are also faster, with the tradeoff of slightly increased memory usage.
         _cameraInfoBuffer = _gl.GenBuffer();
         _gl.BindBuffer(BufferTargetARB.UniformBuffer, _cameraInfoBuffer);
         _gl.BufferData(BufferTargetARB.UniformBuffer, (nuint) sizeof(CameraInfo), &camera, BufferUsageARB.StaticDraw);
@@ -87,38 +100,86 @@ public class Program
         _gl.BindBuffer(BufferTargetARB.UniformBuffer, _modelMatrixBuffer);
         _gl.BufferData(BufferTargetARB.UniformBuffer, (nuint) sizeof(Matrix4x4), &modelMatrix, BufferUsageARB.DynamicDraw);
 
+        // Uniform buffers, unlike VBOs and EBOs, are NOT part of the VAO. As such, we can unbind it here.
         _gl.BindBuffer(BufferTargetARB.UniformBuffer, 0);
 
+        // Create our vertex and fragment shaders by calling the helper function.
         uint vertexShader = CreateShader(ShaderType.VertexShader, File.ReadAllText("Content/Cube.vert"));
         uint fragmentShader = CreateShader(ShaderType.FragmentShader, File.ReadAllText("Content/Cube.frag"));
 
+        // Create our program, attach shaders, and link.
         _program = _gl.CreateProgram();
         _gl.AttachShader(_program, vertexShader);
         _gl.AttachShader(_program, fragmentShader);
         _gl.LinkProgram(_program);
 
+        // Check to make sure it linked correctly.
         _gl.GetProgram(_program, ProgramPropertyARB.LinkStatus, out int status);
         if (status != (int) GLEnum.True)
             throw new Exception($"Failed to link program! {_gl.GetProgramInfoLog(_program)}");
 
+        // Detach and delete our shaders, as we no longer need them.
         _gl.DetachShader(_program, vertexShader);
         _gl.DetachShader(_program, fragmentShader);
         _gl.DeleteShader(vertexShader);
         _gl.DeleteShader(fragmentShader);
 
-        _gl.UseProgram(_program);
-
+        // Enable and setup our position attribute (aPosition).
         const uint positionAttribLocation = 0;
         _gl.EnableVertexAttribArray(positionAttribLocation);
         _gl.VertexAttribPointer(positionAttribLocation, 3, VertexAttribPointerType.Float, false, (uint) sizeof(VertexPositionTexture), (void*) 0);
 
+        // Enable and setup our texture coordinate attribute (aTexCoord).
         const uint texCoordAttribLocation = 1;
         _gl.EnableVertexAttribArray(texCoordAttribLocation);
         _gl.VertexAttribPointer(texCoordAttribLocation, 2, VertexAttribPointerType.Float, false, (uint) sizeof(VertexPositionTexture), (void*) 12);
 
+        // Unbind our vertex array, and then unbind our buffers.
+        // Note: If you unbind any of the buffers before you unbind the vertex array, problems will occur.
+        // In this case, at best the cube will just not render, and at worst a graphics driver crash may occur. 
         _gl.BindVertexArray(0);
         _gl.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
         _gl.BindBuffer(GLEnum.ElementArrayBuffer, 0);
+        
+        // Load our texture data.
+        ImageResult image = ImageResult.FromMemory(File.ReadAllBytes("Content/awesomeface.png"), ColorComponents.RedGreenBlueAlpha);
+        
+        // Create & bind our texture to Texture2D. 
+        _texture = _gl.GenTexture();
+        _gl.BindTexture(TextureTarget.Texture2D, _texture);
+
+        // Give the texture its data.
+        fixed (byte* imgData = image.Data)
+            _gl.TexImage2D(TextureTarget.Texture2D, 0, InternalFormat.Rgba, (uint) image.Width, (uint) image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, imgData);
+
+        // S, T == U, V
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int) TextureWrapMode.Repeat);
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int) TextureWrapMode.Repeat);
+
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int) TextureMinFilter.LinearMipmapLinear);
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int) TextureMagFilter.Linear);
+
+        // Every GPU should have had anisotropic filtering for decades (Direct3D required support since at least D3D9).
+        // Unfortunately OpenGL did not add it into the official spec until 4.6 (released in 2017!!!)
+        // Therefore, we should check to make sure it is present regardless. 
+        if (_gl.IsExtensionPresent("ARB_texture_filter_anisotropic"))
+            _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMaxAnisotropy, 16);
+
+        // Generate mipmaps. If you don't do this, the texture will be black.
+        // There are workarounds, but that is beyond the scope of this demo.
+        _gl.GenerateMipmap(TextureTarget.Texture2D);
+
+        // Finally, unbind our texture.
+        _gl.BindTexture(TextureTarget.Texture2D, 0);
+        
+        _gl.Enable(EnableCap.DepthTest);
+        _gl.DepthFunc(DepthFunction.Lequal);
+
+        // Enable face culling - telling it to cull the back face, and telling it that our vertices are in the clockwise
+        // direction.
+        _gl.Enable(EnableCap.CullFace);
+        _gl.FrontFace(FrontFaceDirection.CW);
+        _gl.CullFace(TriangleFace.Back);
         
         _rotation = Quaternion.Identity;
     }
@@ -126,11 +187,13 @@ public class Program
     private static void Update(double dt)
     {
         _rotation *= Quaternion.CreateFromAxisAngle(Vector3.UnitY, 1 * (float) dt) *
-                     Quaternion.CreateFromAxisAngle(Vector3.UnitX, 1f * (float) dt);
+                     Quaternion.CreateFromAxisAngle(Vector3.UnitX, 0.75f * (float) dt) *
+                     Quaternion.CreateFromAxisAngle(Vector3.UnitZ, 0.5f * (float) dt);
     }
 
     private static unsafe void Draw(double dt)
     {
+        // Update our model matrix buffer.
         Matrix4x4 modelMatrix = Matrix4x4.CreateFromQuaternion(_rotation);
         _gl.BindBuffer(BufferTargetARB.UniformBuffer, _modelMatrixBuffer);
         _gl.BufferSubData(BufferTargetARB.UniformBuffer, 0, (nuint) sizeof(Matrix4x4), &modelMatrix);
@@ -141,6 +204,10 @@ public class Program
         _gl.BindVertexArray(_vao);
         _gl.UseProgram(_program);
 
+        _gl.ActiveTexture(TextureUnit.Texture2);
+        _gl.BindTexture(TextureTarget.Texture2D, _texture);
+
+        // Bind our uniform buffers to their given binding points in the shader.
         _gl.BindBufferBase(BufferTargetARB.UniformBuffer, 0, _cameraInfoBuffer);
         _gl.BindBufferBase(BufferTargetARB.UniformBuffer, 1, _modelMatrixBuffer);
         
