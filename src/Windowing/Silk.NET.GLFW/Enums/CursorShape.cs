bb13b6b04b418a5082e1afca852a8e10a1235b4b
Dylan Perks <11160611+Perksey@users.noreply.github.com>
2024-01-30 12:35:07 -0600

    Add some missing enums GLFW (#1924)

--- src/Windowing/Silk.NET.GLFW/Enums/CursorShape.cs
+++ src/Windowing/Silk.NET.GLFW/Enums/CursorShape.cs
@@ -36,6 +36,26 @@ namespace Silk.NET.GLFW
         /// <summary>
         /// The vertical resize shape. Used when mousing over something that can be vertically resized.
         /// </summary>
-        VResize = 0x00036006
+        VResize = 0x00036006,
+
+        /// <summary>
+        /// The top-left to bottom-right diagonal resize/move arrow shape.
+        /// </summary>
+        NwseResize = 0x00036007,
+
+        /// <summary>
+        /// The top-right to bottom-left diagonal resize/move arrow shape.
+        /// </summary>
+        NeswResize = 0x00036008,
+
+        /// <summary>
+        /// The omni-directional resize/move cursor shape.
+        /// </summary>
+        AllResize = 0x00036009,
+
+        /// <summary>
+        /// The operation-not-allowed shape.
+        /// </summary>
+        NotAllowed = 0x0003600A
     }
 }
