6045dd51e344a7a31ee422daa78c47c3ec2c9dd1
Beyley Thomas <ep1cm1n10n123@gmail.com>
2024-03-23 21:47:40 -0700

    modernize our vkd3d shader compiler build

--- src/Microsoft/Vkd3dCompiler/build.zig
+++ src/Microsoft/Vkd3dCompiler/build.zig
@@ -1,24 +1,39 @@
 const std = @import("std");
 const fs = std.fs;
 
+const vkd3d_folder = "../../../build/submodules/vkd3d/";
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardOptimizeOption(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    const shared_lib_options: std.build.SharedLibraryOptions = .{
+    const vkd3d_compiler = b.addSharedLibrary(.{
         .name = "d3dcompile_vkd3d",
         .target = target,
-        .optimize = mode,
-    };
+        .optimize = optimize,
+    });
 
-    const vkd3d_compiler: *std.build.LibExeObjStep = b.addSharedLibrary(shared_lib_options);
-    vkd3d_compiler.addLibraryPath(".");
-    vkd3d_compiler.linkSystemLibraryName("vkd3d-shader");
-    //Since we statically link with vkd3d-shader, we need to dynamically link against SPIRV-Tools ourselves
-    vkd3d_compiler.linkSystemLibraryName("SPIRV-Tools-shared");
+    //In ReleaseSmall builds, strip the code
+    if (optimize == .ReleaseSmall)
+        vkd3d_compiler.root_module.strip = true;
+
+    vkd3d_compiler.addLibraryPath(.{ .path = "." });
+
     vkd3d_compiler.linkLibC();
 
-    vkd3d_compiler.addCSourceFiles(&.{"lib.c"}, &.{ "-std=c99", "-fPIC" });
-    vkd3d_compiler.addSystemIncludePath("../../../build/submodules/vkd3d/include/");
+    vkd3d_compiler.linkSystemLibrary2("vkd3d-shader", .{ .preferred_link_mode = .static });
+
+    //Since we statically link with vkd3d-shader, we need to dynamically link against SPIRV-Tools ourselves
+    vkd3d_compiler.linkSystemLibrary("SPIRV-Tools-shared");
+
+    vkd3d_compiler.addIncludePath(.{ .path = vkd3d_folder ++ "include/" });
+    vkd3d_compiler.addIncludePath(.{ .path = vkd3d_folder ++ "include/private/" });
+    vkd3d_compiler.addIncludePath(.{ .path = vkd3d_folder ++ "libs/vkd3d-utils/" });
+
+    vkd3d_compiler.addCSourceFiles(.{
+        .files = &.{vkd3d_folder ++ "libs/vkd3d-utils/vkd3d_utils_main.c"},
+        .flags = &.{ "-std=c99", "-fPIC" },
+    });
+
     b.installArtifact(vkd3d_compiler);
 }
