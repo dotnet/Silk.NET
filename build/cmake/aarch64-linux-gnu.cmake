39b610269a8288bb4c57b21f0f5416f0373607d4
Alex Rønne Petersen <alex@alexrp.com>
2024-04-18 20:56:21 +0200

    Native: Set CMAKE_ASM_COMPILER in all toolchain files.

--- build/cmake/aarch64-linux-gnu.cmake
+++ build/cmake/aarch64-linux-gnu.cmake
@@ -1,5 +1,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(CMAKE_ASM_COMPILER "/usr/bin/aarch64-linux-gnu-as")
 set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc")
 set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++")
 set(CMAKE_LINKER "/usr/bin/aarch64-linux-gnu-ld")
--- build/cmake/x86_64-linux-gnu.cmake
+++ build/cmake/x86_64-linux-gnu.cmake
@@ -1,5 +1,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_ASM_COMPILER "/usr/bin/x86_64-linux-gnu-as")
 set(CMAKE_C_COMPILER "/usr/bin/x86_64-linux-gnu-gcc")
 set(CMAKE_CXX_COMPILER "/usr/bin/x86_64-linux-gnu-g++")
 set(CMAKE_LINKER "/usr/bin/x86_64-linux-gnu-ld")
