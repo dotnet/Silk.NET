609c8c10eebfaa31b86d2a62ab01bc4566ac0047
Dylan Perks <11160611+Perksey@users.noreply.github.com>
2024-06-06 14:11:20 -0500

    Override ANDROID_HOME? idk

--- build/nuke/Native/Core.cs
+++ build/nuke/Native/Core.cs
@@ -29,7 +29,7 @@ using static Nuke.Common.Tools.GitHub.GitHubTasks;
 partial class Build {
     [Nuke.Common.Parameter("Build native code")] readonly bool Native;
 
-    [CanBeNull] string AndroidHomeValue;
+    [Parameter("Android home. Will be determined from dotnet if not provided.")] [CanBeNull] string AndroidHomeValue;
 
     static string JobsArg => string.IsNullOrWhiteSpace(GitHubActions.Instance?.Job)
         ? $" -j{Jobs}"
