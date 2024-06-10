408e35fd72bcd14c45bd056c9002b70f50d2bf42
The Silk.NET Automaton <9011267+dotnet-bot@users.noreply.github.com>
2024-06-10 02:43:54 +0000

    New binaries for SDL2 on Microsoft Windows 10.0.20348

---
+++ src/Windowing/SilkDroid/app/build/generated/source/buildConfig/debug/org/libsdl/app/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.libsdl.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "org.libsdl.app";
+  public static final String BUILD_TYPE = "debug";
+}
