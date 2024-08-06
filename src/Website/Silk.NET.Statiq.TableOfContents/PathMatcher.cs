532cebdb380374fa32cac83ee0ce2d1102145710
lumi2021 <94462076+lumi2021@users.noreply.github.com>
2024-08-06 12:32:14 -0300

    Cleanup

--- src/Website/Silk.NET.Statiq.TableOfContents/PathMatcher.cs
+++
@@ -1,9 +0,0 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using Statiq.Common;
-
-namespace Silk.NET.Statiq.TableOfContents
-{
-    public delegate bool PathMatcher(NormalizedPath pathInToc, NormalizedPath candidatePath);
-}
