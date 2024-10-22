648c07b714daa8d990e09768a82dc410d0ad8046
Dylan Perks <dylanperks23@gmail.com>
2024-10-22 22:42:28 +0100

    May have figured it out?

--- build/nuke/Native/SilkDroid.cs
+++ build/nuke/Native/SilkDroid.cs
@@ -103,6 +103,11 @@ partial class Build {
                         SourceDirectory / "Windowing" / "Silk.NET.Windowing.Sdl" / "Android" / "app-release.aar",
                         FileExistsPolicy.Overwrite
                     );
+
+                    // Not expecting this to succeed, but we need to do this so we generate the bindings to go in the public API.
+                    InheritedShell($"dotnet build \"{SourceDirectory / "Windowing" / "Silk.NET.Windowing.Sdl" / "Silk.NET.Windowing.Sdl.csproj"}\"").AssertWaitForExit();
+
+                    // Update the public API.
                     InheritedShell(string.Format(FormatDeclCmd, SourceDirectory / "Windowing" / "Silk.NET.Windowing.Sdl" / "Silk.NET.Windowing.Sdl.csproj")).AssertZeroExitCode();
                     return ret;
                 }
