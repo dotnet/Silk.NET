73f33551f6e144e94ca524a6662ce97104971ba9
Dylan Perks <dylanperks23@gmail.com>
2024-10-28 22:35:39 +0000

    Fix NewsstandKit issue again

--- src/Lab/Experiments/TriangleNET6/Program.cs
+++ src/Lab/Experiments/TriangleNET6/Program.cs
@@ -1,6 +1,14 @@
-#if !__ANDROID__
-#if __IOS__
-Silk.NET.Input.Sdl.SdlInput.RegisterPlatform();
-#endif
+#if !__IOS__ && !__ANDROID__
 Triangle.Program.Run();
+#elif __IOS__
+using Silk.NET.Windowing;
+using Silk.NET.Windowing.Sdl.iOS;
+using Silk.NET.Input.Sdl;
+
+SdlInput.RegisterPlatform();
+SilkMobile.RunApp(args, _ =>
+{
+    Triangle.Program.API = new GraphicsAPI(ContextAPI.OpenGLES, ContextProfile.Core, ContextFlags.Default, new APIVersion(3, 0));
+    Triangle.Program.Run();
+});
 #endif
