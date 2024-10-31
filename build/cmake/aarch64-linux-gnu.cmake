ca28a8fcbd1a620ad713a9c20c01d4570a0d6570
Dylan Perks <11160611+Perksey@users.noreply.github.com>
2024-10-31 13:56:06 +0000

    What if we just use GCC as the assembler

--- build/cmake/aarch64-linux-gnu.cmake
+++ build/cmake/aarch64-linux-gnu.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME "Linux")
 set(CMAKE_SYSTEM_PROCESSOR "aarch64")
 
-set(CMAKE_ASM_COMPILER "/usr/bin/aarch64-linux-gnu-as")
+set(CMAKE_ASM_COMPILER "/usr/bin/aarch64-linux-gnu-gcc")
 set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc")
 set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++")
 
