056c800d57edc6ca98dee7b6125a1d367c5bcce8
Exanite <exanite78012@gmail.com>
2024-12-06 02:15:18 -0500

    Add test cases for testing string encoding

--- src/Core/Silk.NET.Core.Tests/TestSilkMarshal.cs
+++ src/Core/Silk.NET.Core.Tests/TestSilkMarshal.cs
@@ -1,12 +1,51 @@
+using System.Collections.Generic;
+using Silk.NET.Core.Native;
 using Xunit;
 
 namespace Silk.NET.Core.Tests;
 
 public class TestSilkMarshal
 {
-    [Fact]
-    public unsafe void Test()
+    private readonly List<NativeStringEncoding> encodings = new()
     {
-        Assert.Equal(1, 1);
+        NativeStringEncoding.BStr,
+        NativeStringEncoding.LPStr,
+        NativeStringEncoding.LPTStr,
+        NativeStringEncoding.LPUTF8Str,
+        NativeStringEncoding.LPWStr,
+    };
+    
+    [Fact]
+    public void TestEncodingString()
+    {
+        var input = "Hello world";
+        foreach (var encoding in encodings)
+        {
+            var pointer = SilkMarshal.StringToPtr(input, encoding);
+            var roundTrip = SilkMarshal.PtrToString(pointer, encoding);
+            Assert.Equal(input, roundTrip);
+        }
+    }
+    
+    [Fact]
+    public void TestEncodingStringArray()
+    {
+        var inputs = new List<string>()
+        {
+            "Hello world",
+            "Foo",
+            "Bar",
+            "123",
+        };
+        
+        foreach (var encoding in encodings)
+        {
+            var pointer = SilkMarshal.StringArrayToPtr(inputs, encoding);
+            var roundTrip = SilkMarshal.PtrToStringArray(pointer, inputs.Count, encoding);
+            for (var i = 0; i < roundTrip.Length; i++)
+            {
+                Assert.Equal(inputs[i], roundTrip[i]);
+            }
+        }
     }
 }
