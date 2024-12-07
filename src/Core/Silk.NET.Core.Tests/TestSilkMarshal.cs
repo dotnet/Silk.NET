ee1d2f077f94cd0bbdd153f23d257ff753e635fc
William Chen <exanite78012@gmail.com>
2024-12-07 04:58:55 -0500

    Fix behavior of SilkMarshal.StringToPtr and related methods on Linux (#2377)
    * Add Silk.NET.Core.Tests project
    
    * Add test cases for testing string encoding
    
    * Fix issue where PtrToStringArray was ignoring the encoding parameter
    
    * Add test case for testing LPWStr char width
    
    * Use 4-byte LPWStrs on non-Windows platforms
    
    * Use Encoding.UTF32 for PtrToString
    
    * Simplify use of "when not Windows" clauses
    
    * Also use Encoding.UTF32 for StringIntoSpan
    
    * Update test cases and docs to show that LPWStr is UTF-32 on non-Windows
    
    See https://github.com/dotnet/Silk.NET/pull/2377
    
    * Don't test non-ascii characters in TestEncodingString/Array
    
    This is because LPStr doesn't always support non-ascii characters.
    
    ---------
    
    Co-authored-by: Dylan Perks <11160611+Perksey@users.noreply.github.com>

--- src/Core/Silk.NET.Core.Tests/TestSilkMarshal.cs
+++ src/Core/Silk.NET.Core.Tests/TestSilkMarshal.cs
@@ -1,4 +1,10 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
 using Silk.NET.Core.Native;
 using Xunit;
 
@@ -14,6 +20,44 @@ public class TestSilkMarshal
         NativeStringEncoding.LPUTF8Str,
         NativeStringEncoding.LPWStr,
     };
+    
+    private readonly Encoding lpwStrEncoding = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+        ? Encoding.Unicode
+        : Encoding.UTF32;
+
+    private readonly int lpwStrCharacterWidth = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? 2 : 4;
+
+    [Fact]
+    public unsafe void TestEncodingToLPWStr()
+    {
+        var input = "Hello world 🧵";
+        
+        var expectedByteCount = lpwStrEncoding.GetByteCount(input);
+        var expected = new byte[expectedByteCount + lpwStrCharacterWidth];
+        lpwStrEncoding.GetBytes(input, expected);
+        
+        var pointer = SilkMarshal.StringToPtr(input, NativeStringEncoding.LPWStr);
+        var pointerByteCount = lpwStrCharacterWidth * (int) SilkMarshal.StringLength(pointer, NativeStringEncoding.LPWStr);
+        
+        Assert.Equal(expected, new Span<byte>((void*)pointer, pointerByteCount + lpwStrCharacterWidth));
+    }
+ 
+    [Fact]
+    public unsafe void TestEncodingFromLPWStr()
+    {
+        var expected = "Hello world 🧵";
+        
+        var inputByteCount = lpwStrEncoding.GetByteCount(expected);
+        var input = new byte[inputByteCount + lpwStrCharacterWidth];
+        lpwStrEncoding.GetBytes(expected, input);
+
+        fixed (byte* pInput = input)
+        {
+            var output = SilkMarshal.PtrToString((nint)pInput, NativeStringEncoding.LPWStr);
+            
+            Assert.Equal(expected, output);
+        }
+    }
     
     [Fact]
     public void TestEncodingString()
