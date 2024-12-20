d82c7758cdbdf4cf0e6819ff334e3746f186dd2f
Dylan Perks <dylanperks23@gmail.com>
2024-12-20 22:58:53 +0000

    Initial work on native build workflow

---
+++ eng/native/buildsystem/test-workflow-stages.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env -S bash -eu
+
+export NATIVE_LIBRARY_SHORTHANDS="Sdl Aaaa Sdl B"
+export NATIVE_LIBRARY_PATHS="sources/SDL/Native sources/Aaaa/Native sources/SDL-Other/Native sources/B/Native"
+export NATIVE_LIBRARY_SUBMODULE_PATHS="eng/submodules/sdl eng/submodules/aaaa eng/submodules/sdl-other eng/submodules/b"
+export GITHUB_OUTPUT="test.txt"
+export PR_COMMENT=$'Hello\n/build-native sdl aaaa\nworld'
+
+rm -f test.txt
+./workflow-stage1.sh
+
+EXPECTED='workflow_filters<<EOF
+Aaaa: ["sources/Aaaa/Native/*", "eng/submodules/aaaa"]
+B: ["sources/B/Native/*", "eng/submodules/b"]
+Sdl: ["sources/SDL/Native/*", "eng/submodules/sdl", "sources/SDL-Other/Native/*", "eng/submodules/sdl-other"]
+EOF
+unique_targets=["Aaaa", "B", "Sdl"]
+targets_referenced=["Aaaa", "Sdl"]'
+
+if [[ "$(cat test.txt)" == "$EXPECTED" ]]; then
+    echo stage1 pass
+else
+    echo stage1 fail
+    cat test.txt
+fi
+
+export GH="echo"
+
+export NATIVE_LIBRARY_APPLICABLE_SHORTHANDS="Sdl Aaaa"
+export NATIVE_LIBRARY_USER_REFERENCED_SHORTHANDS="Sdl"
+
+rm -f test.txt
+GH_COMMENT="$(./workflow-stage2.sh)"
+
+EXPECTED='matrix_strategy<<EOF
+- target: Sdl
+  runtime: osx
+  exec: build-osx.sh
+  dir: sources/SDL/Native
+EOF'
+EXPECTED_GH_COMMENT="pr comment --body Some of the native library builds modified in this PR were not referenced in the PR description. Please ensure that the PR description contains \`/build-native Sdl Aaaa\`. These libraries won't be rebuilt without this being specified. If you believe this is in error, then please write a comment explaining why and ignore this suggestion. This comment will be automatically updated if rectified."
+if [[ "$(cat test.txt)" == "$EXPECTED" && "$EXPECTED_GH_COMMENT" == "$GH_COMMENT" ]]; then
+    echo stage2 with angry comment pass
+else
+    echo stage2 with angry comment fail
+    cat test.txt
+    echo $GH_COMMENT
+fi
+
+export NATIVE_LIBRARY_APPLICABLE_SHORTHANDS="Sdl Aaaa"
+export NATIVE_LIBRARY_USER_REFERENCED_SHORTHANDS="Sdl Aaaa"
+
+rm -f test.txt
+GH_COMMENT="$(./workflow-stage2.sh)"
+
+EXPECTED='matrix_strategy<<EOF
+- target: Sdl
+  runtime: osx
+  exec: build-osx.sh
+  dir: sources/SDL/Native
+EOF'
+EXPECTED_GH_COMMENT=""
+if [[ "$(cat test.txt)" == "$EXPECTED" && "$EXPECTED_GH_COMMENT" == "$GH_COMMENT" ]]; then
+    echo stage2 with no angry comment pass
+else
+    echo stage2 with no angry comment fail
+    cat test.txt
+    echo $GH_COMMENT
+fi
+
+export NATIVE_LIBRARY_APPLICABLE_SHORTHANDS="Sdl Aaaa"
+export NATIVE_LIBRARY_USER_REFERENCED_SHORTHANDS="Sdl Aaaa B"
+
+rm -f test.txt
+GH_COMMENT="$(./workflow-stage2.sh)"
+
+EXPECTED='matrix_strategy<<EOF
+- target: Sdl
+  runtime: osx
+  exec: build-osx.sh
+  dir: sources/SDL/Native
+EOF'
+EXPECTED_GH_COMMENT=""
+if [[ "$(cat test.txt)" == "$EXPECTED" && "$EXPECTED_GH_COMMENT" == "$GH_COMMENT" ]]; then
+    echo stage2 with no angry comment when we have extra targets pass
+else
+    echo stage2 with no angry comment when we have extra targets fail
+    cat test.txt
+    echo $GH_COMMENT
+fi
+
+export NATIVE_LIBRARY_APPLICABLE_SHORTHANDS="Sdl Aaaa"
+export NATIVE_LIBRARY_USER_REFERENCED_SHORTHANDS="Sdl Aaaa B"
+export PR_EXISTING_NOTICE_ID=123
+
+rm -f test.txt
+GH_COMMENT="$(./workflow-stage2.sh)"
+
+EXPECTED='matrix_strategy<<EOF
+- target: Sdl
+  runtime: osx
+  exec: build-osx.sh
+  dir: sources/SDL/Native
+EOF'
+EXPECTED_GH_COMMENT="pr comment 123 --body All native library builds modified in this PR shall attempt to be built by CI."
+if [[ "$(cat test.txt)" == "$EXPECTED" && "$EXPECTED_GH_COMMENT" == "$GH_COMMENT" ]]; then
+    echo stage2 with updating angry comment to not be angry pass
+else
+    echo stage2 with updating angry comment to not be angry fail
+    cat test.txt
+    echo $GH_COMMENT
+fi
---
+++ eng/native/buildsystem/workflow-stage2.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env -S bash -eu
+. workflow-common.sh
+
+# Now we determine the jobs we actually want to run
+IFS=' ' read -a NATIVE_LIBRARY_APPLICABLE_SHORTHANDS_ARRAY <<< "$NATIVE_LIBRARY_APPLICABLE_SHORTHANDS"
+IFS=' ' read -a NATIVE_LIBRARY_USER_REFERENCED_SHORTHANDS_ARRAY <<< "$NATIVE_LIBRARY_USER_REFERENCED_SHORTHANDS"
+
+COMMENT="Some of the native library builds modified in this PR were not referenced in the PR description. Please ensure that the PR description contains \`/build-native $NATIVE_LIBRARY_APPLICABLE_SHORTHANDS\`. These libraries won't be rebuilt without this being specified. If you believe this is in error, then please write a comment explaining why and ignore this suggestion. This comment will be automatically updated if rectified."
+if [[ ${PR_EXISTING_NOTICE_ID:-0} == 0 ]]; then
+    PR_EXISTING_NOTICE_ID=""
+fi
+
+ANGRY_COMMENT=false
+if [ "${#NATIVE_LIBRARY_APPLICABLE_SHORTHANDS_ARRAY[@]}" -gt "${#NATIVE_LIBRARY_USER_REFERENCED_SHORTHANDS_ARRAY[@]}" ]; then
+   ANGRY_COMMENT=true
+else
+   COMMENT="All native library builds modified in this PR shall attempt to be built by CI."
+fi
+if [[ $PR_EXISTING_NOTICE_ID != 0 && $PR_EXISTING_NOTICE_ID != "" || $ANGRY_COMMENT == true ]]; then
+    ${GH:-gh} pr comment $PR_EXISTING_NOTICE_ID --body "$COMMENT"
+fi
+
+echo "matrix_strategy<<EOF" >> $GITHUB_OUTPUT
+for item in $NATIVE_LIBRARY_USER_REFERENCED_SHORTHANDS; do
+  i=0
+  for target in ${NATIVE_LIBRARY_SHORTHANDS_ARRAY[@]}; do
+    if [[ "$target" == "$item" ]]; then
+      #mkdir -p "../../../${NATIVE_LIBRARY_PATHS_ARRAY[$i]}"
+      shopt -s nullglob
+      for runtime_script in "../../../${NATIVE_LIBRARY_PATHS_ARRAY[$i]}"/build-*.*; do
+        runtime="$(basename "$runtime_script")"
+        runtime="${runtime:6}" # trim build- prefix
+        runtime="${runtime%.*}" # remove extension
+        echo "- target: $target" >> $GITHUB_OUTPUT
+        echo "  runtime: $runtime" >> $GITHUB_OUTPUT
+        # :9 trim ../../../ prefix
+        echo "  exec: $(basename "$runtime_script")" >> $GITHUB_OUTPUT
+        echo "  dir: $(dirname "${runtime_script:9}")" >> $GITHUB_OUTPUT
+      done
+    fi
+    i=$(expr $i + 1)
+  done
+done
+echo EOF >> $GITHUB_OUTPUT
