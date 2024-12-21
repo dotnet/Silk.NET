dc588746e332a44e100f705add083c976827096c
Dylan Perks <dylanperks23@gmail.com>
2024-12-21 22:07:26 +0000

    Fix vcvarsall path

--- sources/SDL/Native/build-win-arm64.cmd
+++ sources/SDL/Native/build-win-arm64.cmd
@@ -2,7 +2,7 @@ if not exist "../../../eng/submodules/sdl/CMakeLists.txt" (
     git submodule update --init --recursive --depth 1 ..\..\..\eng\submodules\sdl
 )
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 17.0\VC\vcvarsall.bat" arm64
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" arm64
 rd /s /q build
 mkdir build
 cd build
