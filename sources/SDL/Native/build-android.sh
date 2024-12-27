cd73ccbfdce221d706361f3aedbfad06b30cc43e
Dylan Perks <dylanperks23@gmail.com>
2024-12-27 14:25:08 +0000

    Install Python 3.11

--- sources/SDL/Native/build-android.sh
+++ sources/SDL/Native/build-android.sh
@@ -4,13 +4,16 @@ if [ ! -e ../../../eng/submodules/sdl/CMakeLists.txt ]; then
     git submodule update --init --recursive --depth 1 ../../../eng/submodules/sdl
 fi
 
+python="python"
 if [ ! -z "$GITHUB_ACTIONS" ]; then
   # NDK already installed: https://github.com/actions/runner-images/blob/main/images/ubuntu/Ubuntu2404-Readme.md
   sdkmanager=( /usr/local/lib/android/sdk/cmdline-tools/*/bin/sdkmanager )
   ${sdkmanager[-1]} --install "platforms;android-19"
+  sudo apt install python3.11
+  python="python3.11"
 fi
 
-../../../eng/submodules/sdl/build-scripts/build-release.py --actions android --root ../../../eng/submodules/sdl
+$python ../../../eng/submodules/sdl/build-scripts/build-release.py --actions android --root ../../../eng/submodules/sdl
 mkdir -p runtimes/android-x86/native runtimes/android-x64/native runtimes/android-arm/native runtimes/android-arm64/native android
 cp ../../../eng/submodules/sdl/install-android/arm64-v8a-install/lib/libSDL3.so runtimes/android-arm64/native
 cp ../../../eng/submodules/sdl/install-android/armeabi-v7a-install/lib/libSDL3.so runtimes/android-arm/native
