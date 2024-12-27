1071026a27bb7259f13cb95fef0b682e044b46f8
Dylan Perks <dylanperks23@gmail.com>
2024-12-27 14:15:38 +0000

    Add Android build for SDL

---
+++ sources/SDL/Native/build-android.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env -S bash -eu
+
+if [ ! -e ../../../eng/submodules/sdl/CMakeLists.txt ]; then
+    git submodule update --init --recursive --depth 1 ../../../eng/submodules/sdl
+fi
+
+if [ ! -z "$GITHUB_ACTIONS" ]; then
+  # NDK already installed: https://github.com/actions/runner-images/blob/main/images/ubuntu/Ubuntu2404-Readme.md
+  sdkmanager --install "platforms;android-19"
+fi
+
+../../../eng/submodules/sdl/build-scripts/build-release.py --actions android --root ../../../eng/submodules/sdl
+mkdir -p runtimes/android-x86/native runtimes/android-x64/native runtimes/android-arm/native runtimes/android-arm64/native android
+cp ../../../eng/submodules/sdl/install-android/arm64-v8a-install/lib/libSDL3.so runtimes/android-arm64/native
+cp ../../../eng/submodules/sdl/install-android/armeabi-v7a-install/lib/libSDL3.so runtimes/android-arm/native
+cp ../../../eng/submodules/sdl/install-android/x86-install/lib/libSDL3.so runtimes/android-x86/native
+cp ../../../eng/submodules/sdl/install-android/x86_64-install/lib/libSDL3.so runtimes/android-x64/native
+jars=( ../../../eng/submodules/sdl/install-android/armeabi-v7a-install/share/java/SDL3/SDL3-*.jar )
+cp "${jars[1]}" android/SDL3.jar
