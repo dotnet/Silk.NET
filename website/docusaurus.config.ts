d65beab3fe732c6b6d522400e13009f6dd386ca8
Dylan Perks <dylanperks23@gmail.com>
2025-01-01 15:30:47 +0000

    Change some configs

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -5,20 +5,21 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Silk.NET',
+  tagline: 'Your one-stop-shop for high performance .NET graphics & compute.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://dotnet.github.io',
+
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Silk.NET/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'dotnet', // Usually your GitHub org/user name.
+  projectName: 'Silk.NET', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
