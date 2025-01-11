358ff239cd93e5858ad7f31b772ec906fe593f47
Andrew Davis <curin@dbtgaming.com>
2025-01-11 11:29:44 -0500

    D3DCommon Generation

---
+++ sources/Win32/Win32/DirectX/d3d12/d3dcommon/D3D_FORMAT_TYPE_LEVEL.gen.cs
@@ -0,0 +1,12 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+// Ported from d3dcommon.h in microsoft/DirectX-Headers tag v1.614.0
+// Original source is Copyright © Microsoft. Licensed under the MIT license
+#pragma warning disable CS1589, CS0419, CA1416, CS0618
+namespace Silk.NET.DirectX;
+
+public enum D3D_FORMAT_TYPE_LEVEL
+{
+    D3DFTL_NO_TYPE = 0,
+    D3DFTL_PARTIAL_TYPE = -2,
+    D3DFTL_FULL_TYPE = -1,
+}
