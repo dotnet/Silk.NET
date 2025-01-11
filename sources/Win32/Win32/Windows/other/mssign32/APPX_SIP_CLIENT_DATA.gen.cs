f41dc67c6f246210a7841af57b41636a8c3db0c8
Andrew Davis <curin@dbtgaming.com>
2025-01-11 10:09:33 -0500

    Revert "Generation Dump"
    This reverts commit 0a1bd47a53af52e7c245673c30655e30452cfea9.

--- sources/Win32/Win32/Windows/other/mssign32/APPX_SIP_CLIENT_DATA.gen.cs
+++ sources/Win32/Win32/Windows/other/mssign32/APPX_SIP_CLIENT_DATA.gen.cs
@@ -1,12 +1,17 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 // Ported from https://docs.microsoft.com/en-us/windows/win32/appxpkg/how-to-programmatically-sign-a-package
 // Original source is Copyright © Microsoft. All rights reserved. Licensed under the MIT License (MIT).
-#pragma warning disable CS1589, CS0419, CA1416, CS0618
 namespace Silk.NET.Windows;
 
+/// <include file='APPX_SIP_CLIENT_DATA.xml' path='doc/member[@name="APPX_SIP_CLIENT_DATA"]/*'/>
 public unsafe partial struct APPX_SIP_CLIENT_DATA
 {
+    /// <include file='APPX_SIP_CLIENT_DATA.xml' path='doc/member[@name="APPX_SIP_CLIENT_DATA.pSignerParams"]/*'/>
+
     [NativeTypeName("PSIGNER_SIGN_EX2_PARAMS")]
     public SIGNER_SIGN_EX2_PARAMS* pSignerParams;
-    public IUnknown pAppxSipState;
+
+    /// <include file='APPX_SIP_CLIENT_DATA.xml' path='doc/member[@name="APPX_SIP_CLIENT_DATA.pAppxSipState"]/*'/>
+
+    public IUnknown* pAppxSipState;
 }
--- sources/Win32/Win32/Windows/other/mssign32/SIGNER_FILE_INFO.gen.cs
+++ sources/Win32/Win32/Windows/other/mssign32/SIGNER_FILE_INFO.gen.cs
@@ -1,15 +1,22 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 // Ported from https://docs.microsoft.com/en-us/windows/win32/appxpkg/how-to-programmatically-sign-a-package
 // Original source is Copyright © Microsoft. All rights reserved. Licensed under the MIT License (MIT).
-#pragma warning disable CS1589, CS0419, CA1416, CS0618
 namespace Silk.NET.Windows;
 
+/// <include file='SIGNER_FILE_INFO.xml' path='doc/member[@name="SIGNER_FILE_INFO"]/*'/>
 public unsafe partial struct SIGNER_FILE_INFO
 {
+    /// <include file='SIGNER_FILE_INFO.xml' path='doc/member[@name="SIGNER_FILE_INFO.cbSize"]/*'/>
+
     [NativeTypeName("DWORD")]
     public uint cbSize;
 
+    /// <include file='SIGNER_FILE_INFO.xml' path='doc/member[@name="SIGNER_FILE_INFO.pwszFileName"]/*'/>
+
     [NativeTypeName("LPCWSTR")]
     public ushort* pwszFileName;
+
+    /// <include file='SIGNER_FILE_INFO.xml' path='doc/member[@name="SIGNER_FILE_INFO.hFile"]/*'/>
+
     public HANDLE hFile;
 }
