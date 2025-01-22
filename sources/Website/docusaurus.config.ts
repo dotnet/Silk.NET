f838a518ea2bb1fd2a6291d874f6974724bf89bf
Dylan Perks <dylanperks23@gmail.com>
2025-01-22 21:47:44 +0000

    Minor nits

--- sources/Website/docusaurus.config.ts
+++ sources/Website/docusaurus.config.ts
@@ -165,6 +165,7 @@ const config: Config = {
         prism: {
             theme: prismThemes.github,
             darkTheme: prismThemes.dracula,
+            additionalLanguages: ["csharp"]
         },
     } satisfies Preset.ThemeConfig,
 };
