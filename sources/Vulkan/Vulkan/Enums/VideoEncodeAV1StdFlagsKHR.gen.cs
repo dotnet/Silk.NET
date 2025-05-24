2002947a1c3452ae4121cf1aed5e17f042b15d6a
William Chen <exanite78012@gmail.com>
2025-05-23 22:25:59 -0400

    Add DllImport VTable config to Vulkan
    This currently doesn't generate anything for some reason (only the enums generate, and that's because of a previous mod)

---
+++ sources/Vulkan/Vulkan/Enums/VideoEncodeAV1StdFlagsKHR.gen.cs
@@ -0,0 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+namespace Silk.NET.Vulkan;
+
+[Transformed]
+public enum VideoEncodeAV1StdFlagsKHR : uint { }
