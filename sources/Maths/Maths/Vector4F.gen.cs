650f30b9602af33915b0c7d25b84eaca9f608991
John Gietzen <john@gietzen.us>
2025-05-30 19:53:13 -0700

    Added some necessary vector bits for testing.

--- sources/Maths/Maths/Vector4F.gen.cs
+++ sources/Maths/Maths/Vector4F.gen.cs
@@ -5,15 +5,81 @@ namespace Silk.NET.Maths
     partial struct Vector4F<T> : IEquatable<Vector4F<T>> where T : IFloatingPointIeee754<T>
     {
         public T X;
+
         public T Y;
+
         public T Z;
+
         public T W;
+
+        public Vector4F(T x, T y, T z, T w) => (X, Y, Z, W) = (x, y, z, w);
+
         public static bool operator ==(Vector4F<T> left, Vector4F<T> right) => left.X == right.X && left.Y == right.Y && left.Z == right.Z && left.W == right.W;
+
         public static bool operator !=(Vector4F<T> left, Vector4F<T> right) => !(left == right);
+
+        /// <inheridoc/>
         public override bool Equals(object? obj) => obj is Vector4F<T> other && Equals(other);
+
         /// <inheridoc/>
         public bool Equals(Vector4F<T> other) => this == other;
+
         /// <inheridoc/>
         public override int GetHashCode() => HashCode.Combine(X, Y, Z, W);
+    }
+
+    static partial class Vector4F
+    {
+        public static Vector4F<TSelf> Log<TSelf>(this Vector4F<TSelf> x)
+            where TSelf : IFloatingPointIeee754<TSelf>, ILogarithmicFunctions<TSelf> =>
+            new(TSelf.Log(x.X), TSelf.Log(x.Y), TSelf.Log(x.Z), TSelf.Log(x.W));
+
+        public static Vector4F<TSelf> Log<TSelf>(this Vector4F<TSelf> x, Vector4F<TSelf> newBase)
+            where TSelf : IFloatingPointIeee754<TSelf>, ILogarithmicFunctions<TSelf> =>
+            new(TSelf.Log(x.X, newBase.X), TSelf.Log(x.Y, newBase.Y), TSelf.Log(x.Z, newBase.Z), TSelf.Log(x.W, newBase.W));
+
+        public static Vector4F<TSelf> LogP1<TSelf>(this Vector4F<TSelf> x)
+            where TSelf : IFloatingPointIeee754<TSelf>, ILogarithmicFunctions<TSelf> =>
+            new(TSelf.LogP1(x.X), TSelf.LogP1(x.Y), TSelf.LogP1(x.Z), TSelf.LogP1(x.W));
+
+        public static Vector4F<TSelf> Log2<TSelf>(this Vector4F<TSelf> x)
+            where TSelf : IFloatingPointIeee754<TSelf>, ILogarithmicFunctions<TSelf> =>
+            new(TSelf.Log2(x.X), TSelf.Log2(x.Y), TSelf.Log2(x.Z), TSelf.Log2(x.W));
+
+        public static Vector4F<TSelf> Log2P1<TSelf>(this Vector4F<TSelf> x)
+            where TSelf : IFloatingPointIeee754<TSelf>, ILogarithmicFunctions<TSelf> =>
+            new(TSelf.Log2P1(x.X), TSelf.Log2P1(x.Y), TSelf.Log2P1(x.Z), TSelf.Log2P1(x.W));
+
+        public static Vector4F<TSelf> Log10<TSelf>(this Vector4F<TSelf> x)
+            where TSelf : IFloatingPointIeee754<TSelf>, ILogarithmicFunctions<TSelf> =>
+            new(TSelf.Log10(x.X), TSelf.Log10(x.Y), TSelf.Log10(x.Z), TSelf.Log10(x.W));
+
+        public static Vector4F<TSelf> Log10P1<TSelf>(this Vector4F<TSelf> x)
+            where TSelf : IFloatingPointIeee754<TSelf>, ILogarithmicFunctions<TSelf> =>
+            new(TSelf.Log10P1(x.X), TSelf.Log10P1(x.Y), TSelf.Log10P1(x.Z), TSelf.Log10P1(x.W));
+
+        public static Vector4F<TSelf> Exp<TSelf>(this Vector4F<TSelf> x)
+            where TSelf : IFloatingPointIeee754<TSelf>, IExponentialFunctions<TSelf> =>
+            new(TSelf.Exp(x.X), TSelf.Exp(x.Y), TSelf.Exp(x.Z), TSelf.Exp(x.W));
+
+        public static Vector4F<TSelf> ExpM1<TSelf>(this Vector4F<TSelf> x)
+            where TSelf : IFloatingPointIeee754<TSelf>, IExponentialFunctions<TSelf> =>
+            new(TSelf.ExpM1(x.X), TSelf.ExpM1(x.Y), TSelf.ExpM1(x.Z), TSelf.ExpM1(x.W));
+
+        public static Vector4F<TSelf> Exp2<TSelf>(this Vector4F<TSelf> x)
+            where TSelf : IFloatingPointIeee754<TSelf>, IExponentialFunctions<TSelf> =>
+            new(TSelf.Exp2(x.X), TSelf.Exp2(x.Y), TSelf.Exp2(x.Z), TSelf.Exp2(x.W));
+
+        public static Vector4F<TSelf> Exp2M1<TSelf>(this Vector4F<TSelf> x)
+            where TSelf : IFloatingPointIeee754<TSelf>, IExponentialFunctions<TSelf> =>
+            new(TSelf.Exp2M1(x.X), TSelf.Exp2M1(x.Y), TSelf.Exp2M1(x.Z), TSelf.Exp2M1(x.W));
+
+        public static Vector4F<TSelf> Exp10<TSelf>(this Vector4F<TSelf> x)
+            where TSelf : IFloatingPointIeee754<TSelf>, IExponentialFunctions<TSelf> =>
+            new(TSelf.Exp10(x.X), TSelf.Exp10(x.Y), TSelf.Exp10(x.Z), TSelf.Exp10(x.W));
+
+        public static Vector4F<TSelf> Exp10M1<TSelf>(this Vector4F<TSelf> x)
+            where TSelf : IFloatingPointIeee754<TSelf>, IExponentialFunctions<TSelf> =>
+            new(TSelf.Exp10M1(x.X), TSelf.Exp10M1(x.Y), TSelf.Exp10M1(x.Z), TSelf.Exp10M1(x.W));
     }
 }
