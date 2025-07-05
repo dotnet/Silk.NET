bf382bcb8b2ab600c96f548212f01b2a734308ec
dom <domportera@gmail.com>
2025-07-05 11:39:30 -0400

    optimize enum performance for larger enums

--- sources/Input/Input/Implementations/EnumInfo.cs
+++ sources/Input/Input/Implementations/EnumInfo.cs
@@ -15,15 +15,13 @@ internal static class EnumInfo<T> where T : unmanaged, Enum
 {
     public static IReadOnlyList<T> Values => _values;
     public static int NameCount => _values.Length;
-    public static int ValueCount => _numericallyDistinctValues.Length;
+    public static int ValueCount => _numericallyDistinctValues.Count;
 
     public static readonly T MaxValue;
     public static readonly T MinValue;
 
     private static readonly T[] _values;
-    private static readonly T[] _numericallyDistinctValues;
-
-    private static readonly int _elementSize = Unsafe.SizeOf<T>();
+    private static readonly Dictionary<T, int> _numericallyDistinctValues;
 
     public static readonly Type UnderlyingType = typeof(T).GetEnumUnderlyingType();
 
@@ -33,58 +31,61 @@ internal static class EnumInfo<T> where T : unmanaged, Enum
         {
             throw new InvalidOperationException("Flags enums are not supported.");
         }
-#if DEBUG
-        if (_elementSize != 1 || _elementSize != 2 || _elementSize != 4 || _elementSize != 8)
-        {
-            throw new Exception("Enum provided uses an unknown numeric base??");
-        }
-#endif
+
         var underlyingType = UnderlyingType;
+        T[] vals;
         if (underlyingType == typeof(int))
         {
             _values = OrderedValues<int>(false);
-            _numericallyDistinctValues = OrderedValues<int>(true);
+            vals = OrderedValues<uint>(true);
         }
         else if (underlyingType == typeof(uint))
         {
             _values = OrderedValues<uint>(false);
-            _numericallyDistinctValues = OrderedValues<uint>(true);
+            vals = OrderedValues<uint>(true);
         }
         else if (underlyingType == typeof(byte))
         {
             _values = OrderedValues<byte>(false);
-            _numericallyDistinctValues = OrderedValues<byte>(true);
+            vals = OrderedValues<byte>(true);
         }
         else if (underlyingType == typeof(sbyte))
         {
             _values = OrderedValues<sbyte>(false);
-            _numericallyDistinctValues = OrderedValues<sbyte>(true);
+            vals = OrderedValues<sbyte>(true);
         }
         else if (underlyingType == typeof(short))
         {
             _values = OrderedValues<short>(false);
-            _numericallyDistinctValues = OrderedValues<short>(true);
+            vals = OrderedValues<short>(true);
         }
         else if (underlyingType == typeof(ushort))
         {
             _values = OrderedValues<ushort>(false);
-            _numericallyDistinctValues = OrderedValues<ushort>(true);
+            vals = OrderedValues<ushort>(true);
         }
         else if (underlyingType == typeof(long))
         {
             _values = OrderedValues<long>(false);
-            _numericallyDistinctValues = OrderedValues<long>(true);
+            vals = OrderedValues<long>(true);
         }
         else if (underlyingType == typeof(ulong))
         {
             _values = OrderedValues<ulong>(false);
-            _numericallyDistinctValues = OrderedValues<ulong>(true);
+            vals = OrderedValues<ulong>(true);
         }
         else
         {
             throw new InvalidOperationException("Enum provided uses an unknown numeric base??");
         }
 
+        var dict = new Dictionary<T, int>(vals.Length);
+        for (int i = 0; i < vals.Length; i++)
+        {
+            dict.Add(vals[i], i);
+        }
+
+        _numericallyDistinctValues = dict;
         MinValue = Values[0];
         MaxValue = Values[^1];
     }
@@ -95,7 +96,8 @@ internal static class EnumInfo<T> where T : unmanaged, Enum
     /// stably sorted across application runs.
     /// </summary>
     /// <param name="value"></param>
-    /// <returns></returns>
+    /// <returns>The index of the sorted enum value</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static int NameIndexOf(T value) => Array.IndexOf(_values, value);
 
     /// <summary>
@@ -103,36 +105,10 @@ internal static class EnumInfo<T> where T : unmanaged, Enum
     /// Values with the same numerical value will return the same index
     /// </summary>
     /// <param name="value"></param>
-    /// <returns></returns>
+    /// <returns>The index of the sorted enum numerical value</returns>
     /// <exception cref="InvalidOperationException"></exception>
-    public static unsafe int ValueIndexOf(T value)
-    {
-        switch (_elementSize)
-        {
-            case 1:
-            {
-                var values = Unsafe.As<byte[]>(_numericallyDistinctValues);
-                return Array.IndexOf(values, *(byte*)&value);
-            }
-            case 2:
-            {
-                var values = Unsafe.As<ushort[]>(_numericallyDistinctValues);
-                return Array.IndexOf(values, *(ushort*)&value);
-            }
-            case 4:
-            {
-                var values = Unsafe.As<uint[]>(_numericallyDistinctValues);
-                return Array.IndexOf(values, *(uint*)&value);
-            }
-            case 8:
-            {
-                var values = Unsafe.As<ulong[]>(_numericallyDistinctValues);
-                return Array.IndexOf(values, *(ulong*)&value);
-            }
-            default:
-                throw new InvalidOperationException("Enum provided uses an unknown numeric base??");
-        }
-    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int ValueIndexOf(T value) => _numericallyDistinctValues[value];
 
     private static unsafe T[] OrderedValues<TNumber>(bool byNumericValue)
         where TNumber : unmanaged, IComparable<TNumber>
