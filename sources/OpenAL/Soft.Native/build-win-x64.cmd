ab77310695a4ecb6f1bb9e56c9afbcc800234749
Dylan Perks <dylanperks23@gmail.com>
2025-10-13 18:34:32 +0100

    Fix build errors (1)

--- sources/OpenAL/Soft.Native/build-win-x64.cmd
+++ sources/OpenAL/Soft.Native/build-win-x64.cmd
@@ -1,12 +1,12 @@
-if not exist "../../../eng/submodules/sdl/CMakeLists.txt" (
-    git submodule update --init --recursive --depth 1 ..\..\..\eng\submodules\sdl
+if not exist "../../../eng/submodules/openal-soft/CMakeLists.txt" (
+    git submodule update --init --recursive --depth 1 ..\..\..\eng\submodules\openal-soft
 )
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 rd /s /q build
 mkdir build
 cd build
-cmake ..\..\..\..\eng\submodules\sdl -DCMAKE_BUILD_TYPE=Release -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF -DALSOFT_BUILD_ROUTER=ON -A x64
+cmake ..\..\..\..\eng\submodules\openal-soft -DCMAKE_BUILD_TYPE=Release -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF -DALSOFT_BUILD_ROUTER=ON -A x64
 cmake --build . --parallel --config Release
 cd ..
 mkdir runtimes\win-x64\native
