ae5ba65ad3c74691cadce8752cf99a3885605d29
Dylan Perks <dylanperks23@gmail.com>
2025-10-25 20:08:17 +0100

    Oops, add sudo back

--- sources/OpenAL/Soft.Native/build-linux-x64.sh
+++ sources/OpenAL/Soft.Native/build-linux-x64.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bash -eu
 if [[ ! -z ${GITHUB_ACTIONS+x} ]]; then
-    apt update
-    apt install -y libasound2-dev:amd64 libpulse-dev:amd64 libsoundio-dev:amd64 libsndfile1-dev:amd64 \
+    sudo apt update
+    sudo apt install -y libasound2-dev:amd64 libpulse-dev:amd64 libsoundio-dev:amd64 libsndfile1-dev:amd64 \
         libmysofa-dev:amd64 qtbase5-dev:amd64 libdbus-1-dev:amd64 libjack-dev:amd64 portaudio19-dev:amd64 git \
         cmake build-essential python3 libpipewire-0.3-dev:amd64 qt6-base-dev:amd64 pulseaudio:amd64
 fi
