5297edd105e4b0a9ff280b86012c79c13bbc17a7
William Chen <exanite78012@gmail.com>
2025-11-23 17:31:52 -0500

    Update vulkan header.txt to use 2015-2025 as the copyright years

--- sources/Vulkan/Vulkan/Vulkan/PhysicalDeviceMaintenance10FeaturesKHR.gen.cs
+++ sources/Vulkan/Vulkan/Vulkan/PhysicalDeviceMaintenance10FeaturesKHR.gen.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // Ported from the Vulkan headers and corresponding dependencies.
-// Original source is Copyright 2015-2023 The Khronos Group Inc. Licensed under the MIT license.
+// Original source is Copyright 2015-2025 The Khronos Group Inc. Licensed under the MIT license.
 using System.Runtime.CompilerServices;
 
 namespace Silk.NET.Vulkan;
