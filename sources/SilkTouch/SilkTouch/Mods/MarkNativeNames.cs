72317820b373e388701394ddf5c3ebda41454989
William Chen <exanite78012@gmail.com>
2025-11-25 03:28:38 -0500

    Cleanup

--- sources/SilkTouch/SilkTouch/Mods/MarkNativeNames.cs
+++ sources/SilkTouch/SilkTouch/Mods/MarkNativeNames.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -16,7 +15,7 @@ namespace Silk.NET.SilkTouch.Mods;
 public class MarkNativeNames(IOptionsSnapshot<MarkNativeNames.Configuration> cfg) : IMod
 {
     /// <summary>
-    /// MarkNativeNames mod configuration.
+    /// <see cref="MarkNativeNames"/> mod configuration.
     /// </summary>
     public record Configuration
     {
