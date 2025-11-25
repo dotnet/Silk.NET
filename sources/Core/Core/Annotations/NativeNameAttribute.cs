aac17e3f112c3016c5fb0a14c440aff9fa82c928
William Chen <exanite78012@gmail.com>
2025-11-25 02:03:26 -0500

    Add NativeName attribute

---
+++ sources/Core/Core/Annotations/NativeNameAttribute.cs
@@ -0,0 +1,26 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Silk.NET.Core;
+
+/// <summary>
+/// Stores the native name of the identifier that this attribute is placed on.
+/// </summary>
+[AttributeUsage(
+    AttributeTargets.Struct
+    | AttributeTargets.Enum
+    | AttributeTargets.Property
+    | AttributeTargets.Field
+    | AttributeTargets.Parameter
+    | AttributeTargets.ReturnValue,
+    AllowMultiple = false,
+    Inherited = true
+)]
+public sealed class NativeNameAttribute : Attribute
+{
+    /// <param name="name">The native name of the identifier.</param>
+    public NativeNameAttribute(string name) => Name = name;
+
+    /// <summary>The native name of the identifier that this attribute is placed on.</summary>
+    public string Name { get; }
+}
