b4cdd0e2fdb68d4a90a12feda6997c3733e5b735
William Chen <exanite78012@gmail.com>
2025-11-26 19:39:41 -0500

    Fix compile errors
    SDL is still erroring:
    
    Sdl.gen.cs(60143,45): Error CS0266 : Cannot implicitly convert type 'long' to 'nint'. An explicit conversion exists (are you missing a cast?)
    Sdl.gen.cs(60143,35): Error CS0133 : The expression being assigned to 'Sdl.MaxSint64' must be constant
    
    [NativeTypeName("#define SDL_MAX_SINT64 SDL_SINT64_C(0x7FFFFFFFFFFFFFFF)")]
    public const nint MaxSint64 = unchecked(0x7FFFFFFFFFFFFFFF);
    
    At first glance, this might be a ClangSharp issue with how these defines are generated on Linux.

--- sources/OpenAL/OpenAL/Extensions.cs
+++ sources/OpenAL/OpenAL/Extensions.cs
@@ -21,10 +21,10 @@ public partial class ALContext
     public static void ThrowError(DeviceHandle device)
     {
         var err = GetError(device);
-        if (err != ContextErrorCode.NoError)
+        if (err != ErrorCode.NoError)
         {
             static void Throw(ErrorCode err) => throw new OpenALException(err);
-            Throw((ErrorCode)(int)err);
+            Throw(err);
         }
     }
 }
@@ -44,10 +44,10 @@ public static class Extensions
     public static void ThrowError(this IALContext alc, DeviceHandle device)
     {
         var err = alc.GetError(device);
-        if (err != ContextErrorCode.NoError)
+        if (err != ErrorCode.NoError)
         {
             static void Throw(ErrorCode err) => throw new OpenALException(err);
-            Throw((ErrorCode)(int)err);
+            Throw(err);
         }
     }
 }
--- tests/SilkTouch/SilkTouch/Naming/NameTests.cs
+++ tests/SilkTouch/SilkTouch/Naming/NameTests.cs
@@ -12,17 +12,17 @@ public class NameTests : NameTrimmer
     [Test, TestCase(null), TestCase("glfw")]
     public void SimpleGlfwTestDetermination(string? hint)
     {
-        var test = new Dictionary<string, (string, List<string>?)>
+        var test = new Dictionary<string, CandidateNames>
         {
-            { "GLFWallocator", ("GLFWallocator", null) },
-            { "GLFWgammaramp", ("GLFWgammaramp", null) },
-            { "GLFWgamepadstate", ("GLFWgamepadstate", null) },
-            { "GLFWvidmode", ("GLFWvidmode", null) },
-            { "GLFWimage", ("GLFWimage", null) },
-            { "Glfw", ("Glfw", null) },
-            { "GLFWcursor", ("GLFWcursor", null) },
-            { "GLFWmonitor", ("GLFWmonitor", null) },
-            { "GLFWwindow", ("GLFWwindow", null) },
+            { "GLFWallocator", new CandidateNames("GLFWallocator", null) },
+            { "GLFWgammaramp", new CandidateNames("GLFWgammaramp", null) },
+            { "GLFWgamepadstate", new CandidateNames("GLFWgamepadstate", null) },
+            { "GLFWvidmode", new CandidateNames("GLFWvidmode", null) },
+            { "GLFWimage", new CandidateNames("GLFWimage", null) },
+            { "Glfw", new CandidateNames("Glfw", null) },
+            { "GLFWcursor", new CandidateNames("GLFWcursor", null) },
+            { "GLFWmonitor", new CandidateNames("GLFWmonitor", null) },
+            { "GLFWwindow", new CandidateNames("GLFWwindow", null) },
         };
         Assert.That(
             GetPrefix(null, hint, test, null, null, false, true)?.Prefix,
@@ -63,12 +63,12 @@ public class NameTests : NameTrimmer
     [Test]
     public void RegressionFragmentShaderColorModMaskATI()
     {
-        var test = new Dictionary<string, (string, List<string>?)>
+        var test = new Dictionary<string, CandidateNames>
         {
-            { "GL_2X_BIT_ATI", ("GL_2X_BIT_ATI", null) },
-            { "GL_COMP_BIT_ATI", ("GL_COMP_BIT_ATI", null) },
-            { "GL_NEGATE_BIT_ATI", ("GL_NEGATE_BIT_ATI", null) },
-            { "GL_BIAS_BIT_ATI", ("GL_BIAS_BIT_ATI", null) },
+            { "GL_2X_BIT_ATI", new CandidateNames("GL_2X_BIT_ATI", null) },
+            { "GL_COMP_BIT_ATI", new CandidateNames("GL_COMP_BIT_ATI", null) },
+            { "GL_NEGATE_BIT_ATI", new CandidateNames("GL_NEGATE_BIT_ATI", null) },
+            { "GL_BIAS_BIT_ATI", new CandidateNames("GL_BIAS_BIT_ATI", null) },
         };
         Trim(
             new NameTrimmerContext
@@ -98,10 +98,10 @@ public class NameTests : NameTrimmer
     [Test]
     public void RegressionEvalTargetNV()
     {
-        var test = new Dictionary<string, (string, List<string>?)>
+        var test = new Dictionary<string, CandidateNames>
         {
-            { "GL_EVAL_2D_NV", ("GL_EVAL_2D_NV", null) },
-            { "GL_EVAL_TRIANGULAR_2D_NV", ("GL_EVAL_TRIANGULAR_2D_NV", null) },
+            { "GL_EVAL_2D_NV", new CandidateNames("GL_EVAL_2D_NV", null) },
+            { "GL_EVAL_TRIANGULAR_2D_NV", new CandidateNames("GL_EVAL_TRIANGULAR_2D_NV", null) },
         };
         Trim(
             new NameTrimmerContext
@@ -136,9 +136,9 @@ public class NameTests : NameTrimmer
     [Test]
     public void RegressionSingleMemberEnumUsesGlobalPrefixHint()
     {
-        var names = new Dictionary<string, (string, List<string>?)>
+        var names = new Dictionary<string, CandidateNames>
         {
-            { "GL_FILL_NV", ("GL_FILL_NV", null) },
+            { "GL_FILL_NV", new CandidateNames("GL_FILL_NV", null) },
         };
         var ctx = new NameTrimmerContext
         {
@@ -149,15 +149,15 @@ public class NameTests : NameTrimmer
         };
         var uut = new NameTrimmer();
         uut.Trim(ctx);
-        Assert.That(names["GL_FILL_NV"].Item1, Is.EqualTo("FILL_NV"));
+        Assert.That(names["GL_FILL_NV"].Primary, Is.EqualTo("FILL_NV"));
     }
 
     [Test]
     public void MultipleGlobalPrefixHints()
     {
-        var names = new Dictionary<string, (string, List<string>?)>
+        var names = new Dictionary<string, CandidateNames>
         {
-            { "ALC_CONTEXT_DEBUG_BIT_EXT", ("ALC_CONTEXT_DEBUG_BIT_EXT", null) },
+            { "ALC_CONTEXT_DEBUG_BIT_EXT", new CandidateNames("ALC_CONTEXT_DEBUG_BIT_EXT", null) },
         };
         var ctx = new NameTrimmerContext
         {
@@ -168,6 +168,6 @@ public class NameTests : NameTrimmer
         };
         var uut = new NameTrimmer();
         uut.Trim(ctx);
-        Assert.That(names["ALC_CONTEXT_DEBUG_BIT_EXT"].Item1, Is.EqualTo("CONTEXT_DEBUG_BIT_EXT"));
+        Assert.That(names["ALC_CONTEXT_DEBUG_BIT_EXT"].Primary, Is.EqualTo("CONTEXT_DEBUG_BIT_EXT"));
     }
 }
