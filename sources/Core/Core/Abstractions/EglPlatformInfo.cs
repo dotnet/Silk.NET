53fbdd35ca4e076c093da7adbd4f10eaba0b34fa
William Chen <exanite78012@gmail.com>
2025-12-06 18:02:23 -0500

    Revert change to EGLPlatformInfo

--- sources/Core/Core/Abstractions/EglPlatformInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace Silk.NET.Core;
-
-/// <summary>
-/// The EGL platform-specific handles.
-/// </summary>
-/// <param name="Display"><c>EglDisplay</c>.</param>
-/// <param name="Surface"><c>EglSurface</c>.</param>
-public readonly record struct EglPlatformInfo(nint Display, nint Surface);
