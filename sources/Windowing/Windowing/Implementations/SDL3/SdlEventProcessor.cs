787081a34ebcef7d8fca2afa463f665f79fe95b6
William Chen <exanite78012@gmail.com>
2025-12-06 19:22:15 -0500

    Revert changes to SdlEventProcessor

--- sources/Windowing/Windowing/Implementations/SDL3/SdlEventProcessor.cs
+++ sources/Windowing/Windowing/Implementations/SDL3/SdlEventProcessor.cs
@@ -82,7 +82,7 @@ internal static class SdlEventProcessor
         SdlSurface? surface = null;
         if (
             @event.Type is >= (uint)EventType.WindowFirst and <= (uint)EventType.WindowLast
-            && !_surfaces.TryGetValue(@event.Window.WindowId, out surface)
+            && !_surfaces.TryGetValue(@event.Window.WindowID, out surface)
         )
         {
             return;
@@ -172,7 +172,7 @@ internal static class SdlEventProcessor
             {
                 foreach (var knownSurface in _surfaces.Values)
                 {
-                    knownSurface.Impl.OnDisplayCoordinatesChanged(@event.Display.DisplayId);
+                    knownSurface.Impl.OnDisplayCoordinatesChanged(@event.Display.DisplayID);
                 }
                 break;
             }
@@ -183,7 +183,7 @@ internal static class SdlEventProcessor
                 foreach (var knownSurface in _surfaces.Values)
                 {
                     knownSurface.Impl.OnPotentialVideoModeChanges(
-                        @event.Display.DisplayId,
+                        @event.Display.DisplayID,
                         out var isDisplayCurrent,
                         currentDisplay
                     );
@@ -203,7 +203,7 @@ internal static class SdlEventProcessor
                 {
                     Display = @event.Display with
                     {
-                        DisplayId = currentDisplay = Sdl.GetDisplayForWindow(surface!.Impl.Handle),
+                        DisplayID = currentDisplay = Sdl.GetDisplayForWindow(surface!.Impl.Handle),
                     },
                 };
                 goto case (uint)EventType.DisplayContentScaleChanged;
@@ -283,7 +283,7 @@ internal static class SdlEventProcessor
             {
                 if (
                     _droppedFiles is null
-                    || !_surfaces.TryGetValue(@event.Window.WindowId, out surface)
+                    || !_surfaces.TryGetValue(@event.Window.WindowID, out surface)
                 )
                 {
                     _droppedFiles = null;
