5761756ef82fb0119cf78e992ebea1167a184f63
William Chen <exanite78012@gmail.com>
2026-01-09 19:45:56 -0500

    Add set of tests: PrettifyTests.CommonCases

--- tests/SilkTouch/SilkTouch/Naming/PrettifyTests.cs
+++ tests/SilkTouch/SilkTouch/Naming/PrettifyTests.cs
@@ -7,6 +7,36 @@ namespace Silk.NET.SilkTouch.UnitTests.Naming;
 
 public class PrettifyTests
 {
+    [Theory]
+    [TestCase("HelloWorld", ExpectedResult = "HelloWorld")]
+    [TestCase("HelloWorld123", ExpectedResult = "HelloWorld123")]
+    [TestCase("HelloWorld_123", ExpectedResult = "HelloWorld123")]
+    [TestCase("Hello_World", ExpectedResult = "HelloWorld")]
+    [TestCase("_Hello__World_", ExpectedResult = "HelloWorld")]
+    [TestCase("_HELLO__WORLD_", ExpectedResult = "HelloWorld")]
+    [TestCase("LONGACRONYM", 4, ExpectedResult = "Longacronym")]
+    [TestCase("LONG_ACRONYM", 4, ExpectedResult = "LongAcronym")]
+    [TestCase(
+        "LONG_ACRONYM",
+        5,
+        ExpectedResult = "LONGAcronym",
+        Description = "LONG is short enough to be uppercased"
+    )]
+    [TestCase("LONG_Acronym", 5, ExpectedResult = "LONGAcronym")]
+    [TestCase(
+        "LONG_ACRONYM",
+        10,
+        ExpectedResult = "LongAcronym",
+        Description = "Both should be uppercased, but since they are adjacent, they conflict and revert back to lowercase"
+    )]
+    [TestCase(
+        "123",
+        ExpectedResult = "X123",
+        Description = "C# identifiers cannot start with a number"
+    )]
+    public string CommonCases(string input, int longAcronymThreshold = 0) =>
+        input.Prettify(new NameUtils.NameTransformer(longAcronymThreshold));
+
     [Test]
     public void IsNotAffectedBy_TrailingUnderscore()
     {
