7230574ccf813d9ccfebfdfc70651446dcf71c93
John Gietzen <john@gietzen.us>
2026-02-10 16:28:44 -0800

    Unused usings.

--- sources/Maths/Maths/Quaternion.Old.cs
+++ sources/Maths/Maths/Quaternion.Old.cs
@@ -1,12 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Globalization;
 using System.Numerics;
-using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
-using Silk.NET.Maths;
 
 namespace Silk.NET.Maths
 {
