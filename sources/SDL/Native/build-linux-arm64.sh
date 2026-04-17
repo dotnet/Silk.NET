4a925ba0805b33eb6219286492351f0ddcd67895
William Chen <exanite78012@gmail.com>
2026-04-16 23:21:47 -0400

    Update dependency list
    (cherry picked from commit b0db2ba97c22bdba5cff2a68e15d7cdecdc24562)

--- sources/SDL/Native/build-linux-arm64.sh
+++ sources/SDL/Native/build-linux-arm64.sh
@@ -10,10 +10,10 @@ if [[ ! -z ${GITHUB_ACTIONS+x} ]]; then
     sudo apt-get update
     sudo apt-get install build-essential git make \
         pkg-config cmake ninja-build gnome-desktop-testing libasound2-dev libpulse-dev \
-        libaudio-dev libjack-dev libsndio-dev libx11-dev libxext-dev \
-        libxrandr-dev libxcursor-dev libxfixes-dev libxi-dev libxss-dev \
+        libaudio-dev libfribidi-dev libjack-dev libsndio-dev libx11-dev libxext-dev \
+        libxrandr-dev libxcursor-dev libxfixes-dev libxi-dev libxss-dev libxtst-dev \
         libxkbcommon-dev libdrm-dev libgbm-dev libgl1-mesa-dev libgles2-mesa-dev \
-        libegl1-mesa-dev libdbus-1-dev libibus-1.0-dev libudev-dev fcitx-libs-dev \
+        libegl1-mesa-dev libdbus-1-dev libibus-1.0-dev libudev-dev libthai-dev \
         libpipewire-0.3-dev libwayland-dev libdecor-0-dev liburing-dev
 fi
 rm -rf build
