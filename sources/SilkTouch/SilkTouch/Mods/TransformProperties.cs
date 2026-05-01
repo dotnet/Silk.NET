e2e9140a396405f681aa8357b3c3a03831ad5af4
William Chen <exanite78012@gmail.com>
2026-05-01 09:37:04 -0400

    Add MaybeBool transformation to fields and properties

--- sources/SilkTouch/SilkTouch/Mods/TransformProperties.cs
+++ sources/SilkTouch/SilkTouch/Mods/TransformProperties.cs
@@ -4,6 +4,8 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.Options;
+using Silk.NET.SilkTouch.Mods.Transformation;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace Silk.NET.SilkTouch.Mods;
@@ -21,13 +23,28 @@ namespace Silk.NET.SilkTouch.Mods;
 /// <c>static Utf8String Thing => "thing"u8;</c>.
 /// 2. Transform fields and properties that are recognised
 /// to be akin to booleans to use the <c>MaybeBool</c> type.
+/// This functionality is based on <see cref="BoolTransformer"/>.
 /// </remarks>
-public class TransformProperties : IMod
+[ModConfiguration<Configuration>]
+public class TransformProperties(IOptionsSnapshot<TransformProperties.Configuration> cfg) : IMod
 {
+    /// <summary>
+    /// Configuration for the <see cref="TransformProperties"/>.
+    /// </summary>
+    public class Configuration
+    {
+        /// <summary>
+        /// Types to treat as boolean and their boolean schemes if different from the default.
+        /// </summary>
+        public Dictionary<string, string?> BoolTypes { get; init; } = [];
+    }
+
     /// <inheritdoc />
     public async Task ExecuteAsync(IModContext ctx, CancellationToken ct = default)
     {
-        var rw = new Rewriter();
+        var config = cfg.Get(ctx.JobKey);
+
+        var rw = new Rewriter(config);
         var proj = ctx.SourceProject;
         foreach (var docId in ctx.SourceProject?.DocumentIds ?? [])
         {
@@ -42,10 +59,62 @@ public class TransformProperties : IMod
         ctx.SourceProject = proj;
     }
 
-    private class Rewriter : CSharpSyntaxRewriter
+    private class Rewriter(Configuration config) : CSharpSyntaxRewriter
     {
+        public override SyntaxNode? VisitFieldDeclaration(FieldDeclarationSyntax node)
+        {
+            // Transform bool-like fields to use MaybeBool
+            var nativeType =
+                node.AttributeLists.GetNativeTypeName() ?? node.Declaration.Type.ToString();
+            if (config.BoolTypes.TryGetValue(nativeType, out var scheme))
+            {
+                var newType = string.IsNullOrWhiteSpace(scheme)
+                    ? GenericName(
+                        Identifier("MaybeBool"),
+                        TypeArgumentList(SingletonSeparatedList(node.Declaration.Type))
+                    )
+                    : GenericName(
+                        Identifier("MaybeBool"),
+                        TypeArgumentList(
+                            SeparatedList(
+                                // ReSharper disable once RedundantCast <-- false positive
+                                (IEnumerable<TypeSyntax>)
+                                    [node.Declaration.Type, IdentifierName(scheme)]
+                            )
+                        )
+                    );
+
+                node = node.WithDeclaration(node.Declaration.WithType(newType));
+            }
+
+            return base.VisitFieldDeclaration(node);
+        }
+
         public override SyntaxNode? VisitPropertyDeclaration(PropertyDeclarationSyntax node)
         {
+            // Transform bool-like properties to use MaybeBool
+            var nativeType = node.AttributeLists.GetNativeTypeName() ?? node.Type.ToString();
+            if (config.BoolTypes.TryGetValue(nativeType, out var scheme))
+            {
+                var newType = string.IsNullOrWhiteSpace(scheme)
+                    ? GenericName(
+                        Identifier("MaybeBool"),
+                        TypeArgumentList(SingletonSeparatedList(node.Type))
+                    )
+                    : GenericName(
+                        Identifier("MaybeBool"),
+                        TypeArgumentList(
+                            SeparatedList(
+                                // ReSharper disable once RedundantCast <-- false positive
+                                (IEnumerable<TypeSyntax>)[node.Type, IdentifierName(scheme)]
+                            )
+                        )
+                    );
+
+                node = node.WithType(newType);
+            }
+
+            // Transform ReadOnlySpan<byte> string constants to use Utf8String
             if (
                 node.Modifiers.Any(SyntaxKind.StaticKeyword)
                 && node.Type
