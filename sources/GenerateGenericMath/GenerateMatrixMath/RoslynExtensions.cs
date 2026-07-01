88f840254c33795994a872620a198d047cafa608
John Gietzen <john@gietzen.us>
2026-07-01 13:29:45 -0700

    Added current state of code generator from: https://github.com/otac0n/GenerateMatrixMath/commit/3a8e59d5d5f3e48b725ba675582cf617c213e2ba

---
+++ sources/GenerateGenericMath/GenerateMatrixMath/RoslynExtensions.cs
@@ -0,0 +1,88 @@
+﻿using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace GenerateMatrixMath
+{
+    internal static class RoslynExtensions
+    {
+        public static string ToCSharpString(this Type type, string[] usingNamespaces = null, Assembly[] usingAssemblies = null, SymbolDisplayFormat symbolDisplayFormat = null)
+        {
+            var compilationUnit = SyntaxFactory.CompilationUnit();
+            if (usingNamespaces != null)
+            {
+                compilationUnit = compilationUnit.AddUsings(
+                    Array.ConvertAll(usingNamespaces, n => SyntaxFactory.UsingDirective(SyntaxFactory.ParseName(n))));
+            }
+            else
+            {
+                compilationUnit = compilationUnit.AddUsings(
+                    SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName("System")));
+            }
+
+            MetadataReference[] metadataReferences;
+            if (usingAssemblies != null)
+            {
+                metadataReferences = Array.ConvertAll(usingAssemblies, u => MetadataReference.CreateFromFile(u.Location));
+            }
+            else
+            {
+                metadataReferences = new[]
+                {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(type.Assembly.Location)
+            };
+            }
+
+            var typeName = SyntaxFactory.ParseTypeName(type.ToFullyQualifiedName());
+
+            var field = SyntaxFactory.FieldDeclaration(
+                SyntaxFactory.VariableDeclaration(typeName).WithVariables(
+                    SyntaxFactory.SingletonSeparatedList(
+                        SyntaxFactory.VariableDeclarator(
+                            SyntaxFactory.Identifier("field")))));
+            compilationUnit = compilationUnit.AddMembers(
+                SyntaxFactory.ClassDeclaration("MyClass").AddMembers(
+                    field))
+                .NormalizeWhitespace();
+
+            var tree = compilationUnit.SyntaxTree;
+            var compilation = CSharpCompilation.Create("MyAssembly", [tree], metadataReferences);
+            var semanticModel = compilation.GetSemanticModel(tree);
+            var root = tree.GetRoot();
+
+            var typeSymbol = semanticModel.GetTypeInfo(compilationUnit
+                .DescendantNodes().OfType<ClassDeclarationSyntax>().Single()
+                .Members.OfType<FieldDeclarationSyntax>().Single()
+                .Declaration.Type);
+
+            return typeSymbol.Type.ToDisplayString(symbolDisplayFormat ?? new SymbolDisplayFormat(
+                typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypes,
+                genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters,
+                miscellaneousOptions: SymbolDisplayMiscellaneousOptions.UseSpecialTypes));
+        }
+
+        public static string ToFullyQualifiedName(this Type type)
+        {
+            switch (type)
+            {
+                case { IsGenericParameter: true }: return type.Name;
+                case { IsArray: true }: return type.GetElementType().ToFullyQualifiedName() + "[]";
+                case { IsPointer: true }: return type.GetElementType().ToFullyQualifiedName() + "*";
+                case { IsByRef: true }: return type.GetElementType().ToFullyQualifiedName() + "&";
+                case { IsGenericType: false }: return string.IsNullOrEmpty(type.FullName) ? type.Name : type.FullName.Replace('+', '.');
+                default:
+                    var fullName = type.GetGenericTypeDefinition().FullName;
+                    var backTickIndex = fullName.IndexOf('`');
+                    if (backTickIndex > 0)
+                    {
+                        fullName = fullName.Substring(0, backTickIndex);
+                    }
+
+                    return fullName.Replace('+', '.') + "<" + string.Join(", ", type.GetGenericArguments().Select(ToFullyQualifiedName)) + ">";
+            }
+            ;
+        }
+    }
+}
---
+++ sources/GenerateGenericMath/GenerateMatrixMath/Templates/Templates.cs
@@ -0,0 +1,50 @@
+﻿namespace GenerateMatrixMath
+{
+    partial class Templates
+    {
+        private static Dictionary<Type, string> NameCache = new();
+
+        private static string Encode(object value) => value switch
+        {
+            Type type => NameCache.TryGetValue(type, out var name) ? name : NameCache[type] = type.ToCSharpString(),
+            _ => value?.ToString() ?? "",
+        };
+
+        public static string Name(dynamic model)
+        {
+            using var writer = new StringWriter();
+            Name(model, writer, null);
+            return writer.ToString();
+        }
+
+        public static void Name(dynamic model, TextWriter writer, string? indentation) => RenderName(model, writer);
+
+        public static void Ordinal(int num, TextWriter writer, string? indentation) => writer.Write(Ordinal(num));
+
+        public static string Ordinal(int num)
+        {
+            if (num <= 0) return num.ToString();
+
+            switch (num % 100)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return num + "th";
+            }
+
+            switch (num % 10)
+            {
+                case 1:
+                    return num + "st";
+                case 2:
+                    return num + "nd";
+                case 3:
+                    return num + "rd";
+                default:
+                    return num + "th";
+            }
+        }
+
+    }
+}
