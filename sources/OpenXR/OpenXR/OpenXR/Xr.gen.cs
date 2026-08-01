58b01a367de1104d8df30b08b1044426e3938c8e
William Chen <exanite78012@gmail.com>
2026-08-01 03:08:25 -0400

    Add todo marking manual cast in Xr.gen.cs

--- sources/OpenXR/OpenXR/OpenXR/Xr.gen.cs
+++ sources/OpenXR/OpenXR/OpenXR/Xr.gen.cs
@@ -5961,6 +5961,7 @@ public unsafe partial class Xr : IXr, IXr.Static
         ["XR_LOADER_VERSION_1_0", "XR_VERSION_1_0", "XR_VERSION_1_1"],
         MinVersion = "1.0"
     )]
+    // TODO: The below is a manual fix for https://github.com/dotnet/ClangSharp/issues/836
     public static ulong MaxEventDataSize => (ulong)sizeof(EventDataBuffer);
 
     [NativeName("XR_EXTENSION_ENUM_BASE")]
